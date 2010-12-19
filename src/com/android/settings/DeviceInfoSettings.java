092128464f1e1b5476641e47ae5793fef8a17205
Chris Soyars <ctso@ctso.me>
2010-12-19 17:26:58 -0500

    Add Modver to Settings.
    Change-Id: I596064a80210517ff0dae30401f71b52adabd614
    
    Rename modversion
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    DeviceInfoSettings: Use ro.cm.display.version instead of ro.cm.version
    
    Follows the same logic as ro.build.display.id/ro.build.id:
    engineering, debug and unsigned builds will have no distinction,
    user and signed builds will show a simplified version
    
    Change-Id: I713ccb2445ea2e333e831129ef7733fd518282eb
    
    settings: This is CyanogenMod!
    
    Change-Id: If9f14d6579b18c4ae049d35e312dd2d98746aced
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -81,6 +81,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_MOD_VERSION = "mod_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -128,6 +129,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_MOD_VERSION, "ro.cm.display.version");
+        findPreference(KEY_MOD_VERSION).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
