f99a8b72245325f743998ce7219ac7141b1a6148
Chris Soyars <ctso@ctso.me>
2010-12-19 17:26:58 -0500

    Add Modver to Settings.
    Change-Id: I596064a80210517ff0dae30401f71b52adabd614
    
    Rename modversion
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    DeviceInfoSettings: Use ro.cm.display.version instead of ro.cm.version
    
    Follows the same logic as ro.build.display.id/ro.build.id:
    engineering, debug and unsigned builds will have no distinction,
    user and signed builds will show a simplified version
    
    Change-Id: I713ccb2445ea2e333e831129ef7733fd518282eb
    
    settings: This is CyanogenMod!
    
    Change-Id: If9f14d6579b18c4ae049d35e312dd2d98746aced
    
    Change-Id: I6557d5aa68a3ad00166cdd2b8951f425ad7800df
    
    Settings: Use cyanogenmod.os.Build for props.
    
    Change-Id: I7c1ac67a001c090e784ec2b02cefdeb1026572b5
    
    Settings: Add CM Api level to about phone.
    
    Change-Id: I802202719ca9a2d1f98fda807f133efceec7be4a
    
    Settings: Update strings for api level.
    
    Change-Id: I9ed5a1020ad5ee750c9270897710784e31ce354f
    
    Added build date (ro.build.date from build.prop) to device info.
    
    Change-Id: I5e19ee04de5c26822e171d4af8f4bfc052be9b74
    
    CM PlatLogo: CyanogenMod version preference (2/2)
    
    Binds the CyanogenMod version preference to launch CM easter egg
    upon multiple taps.
    
    Change-Id: I8e06642944d24581c0932c9964f2d9615bfcb832
    
    Settings: Use cm build properties correctly.
    
    TICKET: CYNGNOS-2379
    Change-Id: I8342589c52d633c044570be02f48c7f5a48f788c

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -73,6 +73,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
     private static final String KEY_MBN_VERSION = "mbn_version";
     private static final String PROPERTY_MBN_VERSION = "persist.mbn.version";
+    private static final String KEY_MOD_VERSION = "mod_version";
+    private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_MOD_API_LEVEL = "mod_api_level";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -125,6 +128,12 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MBN_VERSION, PROPERTY_MBN_VERSION);
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_MBN_VERSION,
                 PROPERTY_MBN_VERSION);
+        findPreference(KEY_MOD_VERSION).setSummary(
+                cyanogenmod.os.Build.CYANOGENMOD_DISPLAY_VERSION);
+        findPreference(KEY_MOD_VERSION).setEnabled(true);
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setExplicitValueSummary(KEY_MOD_API_LEVEL, constructApiLevelString());
+        findPreference(KEY_MOD_API_LEVEL).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
@@ -301,6 +310,20 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             if (b.getBoolean(CarrierConfigManager.KEY_CI_ACTION_ON_SYS_UPDATE_BOOL)) {
                 ciActionOnSysUpdate(b);
             }
+        } else if (preference.getKey().equals(KEY_MOD_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.putExtra("is_cm", true);
+                intent.setClassName("android",
+                        com.android.internal.app.PlatLogoActivity.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
         }
         return super.onPreferenceTreeClick(preference);
     }
@@ -368,6 +391,14 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         }
     }
 
+    private void setExplicitValueSummary(String preference, String value) {
+        try {
+            findPreference(preference).setSummary(value);
+        } catch (RuntimeException e) {
+            // No recovery
+        }
+    }
+
     private void sendFeedback() {
         String reporterPackage = DeviceInfoUtils.getFeedbackReporterPackage(getActivity());
         if (TextUtils.isEmpty(reporterPackage)) {
@@ -376,6 +407,14 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         Intent intent = new Intent(Intent.ACTION_BUG_REPORT);
         intent.setPackage(reporterPackage);
         startActivityForResult(intent, 0);
+    }
+
+    private static String constructApiLevelString() {
+        int sdkInt = cyanogenmod.os.Build.CM_VERSION.SDK_INT;
+        StringBuilder builder = new StringBuilder();
+        builder.append(cyanogenmod.os.Build.getNameForSDKInt(sdkInt))
+                .append(" (" + sdkInt + ")");
+        return builder.toString();
     }
 
     private static class SummaryProvider implements SummaryLoader.SummaryProvider {
