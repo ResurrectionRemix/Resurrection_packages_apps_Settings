81f199fc7a6502fcee129aeee85b0cbadb0393a5
Keir Stiegler <keir.stiegler@gmail.com>
2012-03-09 15:19:20 -0600

    add auto-brightness entries, fix typo
    Conflicts:
    
    	res/values/arrays.xml

--- src/com/android/settings/LightLevelsActivity.java
+++ src/com/android/settings/LightLevelsActivity.java
@@ -14,6 +14,7 @@ import android.os.IPowerManager;
 import android.os.ServiceManager;
 import android.provider.Settings;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
