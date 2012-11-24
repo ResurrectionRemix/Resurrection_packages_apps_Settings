79518da7846b4997e7a62aa39874a7cc8cc820cd
DvTonder <david.vantonder@gmail.com>
2012-11-24 11:10:29 -0500

    Settings: Forward port Expanded desktop capability
    Originally authored by Jesús David and ported from
            ParanoidAndroid ICS branch
    
    Change-Id: Iad1be66f48db2fe022a87822a092d6606c2a6477

--- src/com/android/settings/cyanogenmod/PowerMenu.java
+++ src/com/android/settings/cyanogenmod/PowerMenu.java
@@ -30,12 +30,14 @@ public class PowerMenu extends SettingsPreferenceFragment {
 
     private static final String KEY_REBOOT = "power_menu_reboot";
     private static final String KEY_SCREENSHOT = "power_menu_screenshot";
+    private static final String KEY_EXPANDED_DESKTOP = "power_menu_expanded_desktop";
     private static final String KEY_PROFILES = "power_menu_profiles";
     private static final String KEY_AIRPLANE = "power_menu_airplane";
     private static final String KEY_SILENT = "power_menu_silent";
 
     private CheckBoxPreference mRebootPref;
     private CheckBoxPreference mScreenshotPref;
+    private CheckBoxPreference mExpandedDesktopPref;
     private CheckBoxPreference mProfilesPref;
     private CheckBoxPreference mAirplanePref;
     private CheckBoxPreference mSilentPref;
@@ -53,6 +55,18 @@ public class PowerMenu extends SettingsPreferenceFragment {
         mScreenshotPref = (CheckBoxPreference) findPreference(KEY_SCREENSHOT);
         mScreenshotPref.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0) == 1));
+
+        mExpandedDesktopPref = (CheckBoxPreference) findPreference(KEY_EXPANDED_DESKTOP);
+        boolean showExpandedDesktopPref =
+            getResources().getBoolean(R.bool.config_show_expandedDesktop);
+        if (!showExpandedDesktopPref) {
+            if (mExpandedDesktopPref != null) {
+                getPreferenceScreen().removePreference(mExpandedDesktopPref);
+            }
+        } else {
+            mExpandedDesktopPref.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED, 0) == 1));
+        }
 
         mProfilesPref = (CheckBoxPreference) findPreference(KEY_PROFILES);
         mProfilesPref.setChecked((Settings.System.getInt(getContentResolver(),
@@ -81,6 +95,11 @@ public class PowerMenu extends SettingsPreferenceFragment {
             value = mScreenshotPref.isChecked();
             Settings.System.putInt(getContentResolver(),
                     Settings.System.POWER_MENU_SCREENSHOT_ENABLED,
+                    value ? 1 : 0);
+        } else if (preference == mExpandedDesktopPref) {
+            value = mExpandedDesktopPref.isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.POWER_MENU_EXPANDED_DESKTOP_ENABLED,
                     value ? 1 : 0);
         } else if (preference == mRebootPref) {
             value = mRebootPref.isChecked();
