1015b7382f470c48b0e6ae73a27158e5ac9d46ea
xoomdev <xoomdevnet@gmail.com>
2012-11-27 22:13:10 -0600

    Multiuser: Add Quicklinks to options for restricted users.
    Settings now has a list of id's that it will show for restricted users,
    make sure that quick links are there for these users to access
    features.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -120,7 +120,11 @@ public class Settings extends PreferenceActivity
             R.id.system_section,
             R.id.date_time_settings,
             R.id.about_settings,
-            R.id.accessibility_settings
+            R.id.accessibility_settings,
+            R.id.quick_links,
+            R.id.rom_control,
+            R.id.themes,
+            R.id.advanced_settings
     };
 
     private SharedPreferences mDevelopmentPreferences;
