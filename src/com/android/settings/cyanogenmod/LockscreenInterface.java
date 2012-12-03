3e1dc9e29980d634e9972ca12083bbfa8dc05b58
DvTonder <david.vantonder@gmail.com>
2012-12-02 19:17:32 -0500

    Settings: Forward port Slider shortcuts
    Includes Change I683d9c22: Add the new lockscreen drawables to the
    picker list by Bjorn Lunden (blunden)
    
    Settings : Lockscreen targets alignment fix
    
    Add separate layouts for different layouts to fix alignment issues.
    
    Change-Id: I2453225e8a683244a1e11f0a8d3c18954ba5885f
    
    Change-Id: I155824abc0578d98d3e2f1f5c2dcde3fa7b2ef84

---
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -0,0 +1,53 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class LockscreenInterface extends SettingsPreferenceFragment {
+    private static final String TAG = "LockscreenInterface";
+
+    public boolean hasButtons() {
+        return !getResources().getBoolean(com.android.internal.R.bool.config_showNavigationBar);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
