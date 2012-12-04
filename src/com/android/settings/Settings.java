85d44885cf56bab9261bcea699bb98991f913013
D4rKn3sSyS <jedga_95@outlook.com>
2012-12-04 14:33:29 +0000

    Revert "enable developer options visibility by default for userdebug builds"
    Nope, don't change aosp behaviour please
    
    This reverts commit 9ec04b8a26feab8ea73211c07c226447effc6309.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -414,7 +414,7 @@ public class Settings extends PreferenceActivity
     private void updateHeaderList(List<Header> target) {
         final boolean showDev = mDevelopmentPreferences.getBoolean(
                 DevelopmentSettings.PREF_SHOW,
-                android.os.Build.TYPE.equals("eng") || android.os.Build.TYPE.equals("userdebug"));
+                android.os.Build.TYPE.equals("eng"));
         int i = 0;
 
         mHeaderIndexMap.clear();
