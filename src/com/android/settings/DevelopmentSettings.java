a33938d56d9d7cabfb144b42e3bdd3fcdd49fb84
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-05 18:45:29 -0500

    Developer Options: launch the Development app
    Change-Id: I0f798d197888ef7f40688009536040e061c410f4

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -144,6 +144,8 @@ public class DevelopmentSettings extends PreferenceFragment
 
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
 
+    private static final String DEVELOPMENT_TOOLS = "development_tools";
+
     private static final int RESULT_DEBUG_APP = 1000;
 
     private IWindowManager mWindowManager;
@@ -198,6 +200,7 @@ public class DevelopmentSettings extends PreferenceFragment
 
     private ListPreference mRootAccess;
     private Object mSelectedRootValue;
+    private PreferenceScreen mDevelopmentTools;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
     private final ArrayList<CheckBoxPreference> mResetCbPrefs
@@ -311,6 +314,9 @@ public class DevelopmentSettings extends PreferenceFragment
         if (!removeRootOptionsIfRequired()) {
             mAllPrefs.add(mRootAccess);
         }
+
+        mDevelopmentTools = (PreferenceScreen) findPreference(DEVELOPMENT_TOOLS);
+        mAllPrefs.add(mDevelopmentTools);
     }
 
     private CheckBoxPreference findAndInitCheckboxPref(String key) {
