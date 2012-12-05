cb8e4daee56342a843ebe2e240ba4a15f235516b
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-05 18:45:29 -0500

    Developer Options: launch the Development app
    Change-Id: I0f798d197888ef7f40688009536040e061c410f4

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -208,6 +208,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
 
     private static final String OTA_DISABLE_AUTOMATIC_UPDATE_KEY = "ota_disable_automatic_update";
 
+    private static final String DEVELOPMENT_TOOLS = "development_tools";
+
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
@@ -303,6 +305,7 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     
     private ListPreference mRootAccess;
     private Object mSelectedRootValue;
+    private PreferenceScreen mDevelopmentTools;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
 
@@ -502,6 +505,9 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         if (!removeRootOptionsIfRequired()) {
             mAllPrefs.add(mRootAccess);
         }
+
+        mDevelopmentTools = (PreferenceScreen) findPreference(DEVELOPMENT_TOOLS);
+        mAllPrefs.add(mDevelopmentTools);
     }
 
     private ListPreference addListPreference(String prefKey) {
