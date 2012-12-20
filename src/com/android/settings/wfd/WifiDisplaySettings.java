7740297725b1e69d75307147a9e339d5eace4e8f
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-12-20 00:33:30 +0000

    WifiDisplay: Add an HDCP toggle
    Some WFD dongles have frequent (or permanent) failures while
    negotiating HDCP with the device, so add a toggle to allow
    skipping HDCP entirely
    
    Change-Id: Ia70122d63aa76bcb91428152f588c4d6dd2b610f

--- src/com/android/settings/wfd/WifiDisplaySettings.java
+++ src/com/android/settings/wfd/WifiDisplaySettings.java
@@ -31,6 +31,8 @@ import android.hardware.display.WifiDisplayStatus;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
@@ -73,6 +75,9 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
     private TextView mEmptyView;
 
     private Switch mActionBarSwitch;
+
+    private CheckBoxPreference mDisableHDCP;
+    private static final String DISABLE_HDCP_PREF = "persist.sys.wfd.nohdcp";
 
     public WifiDisplaySettings() {
     }
@@ -185,6 +190,10 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
                 mDisplayManager.connectWifiDisplay(display.getDeviceAddress());
             }
         }
+        else if (preference == mDisableHDCP) {
+            SystemProperties.set(DISABLE_HDCP_PREF, mDisableHDCP.isChecked() ? "1" : "0");
+            return true;
+        }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -209,12 +218,20 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
             final WifiDisplay[] pairedDisplays = mWifiDisplayStatus.getRememberedDisplays();
             final WifiDisplay[] availableDisplays = mWifiDisplayStatus.getAvailableDisplays();
 
+            mDisableHDCP = new CheckBoxPreference(getActivity().getApplicationContext());
+            mDisableHDCP.setTitle(R.string.pref_wifi_disable_hdcp_title);
+            mDisableHDCP.setSummary(R.string.pref_wifi_disable_hdcp_summary);
+            mDisableHDCP.setChecked(Integer.parseInt(SystemProperties.get(DISABLE_HDCP_PREF, "0")) != 0);
+            mDisableHDCP.setOrder(0);
+            preferenceScreen.addPreference(mDisableHDCP);
+
             if (mPairedDevicesCategory == null) {
                 mPairedDevicesCategory = new PreferenceCategory(getActivity());
                 mPairedDevicesCategory.setTitle(R.string.wifi_display_paired_devices);
             } else {
                 mPairedDevicesCategory.removeAll();
             }
+            mPairedDevicesCategory.setOrder(1);
             preferenceScreen.addPreference(mPairedDevicesCategory);
 
             for (WifiDisplay d : pairedDisplays) {
@@ -231,6 +248,7 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
             } else {
                 mAvailableDevicesCategory.removeAll();
             }
+            mAvailableDevicesCategory.setOrder(2);
             preferenceScreen.addPreference(mAvailableDevicesCategory);
 
             for (WifiDisplay d : availableDisplays) {
