8f2d56103582ab49f6f7b4141e5a536587b6fe1f
nebkat <nebkat@teamhacksung.org>
2012-12-30 13:09:29 +0000

    Settings: Add launcher settings to shared
    Change-Id: I6eb85d1f5d592b05891e9b343fc5fce05c2bb2a4

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -122,7 +122,8 @@ public class Settings extends PreferenceActivity
             R.id.system_section,
             R.id.date_time_settings,
             R.id.about_settings,
-            R.id.accessibility_settings
+            R.id.accessibility_settings,
+            R.id.launcher_settings
     };
 
     private SharedPreferences mDevelopmentPreferences;
