5f2f69627c079381314aa407da9b80c3753943cf
Danny Baumann <dannybaumann@web.de>
2013-01-15 14:44:49 +0100

    Remove app security settings from lockscreen settings.
    Change-Id: Id0f4c29bc52117b58392a35804c3cd2b3c7c631b

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -382,15 +382,15 @@ public class SecuritySettings extends SettingsPreferenceFragment
                     mToggleVerifyApps.setEnabled(false);
                 }
             }
-        }
 
-        boolean isTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-        if (isTelephony) {
-            addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
-            mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-            mSmsSecurityCheck.setOnPreferenceChangeListener(this);
-            int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
-            updateSmsSecuritySummary(smsSecurityCheck);
+            boolean isTelephony = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+            if (isTelephony) {
+                addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
+                mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
+                mSmsSecurityCheck.setOnPreferenceChangeListener(this);
+                int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
+                updateSmsSecuritySummary(smsSecurityCheck);
+            }
          }
 
         return root;
