de32891a4b6d97cf6265cabc1659788a7a91b3eb
DvTonder <david.vantonder@gmail.com>
2013-01-30 20:45:16 -0500

    Settings: Hide Development settings when it is turned off
    This commit hides the Development and Performance settings items
    when the user turns off Development settings. This sets things back
    to tapping x number of times on the build number to once again enable
    developer settings
    
    Patch set 1,2 : Initial commit
    Patch set 3   : Fix case where, if user toggles off and on without
                    exiting Development settings, the main menu items
                    should remain visible
    Patch set 4   : Fix typo in comment
    
    Change-Id: I6003f73f5abee54bb000f01757ecffe7b68dba59

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2008 The Android Open Source Project
+ * Portions Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1098,6 +1099,11 @@ public class DevelopmentSettings extends PreferenceFragment
                             Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
                     mLastEnabledState = isChecked;
                     setPrefsEnabledState(mLastEnabledState);
+
+                    // Hide development settings from the Settings menu (Android 4.2 behaviour)
+                    getActivity().getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).edit()
+                        .putBoolean(PREF_SHOW, false)
+                        .apply();
                 }
             }
         }
@@ -1320,6 +1326,13 @@ public class DevelopmentSettings extends PreferenceFragment
                         Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 1);
                 mLastEnabledState = true;
                 setPrefsEnabledState(mLastEnabledState);
+
+                // Make sure the development settings is visible in the main Settings menu
+                // This is needed since we may have just turned off dev settings and want to
+                // turn it on again
+                getActivity().getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).edit()
+                    .putBoolean(PREF_SHOW, true)
+                    .apply();
             }
         } else if (dialog == mRootDialog) {
             if (which == DialogInterface.BUTTON_POSITIVE) {
