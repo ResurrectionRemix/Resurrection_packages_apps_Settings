2dc0b6620a8fc1cfcbdff7dedd7aad9de40b84e4
drcmda <drcmda@gmail.com>
2013-02-01 22:08:14 +0000

    bugfixes
    Change-Id: Icf385b3f322f20d2714de0780da479648edadd14

--- src/com/android/settings/paranoid/Toolbar.java
+++ src/com/android/settings/paranoid/Toolbar.java
@@ -127,15 +127,20 @@ public class Toolbar extends SettingsPreferenceFragment
         mPieMode.setOnPreferenceChangeListener(this);
 
         mPieSize = (ListPreference) prefSet.findPreference(PIE_SIZE);
-        String pieSize = Settings.System.getString(mContext.getContentResolver(),
-                Settings.System.PIE_SIZE);
-        mPieSize.setValue(pieSize != null && !pieSize.isEmpty() ? pieSize : "1");
-        mPieSize.setOnPreferenceChangeListener(this);
-
         mPieTrigger = (ListPreference) prefSet.findPreference(PIE_TRIGGER);
-        String pieTrigger = Settings.System.getString(mContext.getContentResolver(),
-                Settings.System.PIE_TRIGGER);
-        mPieTrigger.setValue(pieTrigger != null && !pieTrigger.isEmpty() ? pieTrigger : "1");
+        try {
+            float pieSize = Settings.System.getFloat(mContext.getContentResolver(),
+                    Settings.System.PIE_SIZE);
+            mPieSize.setValue(String.valueOf(pieSize));
+  
+            float pieTrigger = Settings.System.getFloat(mContext.getContentResolver(),
+                    Settings.System.PIE_TRIGGER);
+            mPieTrigger.setValue(String.valueOf(pieTrigger));
+        } catch(Settings.SettingNotFoundException ex) {
+            // So what
+        }
+
+        mPieSize.setOnPreferenceChangeListener(this);
         mPieTrigger.setOnPreferenceChangeListener(this);
 
         mPieGap = (ListPreference) prefSet.findPreference(PIE_GAP);
@@ -238,9 +243,9 @@ public class Toolbar extends SettingsPreferenceFragment
                     Settings.System.PIE_GAP, pieGap);
             return true;
         } else if (preference == mPieTrigger) {
-            float pierigger = Float.valueOf((String) newValue);
+            float pieTrigger = Float.valueOf((String) newValue);
             Settings.System.putFloat(getActivity().getContentResolver(),
-                    Settings.System.PIE_TRIGGER, pierigger);
+                    Settings.System.PIE_TRIGGER, pieTrigger);
             return true;
         }
         return false;
