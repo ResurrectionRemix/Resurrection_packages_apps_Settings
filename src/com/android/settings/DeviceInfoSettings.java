AOKP/packages_apps_Settings

Settings: add modversion
Change-Id: Ia6b9ea1fe7dccb4ad57bbf215e6c0feabcb16899
Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>

src/com/android/settings/DeviceInfoSettings.java
@@ -62,6 +62,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_MOD_VERSION = "mod_version";
+
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_GPU = "device_gpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
@@ -83,6 +84,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_MOD_VERSION, "ro.aokp.version");
 
         addStringPreference(KEY_DEVICE_CPU,
                 SystemProperties.get("ro.device.cpu", getCPUInfo()));

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/DeviceInfoSettings.java
@@ -100,6 +100,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_AOKP_VERSION, "ro.aokp.version");
         setValueSummary(KEY_CM_VERSION, "ro.cm.version");
         findPreference(KEY_CM_VERSION).setEnabled(true);
         setValueSummary(KEY_PA_VERSION, "ro.pa.version");