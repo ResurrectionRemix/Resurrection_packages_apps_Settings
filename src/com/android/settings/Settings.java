8b775631eb55d8fbe5bb4650668d632c3a53a07e
qnhoang81 <qnhoang81@gmail.com>
2013-02-19 19:38:31 -0700

    Settings part: Add Sprint Tools (CDMATools) into settings. (Part 1 of 3)   -devices can set this in overlay. (Not for all CDMA devices)
    For more information see this thread:  http://forum.xda-developers.com/showthread.php?t=1774503
    
    Patchset 2: get rid of TABS
    Patchset 3: attempting to make it look clean
    Patchset 4: last attempt tonight to clean it...
    Patchset 5: rename
    
    Change-Id: I121f61cd16d4064a1ded85428ac4857c653e6582

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -125,7 +125,8 @@ public class Settings extends PreferenceActivity
             R.id.rom_control,
             R.id.themes,
             R.id.performance,
-            R.id.advanced_settings
+            R.id.advanced_settings,
+            R.id.sprint_tools
     };
 
     private SharedPreferences mDevelopmentPreferences;
@@ -436,7 +437,10 @@ public class Settings extends PreferenceActivity
             } else if (id == R.id.advanced_settings) {
                 if (!needsAdvancedSettings())
                     target.remove(header);
-            } else if (id == R.id.wifi_settings) {
+            } else if (id == R.id.sprint_tools) {
+                if (!isSprintDevice())
+                    target.remove(header);
+	    } else if (id == R.id.wifi_settings) {
                 // Remove WiFi Settings if WiFi service is not available.
                 if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI)) {
                     target.remove(i);
@@ -554,6 +558,10 @@ public class Settings extends PreferenceActivity
 
     private boolean needsAdvancedSettings() {
         return getResources().getBoolean(R.bool.has_advanced_settings);
+    }
+
+    private boolean isSprintDevice() {
+        return getResources().getBoolean(R.bool.is_sprint_device);
     }
 
     private void getMetaData() {
