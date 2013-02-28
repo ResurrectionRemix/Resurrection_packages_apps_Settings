b1b99e29c2918bc4f291e080235dfed6349c9da6
Koushik Dutta <koushd@gmail.com>
2013-02-28 14:49:24 -0800

    Switch to open source superuser
    Change-Id: I0882b4bb296af041ec5766ff29a605554f13b0a7

--- Android.mk
+++ Android.mk
@@ -13,6 +13,12 @@ LOCAL_CERTIFICATE := platform
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
+
+LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
+
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
