7d9bd207fc0fab0b11d346b92401cdd50b938927
drcmda <drcmda@gmail.com>
2013-06-10 11:57:47 -0700

    TEST HALO (2/2)
    HALO is Androids one and only multitasking solution.
    
    We explicitly do not go the Samsung/Cornerstone route. This solution is basically an extension of
    the regular notificationshade, which is an aggregator for notifications that can be checked out when
    time permits. HALO completes it by allowing you to respond to tasks on the spot, trying to be as
    unintrusive as possible and flexible in regards to policies (white/black lists).
    
    Features:
    
    - Apps open up in floating state, apps underneath do not pause
    - complete SystemUI & Core integration, making HALO feel like a native feature, source/SDK and UI/UX
    - HALO has white and blacklists, which for now allow fine control for apps pinging through HALO
    - multiple gestures to navigate HALO:
      - double-tap-move: move
      - tap-drag-left/right: task through notifications, finger up launches
      - tap-drag-up: dismiss notification
      - tap-drag-down: put HALO into semi-hidden state
      - swipe while HALO is hidden wakes it up
      - drag-to-X to remove HALO altogether
    - comes with as few settings as possible, everything should be self explanatory and intutive,
      users must be informed about the double-tap gestures which is the only catch.
    
    Known issues:
    
    - Integration has been finished for PhoneUI. TabletUI and PIE must be integrated still.
    - Packageinstaller must allow HALO through its overlay check or it will disable the install-app button.
    - The low level has a few remaining issues:
      - Some apps still go up fullscale (they trick us with services, circumventing the intent flagging)
      - Some 3D games still pause
      - There is one known app that refuses to end on back or touch-outside, that is WeChat
      - The activitystack pause implementation works but is makeshift
    
    To-Do:
    
    - On-screen tutorial at first start up to teach users the gestures
    - Fixing all known issues, full integration in the remaining UI's
    - Perhaps hybrid engine integration for running floating apps in a decreased size
    
    Merge branch 'multiwindow' into jellybean
    
    Conflicts:
    	res/values/arrays.xml
    	src/com/android/settings/paranoid/Toolbar.java
    
    Change-Id: I87ad32f302592fff2c77e7cb37c5695d40ee90cd
    
    installed_app_details
    
    Change-Id: I01a91910683f1bf00250ee17c2d476927f6bc386
    
    Conflicts:
    
    	res/values/arrays.xml
    	res/values/strings.xml
    	res/xml/system_settings.xml

---
+++ src/com/android/settings/paranoid/Halo.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2012 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.paranoid;
+
+import android.app.AlertDialog;
+import android.app.INotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.IWindowManager;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class Halo extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HALO_STATE = "halo_state";
+    private static final String KEY_HALO_HIDE = "halo_hide";
+    private static final String KEY_HALO_REVERSED = "halo_reversed";
+
+    private ListPreference mHaloState;
+    private CheckBoxPreference mHaloHide;
+    private CheckBoxPreference mHaloReversed;
+
+    private Context mContext;
+    private INotificationManager mNotificationManager;
+    private int mAllowedLocations;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.halo_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        mContext = getActivity();
+
+        mNotificationManager = INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+
+        mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
+        mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
+        mHaloState.setOnPreferenceChangeListener(this);
+
+        mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
+        mHaloHide.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_HIDE, 0) == 1);
+
+        mHaloReversed = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_REVERSED);
+        mHaloReversed.setChecked(Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HALO_REVERSED, 1) == 1);
+        }
+
+    private boolean isHaloPolicyBlack() {
+        try {
+            return mNotificationManager.isHaloPolicyBlack();
+        } catch (android.os.RemoteException ex) {
+                // System dead
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHaloHide) {	
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_HIDE, mHaloHide.isChecked()
+                    ? 1 : 0);	
+        } else if (preference == mHaloReversed) {	
+            Settings.System.putInt(mContext.getContentResolver(),
+                    Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
+                    ? 1 : 0);	
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) newValue) == 1;
+            try {
+                mNotificationManager.setHaloPolicyBlack(state);
+            } catch (android.os.RemoteException ex) {
+                // System dead
+            }          
+            return true;
+        }
+        return false;
+    }
+}
