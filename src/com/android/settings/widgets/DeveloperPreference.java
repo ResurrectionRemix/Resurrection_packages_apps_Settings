a6592fe261114e3c831029d58f0f9b5b94790b87
altan <korukaltan@gmail.com>
2013-06-29 02:14:51 +0300

    about resurrection: Switch to Gravatar for team Avatars
    Since Twitter sucks and blocked our access lets dump their usage
    entirely# Please enter the commit message for your changes. Lines
    starting in favor of gravatar hosting# with '#' will be ignored, and an
    empty message aborts the commit. *Some of the email addresses will need
    to be updated
    
    JBirdVegas<jbirdvegas@aokp.co>Jun 27, 2013 5:23 AM
    Change-Id: I36841d22734efab22fe4b622dc014e3bacee8e81# Changes to be committed:

--- src/com/android/settings/widgets/DeveloperPreference.java
+++ src/com/android/settings/widgets/DeveloperPreference.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -14,8 +15,10 @@ import android.preference.Preference;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.Display;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,11 +34,13 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.params.HttpClientParams;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class DeveloperPreference extends Preference {
 
     private static final String TAG = "DeveloperPreference";
-
+    public static final String GRAVATAR_API = "http://www.gravatar.com/avatar/";
+    public static int mDefaultAvatarSize = 200;
     private ImageView twitterButton;
     private ImageView donateButton;
     private ImageView photoView;
@@ -45,15 +50,26 @@ public class DeveloperPreference extends Preference {
     private String nameDev;
     private String twitterName;
     private String donateLink;
+    private String devEmail;
+    private final Display mDisplay;
 
     public DeveloperPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
-        nameDev = a.getString(R.styleable.DeveloperPreference_nameDev);
-        twitterName = a.getString(R.styleable.DeveloperPreference_twitterHandle);
-        donateLink = a.getString(R.styleable.DeveloperPreference_donateLink);
-        a.recycle();
+        TypedArray typedArray = null;
+        try {
+            typedArray = context.obtainStyledAttributes(attrs, R.styleable.DeveloperPreference);
+            nameDev = typedArray.getString(R.styleable.DeveloperPreference_nameDev);
+            twitterName = typedArray.getString(R.styleable.DeveloperPreference_twitterHandle);
+            donateLink = typedArray.getString(R.styleable.DeveloperPreference_donateLink);
+            devEmail = typedArray.getString(R.styleable.DeveloperPreference_emailDev);
+        } finally {
+            if (typedArray != null) {
+                typedArray.recycle();
+            }
+        }
+        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = wm.getDefaultDisplay();
     }
 
     @Override
@@ -104,11 +120,11 @@ public class DeveloperPreference extends Preference {
 
             // changed to clicking the preference to open twitter
             // it was a hit or miss to click the twitter bird
-            // twitterButton.setOnClickListener(openTwitter);
+
             this.setOnPreferenceClickListener(openTwitter);
-            final String url = "http://api.twitter.com/1/users/profile_image/" + twitterName
-                    + "?size=original";
-            UrlImageViewHelper.setUrlDrawable(this.photoView, url, R.drawable.ic_null,
+            UrlImageViewHelper.setUrlDrawable(this.photoView,
+                    getGravatarUrl(devEmail),
+                    R.drawable.ic_null,
                     UrlImageViewHelper.CACHE_DURATION_ONE_WEEK);
         } else {
             twitterButton.setVisibility(View.INVISIBLE);
@@ -117,5 +133,30 @@ public class DeveloperPreference extends Preference {
 
         devName.setText(nameDev);
 
+    }
+
+    public String getGravatarUrl(String email) {
+        try {
+            Point point = new Point();
+            mDisplay.getSize(point);
+            mDefaultAvatarSize = point.x;
+            String emailMd5 = getMd5(email.trim().toLowerCase());
+            return String.format("%s%s?s=%d",
+                    GRAVATAR_API,
+                    emailMd5,
+                    mDefaultAvatarSize);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    private String getMd5(String devEmail) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(devEmail.getBytes());
+        byte byteData[] = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++)
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        return sb.toString();
     }
 }
