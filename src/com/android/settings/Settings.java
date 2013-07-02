a2b71d7edb4b1595e269e91d08508bf71b5cbef2
altan <korukaltan@gmail.com>
2013-07-02 05:42:33 +0300

    remove aokp stats & fix settings fc
    Change-Id: I424fe9573ea50c77e39d73a0ff764f042f863aaf

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -871,7 +871,6 @@ public class Settings extends PreferenceActivity
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
-    public static class AnonymousStatsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
     public static class ResurrectionActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
