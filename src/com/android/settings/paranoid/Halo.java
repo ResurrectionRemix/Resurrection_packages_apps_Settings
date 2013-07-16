8c0328bcfd2a681a26866db68f2cac28f9324a62
drcmda <drcmda@gmail.com>
2013-07-16 13:58:49 +0100

    HALO Size (2/2)
    Change-Id: I630991372504b152af9dfa063c78c115c79d8c83
    
    Conflicts:
    
    	res/values/arrays.xml
    	res/values/strings.xml

--- src/com/android/settings/paranoid/Halo.java
+++ src/com/android/settings/paranoid/Halo.java
@@ -51,6 +51,7 @@ public class Halo extends SettingsPreferenceFragment
     private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
+    private static final String KEY_HALO_SIZE = "halo_size";
     private static final String KEY_HALO_PAUSE = "halo_pause";
     private static final String KEY_HALO_ENABLED = "halo_enabled";
     private static final String PREF_HALO_COLORS = "halo_colors";
@@ -60,6 +61,7 @@ public class Halo extends SettingsPreferenceFragment
     private static final String PREF_HALO_BUBBLE_TEXT_COLOR = "halo_bubble_text_color";
 
     private ListPreference mHaloState;
+    private ListPreference mHaloSize;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
     private CheckBoxPreference mHaloPause;
@@ -119,6 +121,16 @@ public class Halo extends SettingsPreferenceFragment
 
         mHaloBubbleTextColor = (ColorPickerPreference) findPreference(PREF_HALO_BUBBLE_TEXT_COLOR);
         mHaloBubbleTextColor.setOnPreferenceChangeListener(this);
+
+        mHaloSize = (ListPreference) findPreference(KEY_HALO_SIZE);
+        try {
+            float haloSize = Settings.System.getFloat(mContext.getContentResolver(),
+                    Settings.System.HALO_SIZE, 1.0f);
+            mHaloSize.setValue(String.valueOf(haloSize));  
+        } catch(Exception ex) {
+            // So what
+        }
+        mHaloSize.setOnPreferenceChangeListener(this);
         }
 
     private boolean isHaloPolicyBlack() {
@@ -165,6 +177,11 @@ public class Halo extends SettingsPreferenceFragment
             } catch (android.os.RemoteException ex) {
                 // System dead
             }
+            return true;
+        } else if (preference == mHaloSize) {
+            float haloSize = Float.valueOf((String) Value);
+            Settings.System.putFloat(getActivity().getContentResolver(),
+                    Settings.System.HALO_SIZE, haloSize);
             return true;
         } else if (preference == mHaloCircleColor) {
             String hex = ColorPickerPreference.convertToARGB(
