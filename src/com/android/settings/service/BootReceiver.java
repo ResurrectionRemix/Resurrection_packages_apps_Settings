ecc35005a45149fd12807f61f95a975fa6bf7fe5
altan <korukaltan@gmail.com>
2013-08-21 19:47:22 +0300

    Revert "qh"
    This reverts commit a46ef8743d091b5f07c52eb4b0370bf102cb3ece.
    
    Change-Id: I82b91bf35f454666c09ce6a1e27ced2dcdb8eea6

--- src/com/android/settings/service/BootReceiver.java
+++ src/com/android/settings/service/BootReceiver.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * Copyright (C) 2013 Android Open Kang Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
->>>>>>> 34a5662... Settings: QuietHours AutoSMSReply && QuietHours Bypass (2/2)
 package com.android.settings.service;
 
 import android.content.BroadcastReceiver;
@@ -28,7 +9,6 @@ public class BootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-<<<<<<< HEAD
         if (CallService.returnUserCallSms(context) != 0) {
             context.startService(new Intent(context, CallService.class));
         }
@@ -37,8 +17,4 @@ public class BootReceiver extends BroadcastReceiver {
         }
     }
 
-=======
-        SmsCallHelper.scheduleService(context);
-    }
->>>>>>> 34a5662... Settings: QuietHours AutoSMSReply && QuietHours Bypass (2/2)
 }
