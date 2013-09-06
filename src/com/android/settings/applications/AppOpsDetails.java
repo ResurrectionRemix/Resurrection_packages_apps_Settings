c3e5f3cd3e6eff2fc3636d7cdc021a2096ad52f0
Shashank Mittal <mittals@codeaurora.org>
2013-09-05 19:01:47 -0700

    AppOps: Add more operations in app ops settings.
    Add following operations in app ops settings.
    
    -- Wifi enable/disable
    -- Bluetooth enable/disable
    -- Mobile data enable/disable
    
    Change-Id: I32183d77b88c326168557444a445765b5a69de6f
    
    AppOps: Add support for MMS operations.
    
    Add support  for following MMS operations:
    
    -- Send MMS
    -- Read MMS
    -- Write MMS
    
    Change-Id: Icc823b9bcb446f962927ac0b50fbdae2f3ed3b02
    
    AppOps: Fix appops Settings crash under Chinese
    
    The Chinese array doesn't match the corresponding English array.
    When open appops Settings under Chinese, if ops code is bigger
    than array's size, there will be ArrayIndexOutOfBounds exception.
    
    Change-Id: I2de452550317dd818b7c0fc44f3ae63acfdbed1c
    
    AppOps: Enable App Ops under security setting.
    
    Add support for MODE_ASK mode and enable App Ops under security settings.
    
    Change-Id: I7405f1e8639ca04cc2d7edac144f1738d13dc59f

--- src/com/android/settings/applications/AppOpsDetails.java
+++ src/com/android/settings/applications/AppOpsDetails.java
@@ -36,10 +36,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Switch;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.settings.R;
@@ -60,6 +61,36 @@ public class AppOpsDetails extends Fragment {
     private View mRootView;
     private TextView mAppVersion;
     private LinearLayout mOperationsSection;
+
+    private final int MODE_ALLOWED = 0;
+    private final int MODE_IGNORED = 1;
+    private final int MODE_ASK     = 2;
+
+    private int modeToPosition(int mode) {
+        switch (mode) {
+        case AppOpsManager.MODE_ALLOWED:
+            return MODE_ALLOWED;
+        case AppOpsManager.MODE_IGNORED:
+            return MODE_IGNORED;
+        case AppOpsManager.MODE_ASK:
+            return MODE_ASK;
+        };
+
+        return MODE_IGNORED;
+    }
+
+    private int positionToMode(int position) {
+        switch (position) {
+        case MODE_ALLOWED:
+            return AppOpsManager.MODE_ALLOWED;
+        case MODE_IGNORED:
+            return AppOpsManager.MODE_IGNORED;
+        case MODE_ASK:
+            return AppOpsManager.MODE_ASK;
+        };
+
+        return AppOpsManager.MODE_IGNORED;
+    }
 
     // Utility method to set application label and icon.
     private void setAppLabelAndIcon(PackageInfo pkgInfo) {
@@ -158,16 +189,27 @@ public class AppOpsDetails extends Fragment {
                         entry.getCountsText(res));
                 ((TextView)view.findViewById(R.id.op_time)).setText(
                         entry.getTimeText(res, true));
-                Switch sw = (Switch)view.findViewById(R.id.switchWidget);
+                Spinner sw = (Spinner)view.findViewById(R.id.spinnerWidget);
                 final int switchOp = AppOpsManager.opToSwitch(firstOp.getOp());
-                sw.setChecked(mAppOps.checkOp(switchOp, entry.getPackageOps().getUid(),
-                        entry.getPackageOps().getPackageName()) == AppOpsManager.MODE_ALLOWED);
-                sw.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+                int mode = mAppOps.checkOp(switchOp, entry.getPackageOps().getUid(),
+                        entry.getPackageOps().getPackageName());
+                sw.setSelection(modeToPosition(mode));
+                sw.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+                    boolean firstMode = true;
+
                     @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                        if(firstMode) {
+                            firstMode = false;
+                            return;
+                         }
                         mAppOps.setMode(switchOp, entry.getPackageOps().getUid(),
-                                entry.getPackageOps().getPackageName(), isChecked
-                                ? AppOpsManager.MODE_ALLOWED : AppOpsManager.MODE_IGNORED);
+                                entry.getPackageOps().getPackageName(), positionToMode(position));
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parentView) {
+                        // Do nothing
                     }
                 });
             }
--- src/com/android/settings/applications/AppOpsState.java
+++ src/com/android/settings/applications/AppOpsState.java
@@ -139,15 +139,21 @@ public class AppOpsState {
 
     public static final OpsTemplate MESSAGING_TEMPLATE = new OpsTemplate(
             new int[] { AppOpsManager.OP_READ_SMS,
+                    AppOpsManager.OP_READ_MMS,
                     AppOpsManager.OP_RECEIVE_SMS,
                     AppOpsManager.OP_RECEIVE_EMERGECY_SMS,
                     AppOpsManager.OP_RECEIVE_MMS,
                     AppOpsManager.OP_RECEIVE_WAP_PUSH,
                     AppOpsManager.OP_WRITE_SMS,
+                    AppOpsManager.OP_WRITE_MMS,
                     AppOpsManager.OP_SEND_SMS,
+                    AppOpsManager.OP_SEND_MMS,
                     AppOpsManager.OP_READ_ICC_SMS,
                     AppOpsManager.OP_WRITE_ICC_SMS },
             new boolean[] { true,
+                    true,
+                    true,
+                    true,
                     true,
                     true,
                     true,
@@ -171,7 +177,10 @@ public class AppOpsState {
                     AppOpsManager.OP_AUDIO_MEDIA_VOLUME,
                     AppOpsManager.OP_AUDIO_ALARM_VOLUME,
                     AppOpsManager.OP_AUDIO_NOTIFICATION_VOLUME,
-                    AppOpsManager.OP_AUDIO_BLUETOOTH_VOLUME, },
+                    AppOpsManager.OP_AUDIO_BLUETOOTH_VOLUME,
+                    AppOpsManager.OP_WIFI_CHANGE,
+                    AppOpsManager.OP_BLUETOOTH_CHANGE,
+                    AppOpsManager.OP_DATA_CONNECT_CHANGE },
             new boolean[] { false,
                     true,
                     true,
@@ -184,7 +193,10 @@ public class AppOpsState {
                     false,
                     false,
                     false,
-                    false }
+                    false,
+                    true,
+                    true,
+                    true }
             );
 
     public static final OpsTemplate DEVICE_TEMPLATE = new OpsTemplate(
