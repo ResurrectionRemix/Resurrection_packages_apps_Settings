d82c5c1adf974616892dd42159445c9750383170
Srinu Jella <sjella@codeaurora.org>
2013-10-11 17:40:20 +0530

    Bluetooth: disable scan icon when BT state is off
    Usually onScanningStateChanged function should take care
    of disabling the scan icon when the scan is completed.
    
    But When the BT scan is going on and try to switch of the
    BT, as onScanningStateChanged function doesn't have the
    reference for scan icon it will not disable it, so disabling
    is taken care in BluetoothSettings on receiving BT Off state.
    
    On receiving BT Off state, disable the scan progress icon only
    when available device category is present.
    
    CRs-Fixed: 543777,596214
    Change-Id: Ib827903d4f5c80eeb6ac8a06fb1a945140dd20c9

--- src/com/android/settings/bluetooth/BluetoothSettings.java
+++ src/com/android/settings/bluetooth/BluetoothSettings.java
@@ -327,6 +327,10 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment {
                 break;
 
             case BluetoothAdapter.STATE_OFF:
+                /* reset the progress icon only when available device category present */
+                if(mAvailableDevicesCategoryIsPresent) {
+                    ((BluetoothProgressCategory)mAvailableDevicesCategory).setProgress(false);
+                }
                 messageId = R.string.bluetooth_empty_list_bluetooth_off;
                 break;
 
