9a30818398b6d121ccdb30b7305c9937332232ac
Konsta <konsta09@gmail.com>
2013-12-01 12:44:13 +0200

    settings: bring back volume key cursor control
    Framework part seems to have survived the merge.
    
    Move volume key cursor control from language & input
    to button settings.
    
    Change-Id: Ice2b266e8fdbcdf805d36d8f2e335b569e57be65
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/ButtonSettings.java

--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -43,6 +43,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_APP_SWITCH_LONG_PRESS = "hardware_keys_app_switch_long_press";
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight";
     private static final String KEY_SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+    private static final String KEY_VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
     private static final String CATEGORY_HOME = "home_key";
     private static final String CATEGORY_MENU = "menu_key";
@@ -77,7 +78,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mAssistLongPressAction;
     private ListPreference mAppSwitchPressAction;
     private ListPreference mAppSwitchLongPressAction;
+<<<<<<< HEAD
     private CheckBoxPreference mShowActionOverflow;
+=======
+    private ListPreference mVolumeKeyCursorControl;
+>>>>>>> 7f1c553... settings: bring back volume key cursor control
     private CheckBoxPreference mSwapVolumeButtons;
 
     @Override
@@ -204,6 +209,12 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             mSwapVolumeButtons = (CheckBoxPreference)
                     prefScreen.findPreference(KEY_SWAP_VOLUME_BUTTONS);
             mSwapVolumeButtons.setChecked(swapVolumeKeys > 0);
+
+            int cursorControlAction = Settings.System.getInt(resolver,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
+            mVolumeKeyCursorControl = initActionList(KEY_VOLUME_KEY_CURSOR_CONTROL,
+                    cursorControlAction);
+
             if (!res.getBoolean(R.bool.config_show_volumeRockerWake)) {
                 volumeCategory.removePreference(findPreference(Settings.System.VOLUME_WAKE_SCREEN));
             }
@@ -267,6 +278,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         } else if (preference == mAppSwitchLongPressAction) {
             handleActionListChange(mAppSwitchLongPressAction, newValue,
                     Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mVolumeKeyCursorControl) {
+            handleActionListChange(mVolumeKeyCursorControl, newValue,
+                    Settings.System.VOLUME_KEY_CURSOR_CONTROL);
             return true;
         }
 
