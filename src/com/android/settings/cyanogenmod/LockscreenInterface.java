857fe581e69f267d74e7835002128c24dd3748b7
travp624 <travp624@gmail.com>
2013-12-01 23:53:17 -0600

    Settings: Lockscreen custom wallpaper (2/2) - created a listpreference which changes the lockscreen background style (eg. Color Fill, Custom Image and default) - using the switch the preference for alpha seekbar (custom image only) or ColorPreferencePicker switches.
    *added necessary colorpicker java files and seekbar java
    
    Change-Id: I1ac1351e9a31a5827847e30113e933f2b7da219f
    
    Conflicts:
    
    	res/layout/dialog_color_picker.xml
    	res/values/cm_arrays.xml
    	res/values/cm_strings.xml
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java
    	src/com/android/settings/cyanogenmod/SeekBarPreference.java

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -16,37 +16,90 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.admin.DevicePolicyManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.Context;
+<<<<<<< HEAD
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+=======
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.net.Uri;
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
+import android.provider.MediaStore;
+import android.view.Display;
+import android.view.Window;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.colorpicker.ColorPickerPreference;
+import com.android.settings.cyanogenmod.SeekBarPreference;
 import com.android.settings.ChooseLockSettingsHelper;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+<<<<<<< HEAD
 public class LockscreenInterface extends SettingsPreferenceFragment {
+=======
+import java.io.File;
+import java.io.IOException;
+
+public class LockscreenInterface extends SettingsPreferenceFragment implements
+            Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "LockscreenInterface";
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 
     private static final String LOCKSCREEN_WIDGETS_CATEGORY = "lockscreen_widgets_category";
+<<<<<<< HEAD
     private static final String KEY_ENABLE_WIDGETS = "keyguard_enable_widgets";
     private static final String KEY_ENABLE_CAMERA = "keyguard_enable_camera";
 
     private CheckBoxPreference mEnableKeyguardWidgets;
     private CheckBoxPreference mEnableCameraWidget;
+=======
+    private static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
+    private static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
+    private static final String LOCKSCREEN_BACKGROUND_COLOR_FILL = "lockscreen_background_color_fill";
+    private static final String LOCKSCREEN_WALLPAPER_ALPHA = "lockscreen_wallpaper_alpha";
+
+    private static final int REQUEST_PICK_WALLPAPER = 201;
+    private static final int COLOR_FILL = 0;
+    private static final int CUSTOM_IMAGE = 1;
+    private static final int DEFAULT = 2;
+
+    private CheckBoxPreference mEnableKeyguardWidgets;
+    private ColorPickerPreference mLockColorFill;
+    private ListPreference mLockBackground;
+    private SeekBarPreference mWallpaperAlpha;
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 
     private ChooseLockSettingsHelper mChooseLockSettingsHelper;
     private LockPatternUtils mLockUtils;
     private DevicePolicyManager mDPM;
+<<<<<<< HEAD
+=======
+    private PreferenceCategory mLockscreenBackground;
+    private boolean mIsPrimary;
+    private File wallpaperImage;
+    private File wallpaperTemporary;
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +110,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
         mLockUtils = mChooseLockSettingsHelper.utils();
         mDPM = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
 
+<<<<<<< HEAD
         // Find categories
         PreferenceCategory widgetsCategory = (PreferenceCategory)
                 findPreference(LOCKSCREEN_WIDGETS_CATEGORY);
@@ -64,6 +118,47 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
         // Find preferences
         mEnableKeyguardWidgets = (CheckBoxPreference) findPreference(KEY_ENABLE_WIDGETS);
         mEnableCameraWidget = (CheckBoxPreference) findPreference(KEY_ENABLE_CAMERA);
+=======
+        addPreferencesFromResource(R.xml.lockscreen_interface_settings);
+        PreferenceCategory widgetsCategory = (PreferenceCategory) findPreference(LOCKSCREEN_WIDGETS_CATEGORY);
+        mLockscreenBackground = (PreferenceCategory) findPreference(LOCKSCREEN_BACKGROUND);
+
+        mLockBackground = (ListPreference) findPreference(LOCKSCREEN_BACKGROUND_STYLE);
+        mLockBackground.setOnPreferenceChangeListener(this);
+        mLockBackground.setValue(Integer.toString(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2)));
+        mLockBackground.setSummary(mLockBackground.getEntry());
+
+        mWallpaperAlpha = (SeekBarPreference) findPreference(LOCKSCREEN_WALLPAPER_ALPHA);
+        mWallpaperAlpha.setOnPreferenceChangeListener(this);
+        float mWallpaperAlphaTransparency = 1.0f;
+        try {
+            mWallpaperAlphaTransparency = Settings.System.getFloat(getContentResolver(),
+                    Settings.System.LOCKSCREEN_WALLPAPER_ALPHA);
+        } catch (Exception e) {
+            mWallpaperAlphaTransparency = 1.0f;
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.LOCKSCREEN_WALLPAPER_ALPHA, 1.0f);
+        }
+        mWallpaperAlpha.setInitValue((int) (mWallpaperAlphaTransparency * 100));
+
+        mLockColorFill = (ColorPickerPreference) findPreference(LOCKSCREEN_BACKGROUND_COLOR_FILL);
+        mLockColorFill.setOnPreferenceChangeListener(this);
+        mLockColorFill.setSummary(ColorPickerPreference.convertToARGB(
+                Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_COLOR, 0x00000000)));
+
+        // Determine which user is logged in
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+        if (mIsPrimary) {
+            // Its the primary user, show all the settings
+            if (!Utils.isPhone(getActivity())) {
+                if (widgetsCategory != null) {
+                    widgetsCategory.removePreference(
+                            findPreference(Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS));
+                }
+            }
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 
         // Remove/disable custom widgets based on device RAM and policy
         if (ActivityManager.isLowRamDeviceStatic()) {
@@ -90,6 +185,10 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
             widgetsCategory.removePreference(
                     findPreference(Settings.System.LOCKSCREEN_MAXIMIZE_WIDGETS));
         }
+
+        wallpaperImage = new File(getActivity().getFilesDir()+"/lockwallpaper");
+        wallpaperTemporary = new File(getActivity().getCacheDir()+"/lockwallpaper.tmp");
+        updateVisiblePreferences();
     }
 
     @Override
@@ -104,6 +203,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
         if (mEnableCameraWidget != null) {
             mEnableCameraWidget.setChecked(mLockUtils.getCameraEnabled());
         }
+        updateVisiblePreferences();
     }
 
     @Override
@@ -121,6 +221,7 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
+<<<<<<< HEAD
     /**
      * Checks if a specific policy is disabled by a device administrator, and disables the
      * provided preference if so.
@@ -145,5 +246,125 @@ public class LockscreenInterface extends SettingsPreferenceFragment {
     private boolean featureIsDisabled(int feature) {
         return (mDPM.getKeyguardDisabledFeatures(null) & feature) != 0;
     }
+=======
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mLockBackground) {
+            int index = mLockBackground.findIndexOfValue(objValue.toString());
+            preference.setSummary(mLockBackground.getEntries()[index]);
+            return handleBackgroundSelection(index);
+        } else if (preference == mWallpaperAlpha) {
+            float value = Float.parseFloat((String) objValue);
+            Settings.System.putFloat(getContentResolver(),
+                    Settings.System.LOCKSCREEN_WALLPAPER_ALPHA, value / 100);
+            return true;
+        } else if (preference == mLockColorFill) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(objValue)));
+            preference.setSummary(hex);
+            int value = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_COLOR, value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateVisiblePreferences() {
+        int visible = Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+        if (visible == 1) {
+            mLockscreenBackground.addPreference(mWallpaperAlpha);
+        } else {
+            mLockscreenBackground.removePreference(mWallpaperAlpha);
+        }
+        if (visible == 0) {
+            mLockscreenBackground.addPreference(mLockColorFill);
+        } else {
+            mLockscreenBackground.removePreference(mLockColorFill);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PICK_WALLPAPER) {
+            if (resultCode == Activity.RESULT_OK) {
+                if (wallpaperTemporary.exists()) {
+                    wallpaperTemporary.renameTo(wallpaperImage);
+                }
+                wallpaperImage.setReadable(true, false);
+                Toast.makeText(getActivity(), getResources().getString(R.string.
+                        background_result_successful), Toast.LENGTH_LONG).show();
+                Settings.System.putInt(getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 1);
+                updateVisiblePreferences();
+            } else {
+                if (wallpaperTemporary.exists()) {
+                    wallpaperTemporary.delete();
+                }
+                Toast.makeText(getActivity(), getResources().getString(R.string.
+                        background_result_not_successful), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean handleBackgroundSelection(int index) {
+        if (index == COLOR_FILL) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 0);
+            updateVisiblePreferences();
+            return true;
+        } else if (index == CUSTOM_IMAGE) {
+            // Used to reset the image when already set
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+            // Launches intent for user to select an image/crop it to set as background
+            final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+            intent.setType("image/*");
+            intent.putExtra("crop", "true");
+            intent.putExtra("scale", true);
+            intent.putExtra("scaleUpIfNeeded", false);
+            intent.putExtra("scaleType", 6);
+            intent.putExtra("layout_width", -1);
+            intent.putExtra("layout_height", -2);
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+
+            final Display display = getActivity().getWindowManager().getDefaultDisplay();
+            final Rect rect = new Rect();
+            final Window window = getActivity().getWindow();
+
+            window.getDecorView().getWindowVisibleDisplayFrame(rect);
+
+            int statusBarHeight = rect.top;
+            int contentViewTop = window.findViewById(Window.ID_ANDROID_CONTENT).getTop();
+            int titleBarHeight = contentViewTop - statusBarHeight;
+            boolean isPortrait = getResources().getConfiguration().orientation ==
+                    Configuration.ORIENTATION_PORTRAIT;
+
+            int width = display.getWidth();
+            int height = display.getHeight() - titleBarHeight;
+
+            intent.putExtra("aspectX", isPortrait ? width : height);
+            intent.putExtra("aspectY", isPortrait ? height : width);
+
+            try {
+                wallpaperTemporary.createNewFile();
+                wallpaperTemporary.setWritable(true, false);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(wallpaperTemporary));
+                intent.putExtra("return-data", false);
+                getActivity().startActivityFromFragment(this, intent, REQUEST_PICK_WALLPAPER);
+            } catch (IOException e) {
+            } catch (ActivityNotFoundException e) {
+            }
+        } else if (index == DEFAULT) {
+            // Sets background to default
+            Settings.System.putInt(getContentResolver(),
+                            Settings.System.LOCKSCREEN_BACKGROUND_STYLE, 2);
+            updateVisiblePreferences();
+            return true;
+        }
+        return false;
+    }
+
+    public static class DeviceAdminLockscreenReceiver extends DeviceAdminReceiver {}
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 
 }
--- src/com/android/settings/cyanogenmod/SeekBarPreference.java
+++ src/com/android/settings/cyanogenmod/SeekBarPreference.java
@@ -1,6 +1,7 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.Context;
+<<<<<<< HEAD
 import android.content.res.TypedArray;
 import android.preference.Preference;
 import android.util.AttributeSet;
@@ -229,4 +230,79 @@ public class SeekBarPreference extends Preference implements OnSeekBarChangeList
     public void setValue(int value) {
         mCurrentValue = value;
     }
+=======
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class SeekBarPreference extends Preference
+            implements OnSeekBarChangeListener {
+
+    public static int maximum = 100;
+    public static int interval = 5;
+
+    private TextView monitorBox;
+    private SeekBar bar;
+
+    int defaultValue = 60;
+
+    private OnPreferenceChangeListener changer;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View layout = View.inflate(getContext(), R.layout.slider_preference, null);
+        monitorBox = (TextView) layout.findViewById(R.id.monitor_box);
+        bar = (SeekBar) layout.findViewById(R.id.seek_bar);
+        bar.setOnSeekBarChangeListener(this);
+        bar.setProgress(defaultValue);
+        return layout;
+    }
+
+    public void setInitValue(int progress) {
+        defaultValue = progress;
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        // TODO Auto-generated method stub
+        return super.onGetDefaultValue(a, index);
+    }
+
+    @Override
+    public void setOnPreferenceChangeListener(OnPreferenceChangeListener onPreferenceChangeListener) {
+        changer = onPreferenceChangeListener;
+        super.setOnPreferenceChangeListener(onPreferenceChangeListener);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        progress = Math.round(((float) progress) / interval) * interval;
+        seekBar.setProgress(progress);
+        monitorBox.setText(progress + "%");
+        changer.onPreferenceChange(this, Integer.toString(progress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+>>>>>>> 24fa035... Settings: Lockscreen custom wallpaper (2/2)
 }
