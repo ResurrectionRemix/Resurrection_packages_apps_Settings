fbcde807928ee84c286b1c917fbcfdc81279bff1
westcripp <korukaltan@gmail.com>
2013-12-18 05:46:25 +0200

    fix build

--- src/com/android/settings/rr/Halo.java
+++ src/com/android/settings/rr/Halo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.temasek;
+package com.android.settings.rr;
 
 import android.app.ActivityManager;
 import android.app.INotificationManager;
