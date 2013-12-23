6a112ea51358c8e31b32a1404ae7ef130fdac688
westcripp <korukaltan@gmail.com>
2013-12-23 15:54:17 +0200

    Revert "not for rr"
    This reverts commit b3c421b897641fdf24c0259d7b898dff057840d9.

--- src/com/android/settings/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/quicksettings/QuickSettingsUtil.java
@@ -41,7 +41,6 @@ import static com.android.internal.util.cm.QSConstants.TILE_SCREENTIMEOUT;
 import static com.android.internal.util.cm.QSConstants.TILE_SETTINGS;
 import static com.android.internal.util.cm.QSConstants.TILE_SLEEP;
 import static com.android.internal.util.cm.QSConstants.TILE_SYNC;
-import static com.android.internal.util.cm.QSConstants.TILE_THEME;
 import static com.android.internal.util.cm.QSConstants.TILE_TORCH;
 import static com.android.internal.util.cm.QSConstants.TILE_USER;
 import static com.android.internal.util.cm.QSConstants.TILE_VOLUME;
@@ -158,9 +157,6 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_MUSIC, R.string.title_tile_music,
                 "com.android.systemui:drawable/ic_qs_media_play"));
-        registerTile(new QuickSettingsUtil.TileInfo(
-                 TILE_THEME, R.string.title_tile_theme,
-                "com.android.systemui:drawable/ic_qs_theme_manual"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
