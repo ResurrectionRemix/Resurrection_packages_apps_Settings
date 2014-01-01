a1c8045657c1e755d9408d3f7ec8422057463c5f
westcripp <korukaltan@gmail.com>
2014-01-02 00:14:18 +0200

    fix

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1201,10 +1201,6 @@ public class Settings extends PreferenceActivity
     public static class ApnSettingsActivity extends Settings { /* empty */ }
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
-<<<<<<< HEAD
     public static class SpawnActivity extends Settings { /* empty */ }
-
-=======
     public static class SystemSettingsActivity extends Settings { /* empty */ }
->>>>>>> 1ec4eb5... Re-add system settings fragment direct link.
 }
