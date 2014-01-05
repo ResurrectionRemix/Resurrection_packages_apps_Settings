46967016af7b43f6f2c752460ca914a5b271f8af
Steve Kondik <shade@chemlab.org>
2014-01-04 17:05:21 -0800

    settings: Remove ZRAM settings
     * Android has proper ZRAM support builtin now, and this code doesn't
       even work. Remove it.
    
    Change-Id: I39cb20809d5c71683bffc578057ad39363c5066e
    
    Conflicts:
    
    	res/values-ca/cm_arrays.xml
    	res/values-ca/cm_strings.xml
    	res/values-es/cm_arrays.xml
    	res/values-es/cm_strings.xml
    	res/values-hu/cm_arrays.xml
    	res/values-hu/cm_strings.xml
    	res/values-it/cm_arrays.xml
    	res/values-it/cm_strings.xml
    	res/values-lt/cm_arrays.xml
    	res/values-lt/cm_strings.xml
    	res/values-nl/cm_arrays.xml
    	res/values-nl/cm_strings.xml
    	res/values-pl/cm_arrays.xml
    	res/values-pl/cm_strings.xml
    	res/values-pt/cm_arrays.xml
    	res/values-pt/cm_strings.xml
    	res/values-ru/cm_arrays.xml
    	res/values-ru/cm_strings.xml
    	res/values-th/cm_arrays.xml
    	res/values-th/cm_strings.xml
    	res/values-zh-rCN/cm_arrays.xml
    	res/values-zh-rCN/cm_strings.xml

--- src/com/android/settings/cyanogenmod/MemoryManagement.java
+++ src/com/android/settings/cyanogenmod/MemoryManagement.java
@@ -41,19 +41,11 @@ public class MemoryManagement extends SettingsPreferenceFragment implements
 
     public static final String KSM_PREF_ENABLED = "1";
 
-    private static final String ZRAM_PREF = "pref_zram_size";
-
-    private static final String ZRAM_PERSIST_PROP = "persist.service.zram"; // was compcache
-
-    private static final String ZRAM_DEFAULT = SystemProperties.get("ro.zram.default"); // was compcache
-
     private static final String PURGEABLE_ASSETS_PREF = "pref_purgeable_assets";
 
     private static final String PURGEABLE_ASSETS_PERSIST_PROP = "persist.sys.purgeable_assets";
 
     private static final String PURGEABLE_ASSETS_DEFAULT = "0";
-
-    private ListPreference mzRAM;
 
     private CheckBoxPreference mPurgeableAssetsPref;
 
@@ -69,19 +61,8 @@ public class MemoryManagement extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
-        mzRAM = (ListPreference) prefSet.findPreference(ZRAM_PREF);
         mPurgeableAssetsPref = (CheckBoxPreference) prefSet.findPreference(PURGEABLE_ASSETS_PREF);
         mKSMPref = (CheckBoxPreference) prefSet.findPreference(KSM_PREF);
-
-        if (isSwapAvailable()) {
-            if (SystemProperties.get(ZRAM_PERSIST_PROP) == "1") {
-                SystemProperties.set(ZRAM_PERSIST_PROP, ZRAM_DEFAULT);
-            }
-            mzRAM.setValue(SystemProperties.get(ZRAM_PERSIST_PROP, ZRAM_DEFAULT));
-            mzRAM.setOnPreferenceChangeListener(this);
-        } else {
-            prefSet.removePreference(mzRAM);
-        }
 
         if (Utils.fileExists(KSM_RUN_FILE)) {
             mKSMPref.setChecked(KSM_PREF_ENABLED.equals(Utils.fileReadOneLine(KSM_RUN_FILE)));
@@ -112,23 +93,6 @@ public class MemoryManagement extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mzRAM) {
-            if (newValue != null) {
-                SystemProperties.set(ZRAM_PERSIST_PROP, (String) newValue);
-                return true;
-            }
-        }
-
         return false;
-    }
-
-    /**
-     * Check if swap support is available on the system
-     */
-    private boolean isSwapAvailable() {
-        if (swapAvailable < 0) {
-            swapAvailable = new File("/proc/swaps").exists() ? 1 : 0;
-        }
-        return swapAvailable > 0;
     }
 }
