46e4ff8512bd53afe3a4744985dadfbb2f65eb54
Ethan Chen <intervigil@gmail.com>
2014-01-17 13:40:13 -0800

    Fix reference to privacy settings
    Change-Id: I4d0c4f957778ad814b9d2ac816cff96bcba0e216

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -178,6 +178,7 @@ public class Settings extends PreferenceActivity
             R.id.interface_section,
             R.id.lock_screen_settings,
             R.id.system_settings,
+            R.id.privacy_settings_cyanogenmod,
             R.id.button_settings
     };
 
