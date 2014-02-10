3125a1871408bb522c2a93b97ba78d05f8690263
westcripp <korukaltan@gmail.com>
2014-02-10 02:37:29 +0200

    Settings LightSettingsDialog: update led on text change

--- src/com/android/settings/notificationlight/LightSettingsDialog.java
+++ src/com/android/settings/notificationlight/LightSettingsDialog.java
@@ -393,6 +393,7 @@ public class LightSettingsDialog extends AlertDialog implements
                 }
                 mColorPicker.setColor(color);
                 mNewColor.setColor(color);
+                updateLed();
                 if (mListener != null) {
                     mListener.onColorChanged(color);
                 }
