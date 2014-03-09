85cc1578246ff2150cb53173f692b67da54d08d5
westcripp <korukaltan@gmail.com>
2014-03-09 05:09:19 +0200

    Settings : Slim Notification reminder credit to slim rom

--- src/com/android/settings/cyanogenmod/NotificationDrawer.java
+++ src/com/android/settings/cyanogenmod/NotificationDrawer.java
@@ -16,11 +16,24 @@
 
 package com.android.settings.cyanogenmod;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.RingtonePreference;
 import android.provider.Settings;
+import android.text.Spannable;
+import android.widget.EditText;
+import android.os.UserHandle;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -30,9 +43,15 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
     private static final String TAG = "NotificationDrawer";
 
     private static final String UI_COLLAPSE_BEHAVIOUR = "notification_drawer_collapse_on_dismiss";
-
+    private static final String PREF_NOTI_REMINDER_SOUND =  "noti_reminder_sound";
+    private static final String PREF_NOTI_REMINDER_ENABLED = "noti_reminder_enabled";
+    private static final String PREF_NOTI_REMINDER_RINGTONE = "noti_reminder_ringtone";
+    
     private ListPreference mCollapseOnDismiss;
-
+    CheckBoxPreference mReminder;
+    ListPreference mReminderMode;
+    RingtonePreference mReminderRingtone;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +59,36 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.notification_drawer);
         PreferenceScreen prefScreen = getPreferenceScreen();
 
+        // Notification Remider
+        mReminder = (CheckBoxPreference) findPreference(PREF_NOTI_REMINDER_ENABLED);
+        mReminder.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+        mReminder.setOnPreferenceChangeListener(this);
+
+        mReminderMode = (ListPreference) findPreference(PREF_NOTI_REMINDER_SOUND);
+        int mode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_NOTIFY, 0, UserHandle.USER_CURRENT);
+        mReminderMode.setValue(String.valueOf(mode));
+        mReminderMode.setOnPreferenceChangeListener(this);
+        updateReminderModeSummary(mode);
+
+        mReminderRingtone =
+                (RingtonePreference) findPreference(PREF_NOTI_REMINDER_RINGTONE);
+        Uri ringtone = null;
+        String ringtoneString = Settings.System.getStringForUser(getContentResolver(),
+                Settings.System.REMINDER_ALERT_RINGER, UserHandle.USER_CURRENT);
+        if (ringtoneString == null) {
+            // Value not set, defaults to Default Ringtone
+            ringtone = RingtoneManager.getDefaultUri(
+                    RingtoneManager.TYPE_RINGTONE);
+        } else {
+            ringtone = Uri.parse(ringtoneString);
+        }
+        Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
+        mReminderRingtone.setSummary(alert.getTitle(getActivity()));
+        mReminderRingtone.setOnPreferenceChangeListener(this);
+        mReminderRingtone.setEnabled(mode != 0);
+        
         // Notification drawer
         int collapseBehaviour = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS,
@@ -57,6 +106,27 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
                     Settings.System.STATUS_BAR_COLLAPSE_ON_DISMISS, value);
             updateCollapseBehaviourSummary(value);
             return true;
+        } else if (preference == mReminder) {
+          Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_ENABLED,
+                    (Boolean) objValue ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mReminderMode) {
+            int mode = Integer.valueOf((String) objValue);
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_NOTIFY,
+                    mode, UserHandle.USER_CURRENT);
+            updateReminderModeSummary(mode);
+            mReminderRingtone.setEnabled(mode != 0);
+            return true;
+        } else if (preference == mReminderRingtone) {
+            Uri val = Uri.parse((String) objValue);
+            Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), val);
+            mReminderRingtone.setSummary(ringtone.getTitle(getActivity()));
+            Settings.System.putStringForUser(getContentResolver(),
+                    Settings.System.REMINDER_ALERT_RINGER,
+                    val.toString(), UserHandle.USER_CURRENT);
+            return true;
         }
 
         return false;
@@ -66,5 +136,21 @@ public class NotificationDrawer extends SettingsPreferenceFragment implements
         String[] summaries = getResources().getStringArray(
                 R.array.notification_drawer_collapse_on_dismiss_summaries);
         mCollapseOnDismiss.setSummary(summaries[setting]);
+    }
+
+    private void updateReminderModeSummary(int value) {
+        int resId;
+        switch (value) {
+            case 1:
+                resId = R.string.enabled;
+                break;
+            case 2:
+                resId = R.string.noti_reminder_sound_looping;
+                break;
+            default:
+                resId = R.string.disabled;
+                break;
+        }
+        mReminderMode.setSummary(getResources().getString(resId));
     }
 }
