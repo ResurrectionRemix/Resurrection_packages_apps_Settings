9001b48be6e1429eb2a240f1bebaf3772fc8e31e
Juan Ezquerro LLanes <arrase@gamil.com>
2014-03-14 01:39:09 +0100

    [1/2] OmniGears: Active Display add shake event
    PS11: Fix strings
    
    - correct dependency option
    - added option to bypass Quiet Hours (default now is not , that is 'privacy is first' decision)
    - added option to configure time between shakes
    - added option to set disable timeout
    - added option to run like "PA PEEK"
    
    PS12:
    -disable default event with the other shake settings
    -simplify titles
    
    Change-Id: Ibdf35b1e323fa15bf2e5da9ca8225533cd4c42c4
    
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/active_display_settings.xml

--- src/com/android/settings/fragments/ls/ActiveDisplaySettings.java
+++ src/com/android/settings/fragments/ls/ActiveDisplaySettings.java
@@ -43,8 +43,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.util.Log;
-
 public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
     private static final String TAG = "ActiveDisplaySettings";
@@ -62,6 +60,9 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_TIMEOUT = "ad_timeout";
     private static final String KEY_THRESHOLD = "ad_threshold";
     private static final String KEY_TURNOFF_MODE = "ad_turnoff_mode";
+    private static final String KEY_SHAKE_THRESHOLD = "ad_shake_threshold";
+    private static final String KEY_SHAKE_LONGTHRESHOLD = "ad_shake_long_threshold";
+    private static final String KEY_SHAKE_TIMEOUT = "ad_shake_timeout";
 
     private ContentResolver mResolver;
     private Context mContext;
@@ -75,13 +76,15 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mTurnOffModePref;
     private SeekBarPreference mBrightnessLevel;
     private SeekBarPreference mAnnoyingNotification;
+    private SeekBarPreference mShakeThreshold;
+    private SeekBarPreference mShakeLongThreshold;
+    private SeekBarPreference mShakeTimeout;
     private ListPreference mDisplayTimeout;
     private ListPreference mPocketModePref;
     private ListPreference mProximityThreshold;
     private ListPreference mRedisplayPref;
     private int mMinimumBacklight;
     private int mMaximumBacklight;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -149,6 +152,21 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVE_DISPLAY_ANNOYING, 0));
         mAnnoyingNotification.setOnPreferenceChangeListener(this);
 
+        mShakeThreshold = (SeekBarPreference) prefSet.findPreference(KEY_SHAKE_THRESHOLD);
+        mShakeThreshold.setValue(Settings.System.getInt(mResolver,
+                Settings.System.ACTIVE_DISPLAY_SHAKE_THRESHOLD, 10));
+        mShakeThreshold.setOnPreferenceChangeListener(this);
+
+        mShakeLongThreshold = (SeekBarPreference) prefSet.findPreference(KEY_SHAKE_LONGTHRESHOLD);
+        mShakeLongThreshold.setValue(Settings.System.getInt(mResolver,
+                Settings.System.ACTIVE_DISPLAY_SHAKE_LONGTHRESHOLD, 2));
+        mShakeLongThreshold.setOnPreferenceChangeListener(this);
+
+        mShakeTimeout = (SeekBarPreference) prefSet.findPreference(KEY_SHAKE_TIMEOUT);
+        mShakeTimeout.setValue(Settings.System.getInt(mResolver,
+                Settings.System.ACTIVE_DISPLAY_SHAKE_TIMEOUT, 10));
+        mShakeTimeout.setOnPreferenceChangeListener(this);
+
         mExcludedAppsPref = (AppMultiSelectListPreference) prefSet.findPreference(KEY_EXCLUDED_APPS);
         Set<String> excludedApps = getExcludedApps();
         if (excludedApps != null) {
@@ -166,6 +184,7 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
         mShowAmPmPref = (CheckBoxPreference) prefSet.findPreference(KEY_SHOW_AMPM);
         mShowAmPmPref.setChecked((Settings.System.getInt(mResolver,
                 Settings.System.ACTIVE_DISPLAY_SHOW_AMPM, 0) == 1));
+        mShowAmPmPref.setEnabled(!is24Hour());
 
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mMinimumBacklight = pm.getMinimumScreenBrightnessSetting();
@@ -176,7 +195,16 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
                 Settings.System.ACTIVE_DISPLAY_BRIGHTNESS, mMaximumBacklight);
         int realBrightness =  (int)(((float)brightness / (float)mMaximumBacklight) * 100);
         mBrightnessLevel.setValue(realBrightness);
-        mBrightnessLevel.setOnPreferenceChangeListener(this); 
+        mBrightnessLevel.setOnPreferenceChangeListener(this);
+
+        try {
+            if (Settings.System.getInt(mResolver,
+                    Settings.System.SCREEN_BRIGHTNESS_MODE) == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC) {
+                mBrightnessLevel.setEnabled(false);
+                mBrightnessLevel.setSummary(R.string.status_bar_toggle_info);
+            }
+        } catch (SettingNotFoundException e) {
+        }
 
         mDisplayTimeout = (ListPreference) prefSet.findPreference(KEY_TIMEOUT);
         timeout = Settings.System.getLong(mResolver,
@@ -217,6 +245,21 @@ public class ActiveDisplaySettings extends SettingsPreferenceFragment implements
             int annoying = ((Integer)newValue).intValue();
             Settings.System.putInt(mResolver,
                     Settings.System.ACTIVE_DISPLAY_ANNOYING, annoying);
+            return true;
+        } else if (preference == mShakeThreshold) {
+            int threshold = ((Integer)newValue).intValue();
+            Settings.System.putInt(mResolver,
+                    Settings.System.ACTIVE_DISPLAY_SHAKE_THRESHOLD, threshold);
+            return true;
+        } else if (preference == mShakeLongThreshold) {
+            int longThreshold = ((Integer)newValue).intValue();
+            Settings.System.putInt(mResolver,
+                    Settings.System.ACTIVE_DISPLAY_SHAKE_LONGTHRESHOLD, longThreshold);
+            return true;
+        } else if (preference == mShakeTimeout) {
+            int timeout = ((Integer)newValue).intValue();
+            Settings.System.putInt(mResolver,
+                    Settings.System.ACTIVE_DISPLAY_SHAKE_TIMEOUT, timeout);
             return true;
         } else if (preference == mBrightnessLevel) {
             int brightness = ((Integer)newValue).intValue();
