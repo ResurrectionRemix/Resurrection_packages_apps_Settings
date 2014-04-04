ab511097cd37a43cb015a0be3e7e8745bad01229
Chris Soyars <ctso@ctso.me>
2014-04-04 11:26:53 -0700

    Use SSL for submitting stats
    Change-Id: If830c05ee121d3951ffcf34cd3bbd499c5107f1d

--- src/com/android/settings/cmstats/ReportingService.java
+++ src/com/android/settings/cmstats/ReportingService.java
@@ -103,7 +103,7 @@ public class ReportingService extends Service {
 
             // report to the cmstats service
             HttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost("http://stats.cyanogenmod.org/submit");
+            HttpPost httpPost = new HttpPost("https://stats.cyanogenmod.org/submit");
             boolean success = false;
 
             try {
