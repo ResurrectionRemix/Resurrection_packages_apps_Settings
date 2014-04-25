34e60c736ec8e0ca5f31ffe45abb86cd629b9fc3
Roman Birg <roman@cyngn.com>
2014-04-24 22:27:07 -0700

    Settings: move keyguard wallpaper to system (2/2)
    Change-Id: Id3bb66aa78c2e3dbd1170e74e33fcd160e5a70bb
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	res/xml/theme_settings.xml
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -16,43 +16,23 @@
 
 package com.android.settings.cyanogenmod;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
-import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.hardware.Camera;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
-import android.provider.MediaStore;
 import android.provider.Settings;
-import android.view.Display;
-import android.widget.Toast;
 
-import com.android.internal.util.cm.LockscreenBackgroundUtil;
 import com.android.internal.widget.LockPatternUtils;
-import com.android.settings.R;
 import com.android.settings.ChooseLockSettingsHelper;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
@@ -69,25 +49,17 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     private static final String PREF_LOCKSCREEN_TORCH = "lockscreen_torch";
     private static final String KEY_LOCKSCREEN_MODLOCK_ENABLED = "lockscreen_modlock_enabled";
     
-    private static final String LOCKSCREEN_BACKGROUND_STYLE = "lockscreen_background_style";
-
-    private static final String LOCKSCREEN_WALLPAPER_TEMP_NAME = ".lockwallpaper";
-
-    private static final int REQUEST_PICK_WALLPAPER = 201;
 
     private CheckBoxPreference mEnableKeyguardWidgets;
     private CheckBoxPreference mEnableCameraWidget;
     private CheckBoxPreference mQuickUnlock;
     private CheckBoxPreference mGlowpadTorch;
     private CheckBoxPreference mEnableModLock;
-    private ListPreference mLockBackground;
     private ListPreference mBatteryStatus;
 
     private ChooseLockSettingsHelper mChooseLockSettingsHelper;
     private LockPatternUtils mLockUtils;
     private DevicePolicyManager mDPM;
-
-    private File mTempWallpaper, mWallpaper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -176,13 +148,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
 		mQuickUnlock.setChecked(Settings.System.getInt(getContentResolver(),
 		Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
         }
-
-	    // Lockscreen Background
-        mLockBackground = (ListPreference) findPreference(LOCKSCREEN_BACKGROUND_STYLE);
-        mLockBackground.setOnPreferenceChangeListener(this);
-
-        mTempWallpaper = getActivity().getFileStreamPath(LOCKSCREEN_WALLPAPER_TEMP_NAME);
-        mWallpaper = LockscreenBackgroundUtil.getWallpaperFile(getActivity());
     }
 
     @Override
@@ -212,7 +177,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
                     
         if (mEnableCameraWidget != null) {
             mEnableCameraWidget.setChecked(mLockUtils.getCameraEnabled());
-
         }
         
 	    if (mQuickUnlock != null) {
@@ -220,12 +184,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
                   Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
         }
 
-        updateBackgroundPreference();
-    }
-
-    private void updateBackgroundPreference() {
-        int lockVal = LockscreenBackgroundUtil.getLockscreenStyle(getActivity());
-        mLockBackground.setValue(Integer.toString(lockVal));
     }
 
     @Override
@@ -266,9 +224,6 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
             Settings.System.putInt(cr, Settings.System.LOCKSCREEN_MODLOCK_ENABLED,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mLockBackground) {
-            int index = mLockBackground.findIndexOfValue((String) objValue);
-            handleBackgroundSelection(index);
         }
         return false;
     }
@@ -315,159 +270,4 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         return (mDPM.getKeyguardDisabledFeatures(null) & feature) != 0;
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_PICK_WALLPAPER) {
-            if (resultCode == Activity.RESULT_OK) {
-                Uri uri = data != null ? data.getData() : null;
-                if (uri == null) {
-                    uri = Uri.fromFile(mTempWallpaper);
-                }
-                new SaveUserWallpaperTask(getActivity().getApplicationContext()).execute(uri);
-            } else {
-                toastLockscreenWallpaperStatus(getActivity(), false);
-            }
-        }
-    }
-
-    private Bitmap getBitmapFromUri(Uri uri) {
-        ParcelFileDescriptor parcelFileDescriptor = null;
-        try {
-            parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
-            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-            return image;
-        } catch (IOException e) {
-        } finally {
-            if (parcelFileDescriptor != null) {
-                try {
-                    parcelFileDescriptor.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-        return null;
-    }
-
-    private void handleBackgroundSelection(int index) {
-        if (index == LockscreenBackgroundUtil.LOCKSCREEN_STYLE_IMAGE) {
-            // Launches intent for user to select an image/crop it to set as background
-            final Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
-            intent.setType("image/*");
-            intent.putExtra("crop", "true");
-            intent.putExtra("scale", true);
-            intent.putExtra("scaleUpIfNeeded", false);
-            intent.putExtra("scaleType", 6);
-            intent.putExtra("layout_width", -1);
-            intent.putExtra("layout_height", -2);
-            intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
-
-            final Display display = getActivity().getWindowManager().getDefaultDisplay();
-            boolean isPortrait = getResources().getConfiguration().orientation ==
-                    Configuration.ORIENTATION_PORTRAIT;
-
-            Point screenDimension = new Point();
-            display.getSize(screenDimension);
-            int width = screenDimension.x;
-            int height = screenDimension.y;
-
-            intent.putExtra("aspectX", isPortrait ? width : height);
-            intent.putExtra("aspectY", isPortrait ? height : width);
-
-            try {
-                mTempWallpaper.createNewFile();
-                mTempWallpaper.setWritable(true, false);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mTempWallpaper));
-                intent.putExtra("return-data", false);
-                getActivity().startActivityFromFragment(this, intent, REQUEST_PICK_WALLPAPER);
-            } catch (IOException e) {
-                toastLockscreenWallpaperStatus(getActivity(), false);
-            } catch (ActivityNotFoundException e) {
-                toastLockscreenWallpaperStatus(getActivity(), false);
-            }
-        } else if (index == LockscreenBackgroundUtil.LOCKSCREEN_STYLE_DEFAULT) {
-            // Sets background to default
-            Settings.System.putInt(getContentResolver(),
-                            Settings.System.LOCKSCREEN_BACKGROUND_STYLE, LockscreenBackgroundUtil.LOCKSCREEN_STYLE_DEFAULT);
-            if (mWallpaper.exists()) {
-                mWallpaper.delete();
-            }
-            updateKeyguardWallpaper(getActivity());
-            updateBackgroundPreference();
-        }
-    }
-
-    private static void toastLockscreenWallpaperStatus(Context context, boolean success) {
-        Toast.makeText(context, context.getResources().getString(
-                success ? R.string.background_result_successful
-                        : R.string.background_result_not_successful),
-                Toast.LENGTH_LONG).show();
-    }
-
-    private static void updateKeyguardWallpaper(Context context) {
-        context.sendBroadcast(new Intent(Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED));
-    }
-
-    private class SaveUserWallpaperTask extends AsyncTask<Uri, Void, Boolean> {
-
-        private Toast mToast;
-        Context mContext;
-
-        public SaveUserWallpaperTask(Context ctx) {
-            mContext = ctx;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            mToast = Toast.makeText(getActivity(), R.string.setting_lockscreen_background,
-                    Toast.LENGTH_LONG);
-            mToast.show();
-        }
-
-        @Override
-        protected Boolean doInBackground(Uri... params) {
-            if (getActivity().isFinishing()) {
-                return false;
-            }
-            FileOutputStream out = null;
-            try {
-                Bitmap wallpaper = getBitmapFromUri(params[0]);
-                if (wallpaper == null) {
-                    return false;
-                }
-                mWallpaper.createNewFile();
-                mWallpaper.setReadable(true, false);
-                out = new FileOutputStream(mWallpaper);
-                wallpaper.compress(Bitmap.CompressFormat.JPEG, 85, out);
-
-                if (mTempWallpaper.exists()) {
-                    mTempWallpaper.delete();
-                }
-                return true;
-            } catch (IOException e) {
-            } finally {
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                    }
-                }
-            }
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean result) {
-            mToast.cancel();
-            toastLockscreenWallpaperStatus(mContext, result);
-            if (result) {
-                Settings.System.putInt(getContentResolver(),
-                        Settings.System.LOCKSCREEN_BACKGROUND_STYLE,
-                        LockscreenBackgroundUtil.LOCKSCREEN_STYLE_IMAGE);
-                updateKeyguardWallpaper(mContext);
-                if (!isDetached()) {
-                    updateBackgroundPreference();
-                }
-            }
-        }
-    }
 }
