512908c09f1d16f39dd42e1c4fc4e55356b998cd
Christopher R. Palmer <crpalmer@gmail.com>
2014-05-09 15:54:28 -0400

    Themes: List ThemeSettings as a valid fragment
    This fixes a force close in Settings in tablet mode.
    
    Change-Id: I6542feba0d0fa8491ab9e092a90709d1034ffca8
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -385,7 +385,8 @@ public class Settings extends PreferenceActivity
         PerformanceSettings.class.getName(),
         PolicyNativeFragment.class.getName(),
         com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-        com.android.settings.cyanogenmod.QuietHours.class.getName()
+        com.android.settings.cyanogenmod.QuietHours.class.getName(),
+        ThemeSettings.class.getName()
     };
 
     @Override
