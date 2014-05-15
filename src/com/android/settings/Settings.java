a62ceb29b70a9e5dd609756ccb45b8d7f4ceb684
Raj Yengisetty <rajesh@cyngn.com>
2014-05-14 18:20:34 -0700

    Settings: Allow access for external apps to launch ThemeSettings Fragment directly
    Change-Id: I93b676d3eb570c7064eeb4217c8e420a186c8e32

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1265,4 +1265,5 @@ public class Settings extends PreferenceActivity
     public static class ASSRamBarActivity extends Settings { /* empty */ } 
     public static class SystemSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
+    public static class ThemeSettingsActivity extends Settings { /* empty */ }
 }
