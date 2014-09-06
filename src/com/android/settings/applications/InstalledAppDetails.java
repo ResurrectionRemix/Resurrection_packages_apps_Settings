020e26676df74cb25707efff5286a910569d3f04
Adam77Root <adam77root@gmail.com>
2014-09-06 18:13:18 +0200

    Open app when clicking on icon in App Info screen
    Based on Xposed module Shortcut in App Info:
    http://repo.xposed.info/module/com.mohammadag.shortcutinappinfo
    
    There is no vibration as in the module as I didn't find it necessary
    
    Change-Id: I07b3f947aaaa13702adba3b4e78bb780fe861baf

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -639,6 +639,21 @@ public class InstalledAppDetails extends Fragment
         ImageView icon = (ImageView) appSnippet.findViewById(R.id.app_icon);
         mState.ensureIcon(mAppEntry);
         icon.setImageDrawable(mAppEntry.icon);
+
+        // Clicking on application icon opens application.
+        final String finalPackageName = pkgInfo.packageName;
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PackageManager pm = v.getContext().getPackageManager();
+                Intent intent = pm.getLaunchIntentForPackage(finalPackageName);
+                if (intent == null)
+                    return;
+
+                v.getContext().startActivity(intent);
+            }
+        });
+
         // Set application name.
         TextView label = (TextView) appSnippet.findViewById(R.id.app_name);
         label.setText(mAppEntry.label);
