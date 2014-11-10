64ffa90d252a225265d3c9ef810bf857aa0acd44
Rohit Singh <singhr@codeaurora.org>
2014-11-10 20:11:18 +0530

    Bluetooth: Enable Page and Inquiry Scan.
    When Bluetooth Setting is in foreground enable both
    Page Scan and Inquiry Scan.
    Changes made to make device both in connectable and
    discoverable if user is in Bluetooth setting menu.
    CRs-Fixed: 753990
    
    Change-Id: I2d72ce3192f6b7fa9eb6beb5a175a3e732dac5a8

--- src/com/android/settings/bluetooth/BluetoothEnabler.java
+++ src/com/android/settings/bluetooth/BluetoothEnabler.java
@@ -134,6 +134,8 @@ public final class BluetoothEnabler implements SwitchBar.OnSwitchChangeListener 
                 setChecked(true);
                 mSwitch.setEnabled(true);
                 updateSearchIndex(true);
+                // enable page and inquiry scan
+                mLocalAdapter.setScanMode(BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE);
                 break;
             case BluetoothAdapter.STATE_TURNING_OFF:
                 mSwitch.setEnabled(false);
