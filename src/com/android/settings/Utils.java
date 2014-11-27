713840e9cba691fecced0a80673ce6c4c219a0b9
Hu Wang <huw@codeaurora.org>
2014-11-27 18:08:19 +0800

    Bluetooth: Add Manifest configure to fix settings crash issue.
    Settings is crashed when press back key after rotating screen when
    Bluetooth profile activity is opened. Add manifest configure to
    enable screen orientation feature.
    
    Change-Id: I4550b18383d817d1a2f46fbeebda8a3bdab580c6
    CRs-Fixed: 762437

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -76,6 +76,7 @@ import android.widget.TabWidget;
 import com.android.internal.util.ImageUtils;
 import com.android.internal.util.UserIcons;
 import com.android.settings.UserSpinnerAdapter.UserDetails;
+import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardTile;
 import com.android.settings.drawable.CircleFramedDrawable;
@@ -691,7 +692,11 @@ public final class Utils {
     public static Intent onBuildStartFragmentIntent(Context context, String fragmentName,
             Bundle args, int titleResId, CharSequence title, boolean isShortcut) {
         Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.setClass(context, SubSettings.class);
+        if (BluetoothSettings.class.getName().equals(fragmentName)) {
+            intent.setClass(context, SubSettings.BluetoothSubSettings.class);
+        } else {
+            intent.setClass(context, SubSettings.class);
+        }
         intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT, fragmentName);
         intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS, args);
         intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_TITLE_RESID, titleResId);
