1f7e44018b02f505ddd3a53faa048b60b8687ed5
Danny Baumann <dannybaumann@web.de>
2014-11-30 23:09:29 -0600

    Settings: allow home button to wake device [2/2]
    Change-Id: I5c887422636f8bc51cb324339b3cdcac7af54400

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -151,6 +151,10 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         }
 
         if (hasHomeKey) {
+            if (!res.getBoolean(R.bool.config_show_homeWake)) {
+                homeCategory.removePreference(findPreference(Settings.System.HOME_WAKE_SCREEN));
+            }
+
             int defaultLongPressAction = res.getInteger(
                     com.android.internal.R.integer.config_longPressOnHomeBehavior);
             if (defaultLongPressAction < ACTION_NOTHING ||
