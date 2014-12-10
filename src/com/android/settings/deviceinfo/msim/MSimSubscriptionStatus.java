aa2f9ecdcacf22bbf562731b2299657f1229fede
Steve Kondik <steve@cyngn.com>
2014-12-09 21:37:35 -0800

    settings: Fix warnings
    Change-Id: I51fae71c7c4f820aeee0479d89ab873993c02a7a

--- src/com/android/settings/deviceinfo/msim/MSimSubscriptionStatus.java
+++ src/com/android/settings/deviceinfo/msim/MSimSubscriptionStatus.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.deviceinfo;
+package com.android.settings.deviceinfo.msim;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
