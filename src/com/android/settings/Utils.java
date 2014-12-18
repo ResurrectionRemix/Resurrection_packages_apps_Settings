3ed47b156e6eb4a3d069b05b0863ec9823f86480
Michael Bestas <mikeioannina@gmail.com>
2014-12-18 04:26:11 +0200

    Cleanup button wake settings (1/2)
    Change-Id: I972251384eee15cf9e4915cd82d189ed7ef3050d

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -629,7 +629,9 @@ public final class Utils {
 
     /* returns whether the device has volume rocker or not. */
     public static boolean hasVolumeRocker(Context context) {
-        return context.getResources().getBoolean(R.bool.has_volume_rocker);
+        final int deviceKeys = context.getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+        return (deviceKeys & ButtonSettings.KEY_MASK_VOLUME) != 0;
     }
 
     /**
