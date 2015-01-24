cc90e8ef1c891fb876703a6b8222a937345fb2ee
Danesh M <daneshm90@gmail.com>
2015-01-23 18:49:56 -0800

    Settings : Add screen timeout
    Change-Id: I146be2a154f701d3f33421cddb177f0511dea2fb
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/qs/QSTileHolder.java

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -133,6 +133,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_lock_screen_on";
                 stringId = R.string.qs_tile_lockscreen;
                 break;
+            case QSConstants.TILE_SCREEN_TIMEOUT:
+                resourceName = "ic_qs_screen_timeout_vector";
+                stringId = R.string.qs_tile_screen_timeout;
+                break;
             default:
                 return null;
         }
