474151cfb0dbb13ca5094dfc1a3037ef17962e36
BestPig <bestpig82@gmail.com>
2015-02-01 00:51:10 +0100

    Settings: add USB Tether tile
    Change-Id: I8284663b4e8d01a9409dc3f7b45c664bbe7acafa

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -185,6 +185,10 @@ public class QSTileHolder {
                 resourceName ="ic_livedisplay_day";
                 stringId = R.string.live_display_title;
                 break;
+            case QSConstants.TILE_USB_TETHER:
+                resourceName = "ic_qs_usb_tether_on";
+                stringId = R.string.qs_tile_usb_tether;
+                break;
             default:
                 return null;
         }
