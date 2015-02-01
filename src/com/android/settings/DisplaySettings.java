be1abb80a1ef05538c0006e1e6690b86587055bc
westcripp <korukaltan@gmail.com>
2015-02-01 19:18:15 +0200

    Settings: Add ability to control default value of DOZE_ENABLED Devices can enable the doze feature. Devices without AMODLED screens can use it but it will use then of course more battery. This devices should disable this f$ by default and let the user decide to turn it on.
    to change the default behaviour add into device overlay
    
    config_doze_enabled_by_default and set it to false
    
    by  kufikugel
    
    Change-Id: I1138d0fb416129507fc7d5478e19cdf3aa844026

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -525,7 +525,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
         // Update doze if it is available.
         if (mDozePreference != null) {
-            int value = Settings.Secure.getInt(getContentResolver(), DOZE_ENABLED, 1);
+            int value = Settings.Secure.getInt(getContentResolver(), DOZE_ENABLED,
+                    getActivity().getResources().getBoolean(
+                    com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0);
             mDozePreference.setChecked(value != 0);
         }
     }
