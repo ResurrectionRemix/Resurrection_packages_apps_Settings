4f5208a7bb32b9730dd9aae378fbc04de2e0c790
eyosen <abittin@gmail.com>
2015-02-05 14:21:01 +0200

    QS: add Screenrecord tile (2/2)
    Change-Id: Ie2a283fedbffe4f94ccd448421eabc879225868a
    
    Conflicts:
    	res/values/strings_aicp.xml

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -144,6 +144,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screenshot";
                 stringId = R.string.qs_screenshot_tile;
                 break;
+            case QSConstants.TILE_SCREENRECORD:
+                resourceName = "ic_qs_screenrecord";
+                stringId = R.string.qs_screenrecord_tile;
+                break;
             case QSConstants.TILE_SYNC:
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_sync_tile;
