beba5da80586c1e685c08753dcc0be54ffac9ff4
westcripp <korukaltan@gmail.com>
2015-02-12 04:35:23 +0200

    Settings: Navigation bar button color (2/2) originally commited by lichti1901
    Change-Id: Ief644ae317eb3ba938982c47c7914c66d1f3739f

--- src/com/android/settings/rr/InterfaceSettings.java
+++ src/com/android/settings/rr/InterfaceSettings.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2013 The CyanogenMod project
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,30 +16,72 @@
 
 package com.android.settings.rr;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
+import android.os.RemoteException;
+import android.os.Handler;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import android.provider.Settings.SettingNotFoundException;
 
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.settings.search.Indexable;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import java.util.ArrayList;
+import java.util.List;
 
-public class InterfaceSettings extends SettingsPreferenceFragment {
 
+public class InterfaceSettings extends SettingsPreferenceFragment implements
+              Preference.OnPreferenceChangeListener, Indexable {
+	
+private static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
+private ColorPickerPreference mNavbarButtonTint;
+        
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.rr_interface_settings);
+        
+        // Navigation bar button color
+        mNavbarButtonTint = (ColorPickerPreference) findPreference(NAVIGATION_BAR_TINT);
+        mNavbarButtonTint.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_TINT, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mNavbarButtonTint.setSummary(hexColor);
+        mNavbarButtonTint.setNewPreviewColor(intColor);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		    if (preference == mNavbarButtonTint) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_TINT, intHex);
+            return true;
+        }
         return false;
     }
 
