32454d5f076511486cba7fa2bb4d8ab9aed3f966
Roman Birg <roman@cyngn.com>
2015-02-19 15:20:23 -0800

    Settings: fix Vibrate on touch preference not working
    It was moved out of Other sounds, and never got hooked back up.
    
    Change-Id: Ibdc27be82d1fdad27e28d3dc20b245c6e81a125f
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -43,6 +43,7 @@ import android.os.Vibrator;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.preference.SeekBarVolumizer;
 import android.preference.SwitchPreference;
 import android.preference.TwoStatePreference;
@@ -61,6 +62,7 @@ import com.android.settings.Utils;
 import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+import cyanogenmod.hardware.CMHardwareManager;
 import cyanogenmod.providers.CMSettings;
 
 import java.util.ArrayList;
@@ -87,6 +89,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
     private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
     private static final String KEY_INCREASING_RING_VOLUME = "increasing_ring_volume";
     private static final String KEY_VIBRATION_INTENSITY = "vibration_intensity";
+    private static final String KEY_VIBRATE_ON_TOUCH = "vibrate_on_touch";
     private static final String KEY_ZEN_ACCESS = "manage_zen_access";
     private static final String KEY_ZEN_MODE = "zen_mode";
 
@@ -176,8 +179,9 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
             volumes.removePreference(volumes.findPreference(KEY_RING_VOLUME));
         }
 
-        if (!VibratorIntensity.isSupported(mContext)) {
-            removePreference(KEY_VIBRATION_INTENSITY);
+        CMHardwareManager hardware = CMHardwareManager.getInstance(mContext);
+        if (!hardware.isSupported(CMHardwareManager.FEATURE_VIBRATOR)) {
+            vibrate.removePreference(vibrate.findPreference(KEY_VIBRATION_INTENSITY));
         }
 
         initRingtones(sounds);
@@ -220,6 +224,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         mVolumeCallback.stopSample();
         mSettingsObserver.register(false);
         mReceiver.register(false);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     // === Volumes ===
@@ -584,6 +593,12 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
+        private boolean mHasVibratorIntensity;
+
+        @Override
+        public void prepare() {
+            super.prepare();
+        }
 
         public List<SearchIndexableResource> getXmlResourcesToIndex(
                 Context context, boolean enabled) {
@@ -602,6 +617,15 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
                 rt.add(KEY_WIFI_DISPLAY);
                 rt.add(KEY_VIBRATE_WHEN_RINGING);
             }
+            Vibrator vib = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+            if (vib == null || !vib.hasVibrator()) {
+                rt.add(KEY_VIBRATE);
+            }
+            CMHardwareManager hardware = CMHardwareManager.getInstance(context);
+            if (!hardware.isSupported(CMHardwareManager.FEATURE_VIBRATOR)) {
+                rt.add(KEY_VIBRATION_INTENSITY);
+            }
+
             return rt;
         }
     };
