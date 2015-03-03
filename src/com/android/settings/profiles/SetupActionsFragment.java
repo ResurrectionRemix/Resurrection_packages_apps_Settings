379fb2e2307c201dcfe99fa217941ae1bd32a225
Roman Birg <roman@cyngn.com>
2015-03-03 15:21:54 -0800

    Settings: always allow profile app groups to be linked/unlinked
    Change-Id: I271d409f925805d26c918d156e8ae41253cc08b1
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/profiles/SetupActionsFragment.java
+++ src/com/android/settings/profiles/SetupActionsFragment.java
@@ -229,11 +229,11 @@ public class SetupActionsFragment extends SettingsPreferenceFragment
                     mItems.add(new AppGroupItem(mProfile, mProfile.getDefaultGroup()));
                 }
             }
-            if (groupsAdded > 0) {
-                // add dummy "add/remove app groups" entry
+            if (mProfileManager.getNotificationGroups().length > 0) {
+                // if there are notification groups available, allow them to be configured
                 mItems.add(new AppGroupItem());
-            } else {
-                // remove the header since there are no options
+            } else if (groupsAdded == 0) {
+                // no notification groups available at all, nothing to add/remove
                 mItems.remove(mItems.get(mItems.size() - 1));
             }
         }
