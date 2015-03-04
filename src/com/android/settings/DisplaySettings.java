5139e2acd6cab19b678b3465750451e42eed005b
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-04 10:02:35 +0200

    Display settings: show enabled/disabled summary for ambient display
    Change-Id: Iff86514cacfff4e7119e7fd0150ca98e335ad31f

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -455,6 +455,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
         final ContentResolver resolver = getContentResolver();
 
+        boolean dozeEnabled = Settings.Secure.getInt(
+                getContentResolver(), Settings.Secure.DOZE_ENABLED, 1) != 0;
+        if (mDozeFragement != null) {
+            mDozeFragement.setSummary(dozeEnabled
+                    ? R.string.summary_doze_enabled : R.string.summary_doze_disabled);
+        }
+
         // Display rotation observer
         resolver.registerContentObserver(
                 Settings.System.getUriFor(Settings.System.ACCELEROMETER_ROTATION), true,
