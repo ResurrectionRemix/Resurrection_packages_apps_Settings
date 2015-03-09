bcb815282c30f07fa8efa001c7b4b660c57dc7e3
cretin45 <cretin45@gmail.com>
2015-03-09 14:47:25 -0700

    Settings: Override default activity transitions for OOBE wifi
    Change-Id: I137bafaa173d6848d1ae9332f348cddb16ab868c

--- src/com/android/settings/wifi/WifiSetupActivity.java
+++ src/com/android/settings/wifi/WifiSetupActivity.java
@@ -167,6 +167,12 @@ public class WifiSetupActivity extends WifiPickerActivity
     }
 
     @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.translucent_enter, R.anim.translucent_exit);
+    }
+
+    @Override
     protected void onApplyThemeResource(Resources.Theme theme, int resid, boolean first) {
         String themeName = getIntent().getStringExtra(EXTRA_THEME);
         if (THEME_HOLO_LIGHT.equalsIgnoreCase(themeName) ||
