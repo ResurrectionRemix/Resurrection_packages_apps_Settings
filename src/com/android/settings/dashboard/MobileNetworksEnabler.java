a3a4c0837c0de4c96760fcf6e8e4fd7893efda39
Roman Birg <roman@cyngn.com>
2015-03-23 18:21:36 -0700

    Settings: fix mobile network switch being unchecked
    Sometimes when connected to Wifi, the mobile data switch gets checked
    off, even though mobile data is enabled.
    
    Change-Id: I4d28366f9c7aa63d20b215d39bdcb04f7b6e83dc
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/dashboard/MobileNetworksEnabler.java
+++ src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,29 +83,8 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        switch (mTelephonyManager.getDataState()) {
-            case TelephonyManager.DATA_CONNECTED:
-            case TelephonyManager.DATA_SUSPENDED:
-                setEnabled(true);
-                setChecked(true);
-                break;
-
-            case TelephonyManager.DATA_CONNECTING:
-                setChecked(true);
-                setEnabled(false);
-                break;
-
-            case TelephonyManager.DATA_DISCONNECTED:
-                setEnabled(true);
-                setChecked(false);
-                break;
-
-            default:
-            case TelephonyManager.DATA_UNKNOWN:
-                setEnabled(false);
-                setChecked(false);
-                break;
-        }
+        setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+        setChecked(mTelephonyManager.getDataEnabled());
     }
 
     @Override
