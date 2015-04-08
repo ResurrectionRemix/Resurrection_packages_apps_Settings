564e491f7d476d597a6bf443b421b873a5af9bfa
cretin45 <cretin45@gmail.com>
2015-04-08 16:30:12 -0700

    Settings: Restore proper ringtone for msim
    Change-Id: I4dc5a44e7c820cfbba8dcb10afe624e828f2eab2

--- src/com/android/settings/DefaultRingtonePreference.java
+++ src/com/android/settings/DefaultRingtonePreference.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.preference.RingtonePreference;
+import android.telephony.TelephonyManager;
 import android.util.AttributeSet;
 import android.util.Log;
 
@@ -50,7 +51,15 @@ public class DefaultRingtonePreference extends RingtonePreference {
 
     @Override
     protected Uri onRestoreRingtone() {
-        return RingtoneManager.getActualDefaultRingtoneUri(getContext(), getRingtoneType());
+        final int ringtoneType = getRingtoneType();
+        final Context context = getContext();
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(
+                Context.TELEPHONY_SERVICE);
+        if (telephonyManager.isMultiSimEnabled() && ringtoneType == RingtoneManager.TYPE_RINGTONE) {
+            return RingtoneManager.getActualRingtoneUriBySubId(context, getSubId());
+        } else {
+            return RingtoneManager.getActualDefaultRingtoneUri(context, ringtoneType);
+        }
     }
     
 }
