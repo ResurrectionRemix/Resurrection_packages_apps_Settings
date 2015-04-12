d4f84410bf3563773bc2c30be2775d3cd9dc326c
Lokesh Chamane <lokesh.c703@gmail.com>
2015-04-12 01:16:38 -0400

    Settings: HeadsUp snooze function [2/2]
    HeadsUp: change timeout and snooze options to SeekBarPreferences
    https://github.com/Euphoria-OS/android_packages_apps_Settings/commit/fc3886878531f42ed53a59a57be59c741a649d9f
    
    5.1 introduced a snooze feature which activates when swiping up.
    Default snooze time is 1 minute. Let's make it configurable!
    
    Change-Id: I980008bde5f43d581903511f65965cbfecc68130
    Signed-off-by: Lokesh Chamane <lokesh.c703@gmail.com>

--- src/com/android/settings/headsup/HeadsUpSettings.java
+++ src/com/android/settings/headsup/HeadsUpSettings.java
@@ -44,13 +44,15 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Switch;
 
+import com.android.internal.util.cm.ScreenType;
+
 import com.android.settings.R;
+import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import com.android.settings.cyanogenmod.BaseSystemSettingSwitchBar;
 import com.android.settings.cyanogenmod.PackageListAdapter;
 import com.android.settings.cyanogenmod.PackageListAdapter.PackageItem;
-import com.android.settings.SettingsActivity;
-import com.android.settings.cyanogenmod.BaseSystemSettingSwitchBar;
+import com.android.settings.rr.SeekBarPreference;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -66,8 +68,10 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final int DIALOG_BLACKLIST_APPS = 1;
 
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+    private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
 
-    private ListPreference mHeadsUpTimeOut;
+    private SeekBarPreference mHeadsUpTimeOut;
+    private SeekBarPreference mHeadsUpSnoozeTime;
 
     private PackageListAdapter mPackageAdapter;
     private PackageManager mPackageManager;
@@ -103,12 +107,19 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(
                     "com.android.systemui:integer/heads_up_notification_decay", null, null));
-        mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
-        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+        mHeadsUpTimeOut = (SeekBarPreference) findPreference(PREF_HEADS_UP_TIME_OUT);
         int headsUpTimeOut = Settings.System.getInt(getContentResolver(),
-                Settings.System.HEADS_UP_NOTIFCATION_DECAY, defaultTimeOut);
-        mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
-        updateHeadsUpTimeOutSummary(headsUpTimeOut);
+                Settings.System.HEADS_UP_NOTIFICATION_DECAY, defaultTimeOut);
+        mHeadsUpTimeOut.setValue(headsUpTimeOut / 1000);
+        mHeadsUpTimeOut.setOnPreferenceChangeListener(this);
+
+        int defaultSnooze = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_default_snooze_length_ms", null, null));
+        mHeadsUpSnoozeTime = (SeekBarPreference) findPreference(PREF_HEADS_UP_SNOOZE_TIME);
+        int headsUpSnooze = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_NOTIFICATION_SNOOZE, defaultSnooze);
+        mHeadsUpSnoozeTime.setValue(headsUpSnooze / 60 / 1000);
+        mHeadsUpSnoozeTime.setOnPreferenceChangeListener(this);
 
         mDndPrefList = (PreferenceGroup) findPreference("dnd_applications_list");
         mDndPrefList.setOrderingAsAdded(false);
@@ -169,7 +180,7 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         // If running on a phone, remove padding around container
         // and the preference listview
-        if (!Utils.isTablet(getActivity())) {
+        if (!ScreenType.isTablet(getActivity())) {
             mPrefsContainer.setPadding(0, 0, 0, 0);
             getListView().setPadding(0, 0, 0, 0);
         }
@@ -306,25 +317,19 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mHeadsUpTimeOut) {
-            int headsUpTimeOut = Integer.valueOf((String) newValue);
+            int headsUpTimeOut = (Integer) newValue;
             Settings.System.putInt(getContentResolver(),
-                    Settings.System.HEADS_UP_NOTIFCATION_DECAY,
-                    headsUpTimeOut);
-            updateHeadsUpTimeOutSummary(headsUpTimeOut);
+                    Settings.System.HEADS_UP_NOTIFICATION_DECAY,
+                    headsUpTimeOut * 1000);
+            return true;
+        } else if (preference == mHeadsUpSnoozeTime) {
+            int headsUpSnooze = (Integer) newValue;
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_NOTIFICATION_SNOOZE,
+                    headsUpSnooze * 60 * 1000);
             return true;
         }
         return false;
-    }
-
-    private void updateHeadsUpTimeOutSummary(int value) {
-        String summary = getResources().getString(R.string.heads_up_time_out_summary,
-                value / 1000);
-        if (value == 0) {
-            mHeadsUpTimeOut.setSummary(
-                    getResources().getString(R.string.heads_up_time_out_never_summary));
-        } else {
-            mHeadsUpTimeOut.setSummary(summary);
-        }
     }
 
     @Override
