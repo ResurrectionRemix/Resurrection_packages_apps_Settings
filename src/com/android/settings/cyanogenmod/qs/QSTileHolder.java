85f6727bfd76bf8302af9e12203fd25706f02139
westcripp <korukaltan@gmail.com>
2015-04-16 01:38:35 +0300

    Settings: Add music tile (2/2)
    Thanks to camcory
    
    Change-Id: Id45b5177d1c2c49198b015095416ad53dfbf1d98

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -189,6 +189,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_usb_tether_on";
                 stringId = R.string.qs_tile_usb_tether;
                 break;
+            case QSConstants.TILE_MUSIC:
+                resourceName = "ic_qs_media_play";
+                stringId = R.string.qs_music_play_tile;
+                break;
             default:
                 return null;
         }
