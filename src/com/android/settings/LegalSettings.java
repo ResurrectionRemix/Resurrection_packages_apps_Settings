c3edb7f66b79d24c42b8685c2d12a557d5579502
Roman Birg <roman@cyngn.com>
2015-04-16 16:54:23 -0700

    Settings: Add Cyanogenmod legal info
    Open up the CyanogenMod legal info in the browser.
    
    Change-Id: I263ccc0509e275d17512528deb606341d58e7a0d
    Ticket-Id: CYNGNOS-1895
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/LegalSettings.java
+++ src/com/android/settings/LegalSettings.java
@@ -22,9 +22,14 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -36,10 +41,13 @@ import java.util.List;
 
 public class LegalSettings extends SettingsPreferenceFragment implements Indexable {
 
+    private static final String LOG_TAG = "LegalSettings";
     private static final String KEY_TERMS = "terms";
     private static final String KEY_LICENSE = "license";
     private static final String KEY_COPYRIGHT = "copyright";
     private static final String KEY_WEBVIEW_LICENSE = "webview_license";
+    private static final String PROPERTY_CMLICENSE_URL = "ro.cmlegal.url";
+    private static final String KEY_CM_LICENSE = "cmlicense";
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -56,6 +64,22 @@ public class LegalSettings extends SettingsPreferenceFragment implements Indexab
                 Utils.UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY);
         Utils.updatePreferenceToSpecificActivityOrRemove(act, parentPreference, KEY_WEBVIEW_LICENSE,
                 Utils.UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference.getKey().equals(KEY_CM_LICENSE)) {
+            String userCMLicenseUrl = SystemProperties.get(PROPERTY_CMLICENSE_URL);
+            final Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.parse(userCMLicenseUrl));
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override
