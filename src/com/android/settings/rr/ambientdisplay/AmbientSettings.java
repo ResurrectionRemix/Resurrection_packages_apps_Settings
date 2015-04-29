8859caa96fa67039d9780dfd8ac4fb17bba64ccc
westcripp <korukaltan@gmail.com>
2015-04-29 14:13:11 +0300

    Settings: clean up doze settings
    Change-Id: Ie189ca9e7d0a053e5ede3fcf0fa7bbf5af88a78a

--- src/com/android/settings/rr/ambientdisplay/AmbientSettings.java
+++ src/com/android/settings/rr/ambientdisplay/AmbientSettings.java
@@ -21,8 +21,10 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.os.Build;
@@ -44,6 +46,7 @@ import android.util.MathUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.cyanogenmod.SystemSettingSwitchPreference;
+import com.android.settings.rr.ambientdisplay.ShakeSensorManager;
 import com.android.settings.rr.SeekBarPreference;
 
 import java.util.ArrayList;
@@ -51,7 +54,7 @@ import java.util.List;
 
 public class AmbientSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, OnPreferenceClickListener,
-        Indexable {
+        Indexable, ShakeSensorManager.ShakeListener {
     private static final String TAG = "AmbientSettings";
 
     private static final String KEY_DOZE = "doze";
@@ -59,15 +62,27 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
     private static final String KEY_DOZE_PULSE_IN = "doze_pulse_in";
     private static final String KEY_DOZE_PULSE_VISIBLE = "doze_pulse_visible";
     private static final String KEY_DOZE_PULSE_OUT = "doze_pulse_out";
+    private static final String KEY_DOZE_LIST_MODE = "doze_list_mode";
+    private static final String KEY_DOZE_PULSE_MODE = "doze_pulse_on_notifications";
+    private static final String KEY_DOZE_SHAKE_CATEGORY = "doze_shake_category";
+    private static final String KEY_DOZE_SHAKE_THRESHOLD = "doze_shake_threshold";
+    private static final String KEY_DOZE_TIME_MODE = "doze_time_mode";
     private static final String KEY_DOZE_BRIGHTNESS = "doze_brightness";
 
     private int mAccValue;
     private int mOldAccValue;
     private SwitchPreference mDozePreference;
+    private ListPreference mDozeListMode;
     private ListPreference mDozePulseIn;
     private ListPreference mDozePulseVisible;
     private ListPreference mDozePulseOut;
+    private ListPreference mDozeShakeThreshold;
+    private SystemSettingSwitchPreference mDozeTimeMode;
     private SeekBarPreference mDozeBrightness;
+
+    private ShakeSensorManager mShakeSensorManager;
+    private AlertDialog mDialog;
+    private Button mShakeFoundButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -90,7 +105,25 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
         mDozePulseOut = (ListPreference) findPreference(KEY_DOZE_PULSE_OUT);
         mDozePulseOut.setOnPreferenceChangeListener(this);
 
+        if (isAccelerometerAvailable(activity)) {
+            mDozeListMode = (ListPreference) findPreference(KEY_DOZE_LIST_MODE);
+            mDozeListMode.setOnPreferenceChangeListener(this);
+
+            mDozeTimeMode = (SystemSettingSwitchPreference) findPreference(KEY_DOZE_TIME_MODE);
+
+            mDozeShakeThreshold = (ListPreference) findPreference(KEY_DOZE_SHAKE_THRESHOLD);
+            mDozeShakeThreshold.setOnPreferenceChangeListener(this);
+
+            removePreference(KEY_DOZE_PULSE_MODE);
+        } else {
+            removePreference(KEY_DOZE_LIST_MODE);
+            removePreference(KEY_DOZE_TIME_MODE);
+            removePreference(KEY_DOZE_SHAKE_THRESHOLD);
+            removePreference(KEY_DOZE_SHAKE_CATEGORY);
+        }
+        updateDozeListMode();
         updateDozeOptions();
+        mShakeSensorManager = new ShakeSensorManager(activity, this);
 
         mDozeBrightness = (SeekBarPreference) findPreference(KEY_DOZE_BRIGHTNESS);
         int dozeBrightness = Settings.System.getInt(resolver,
@@ -104,6 +137,12 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
         return MathUtils.constrain(value, PowerManager.BRIGHTNESS_OFF, PowerManager.BRIGHTNESS_ON);
     }
 
+    private static boolean isAccelerometerAvailable(Context context) {
+        boolean useAccelerometer = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_dozeUseAccelerometer);
+        return useAccelerometer;
+    }
+
     private static boolean isDozeAvailable(Context context) {
         String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
         if (TextUtils.isEmpty(name)) {
@@ -111,6 +150,195 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
                     com.android.internal.R.string.config_dozeComponent);
         }
         return !TextUtils.isEmpty(name);
+    }
+
+    private void updateDozeListMode() {
+        final boolean pocketMode = Settings.System.getInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 0) != 0;
+        final boolean shakeMode = Settings.System.getInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 0) != 0;
+        final boolean pulseMode = Settings.System.getInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1) != 0;
+        int listMode = 6;
+        if (pocketMode && shakeMode && pulseMode) {
+            listMode = 1;
+            enableShakeThreshold(true);
+        } else if (pocketMode && shakeMode && !pulseMode) {
+            listMode = 2;
+            enableShakeThreshold(true);
+        } else if (pocketMode && !shakeMode && pulseMode) {
+            listMode = 3;
+            enableShakeThreshold(false);
+        } else if (pocketMode && !shakeMode && !pulseMode) {
+            listMode = 4;
+            enableShakeThreshold(false);
+        } else if (!pocketMode && shakeMode && pulseMode) {
+            listMode = 5;
+            enableShakeThreshold(true);
+        } else if (!pocketMode && !shakeMode && pulseMode) {
+            listMode = 6;
+            enableShakeThreshold(false);
+        }
+        enabledTimeMode(pocketMode);
+        if (mDozeListMode != null) {
+            mDozeListMode.setValue(String.valueOf(listMode));
+            int index = mDozeListMode.findIndexOfValue(String.valueOf(listMode));
+            if (index != -1) {
+                mDozeListMode.setSummary(mDozeListMode.getEntries()[index]);
+            }
+        }
+    }
+
+    private void enabledTimeMode(boolean enable) {
+        if (mDozeTimeMode != null) {
+            mDozeTimeMode.setEnabled(enable);
+        }
+    }
+
+    private void showAccDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.doze_shake_mode_title);
+        alertDialog.setMessage(R.string.doze_shake_mode_test_summary);
+        alertDialog.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                startAcctest();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                stopAcctest();
+            }
+        });
+        mDialog = alertDialog.create();
+        mDialog.show();
+    }
+
+    @Override
+    public synchronized void onShake() {
+        String msg1 = getResources().getString(R.string.doze_shake_it);
+        String msg2 = getResources().getString(R.string.doze_shake_mode_test_result);
+
+        String msg = msg1 + "\n" + msg2;
+        mDialog.setMessage(msg);
+        Button shakeCancelButton = mDialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+        if (shakeCancelButton != null) {
+            shakeCancelButton.setText(R.string.no);
+        }
+        if (mShakeFoundButton != null) {
+            mShakeFoundButton.setEnabled(true);
+        }
+        mShakeSensorManager.disable();
+    }
+
+    private void startAcctest() {
+        mShakeSensorManager.enable(mAccValue);
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle(R.string.doze_shake_mode_title);
+        alertDialog.setMessage(R.string.doze_shake_test);
+        alertDialog.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                stopAcctest();
+            }
+        });
+        alertDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                revertAcctest();
+            }
+        });
+        mDialog = alertDialog.create();
+        mDialog.show();
+
+        mShakeFoundButton = mDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        if (mShakeFoundButton != null) {
+            mShakeFoundButton.setEnabled(false);
+        }
+    }
+
+    private void revertAcctest() {
+        mAccValue = mOldAccValue;
+        mDozeShakeThreshold.setValue(String.valueOf(mOldAccValue));
+        mShakeSensorManager.disable();
+    }
+
+    private void stopAcctest() {
+        appliedAccTest();
+        mShakeSensorManager.disable();
+    }
+
+    private void appliedAccTest() {
+        Settings.System.putInt(getContentResolver(),
+                    Settings.System.DOZE_SHAKE_ACC_THRESHOLD, mAccValue);
+        mDozeShakeThreshold.setValue(String.valueOf(mAccValue));
+        int index = mDozeShakeThreshold.findIndexOfValue(String.valueOf(mAccValue));
+        if (index != -1) {
+            mDozeShakeThreshold.setSummary(mDozeShakeThreshold.getEntries()[index]);
+        }
+    }
+
+    private void enableShakeThreshold(boolean enabled) {
+        final boolean writeMode = Settings.System.getInt(getContentResolver(),
+                   Settings.System.DOZE_OVERWRITE_VALUE, 0) != 0;
+        if (mDozeShakeThreshold != null) {
+            mDozeShakeThreshold.setEnabled(enabled && writeMode);
+        }
+    }
+
+    private void updateDozeListModeValue(int listMode) {
+        if (listMode == 1) {
+            enableShakeThreshold(true);
+            enabledTimeMode(true);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1);
+        } else if (listMode == 2) {
+            enableShakeThreshold(true);
+            enabledTimeMode(true);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 0);
+        } else if (listMode == 3) {
+            enableShakeThreshold(false);
+            enabledTimeMode(true);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 0);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1);
+        } else if (listMode == 4) {
+            enableShakeThreshold(false);
+            enabledTimeMode(true);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 0);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 0);
+        } else if (listMode == 5) {
+            enableShakeThreshold(true);
+            enabledTimeMode(false);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 0);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 1);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1);
+        } else if (listMode == 6) {
+            enableShakeThreshold(false);
+            enabledTimeMode(false);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_POCKET_MODE, 0);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_SHAKE_MODE, 0);
+            Settings.System.putInt(getContentResolver(),
+                   Settings.System.DOZE_PULSE_ON_NOTIFICATIONS, 1);
+        }
     }
 
     private void updateDozeOptions() {
@@ -141,6 +369,15 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
                mDozePulseOut.setSummary(mDozePulseOut.getEntries()[index]);
             }
         }
+        if (mDozeShakeThreshold != null) {
+            mAccValue = Settings.System.getInt(getContentResolver(),
+                    Settings.System.DOZE_SHAKE_ACC_THRESHOLD, 10);
+            mDozeShakeThreshold.setValue(String.valueOf(mAccValue));
+            int index = mDozeShakeThreshold.findIndexOfValue(String.valueOf(mAccValue));
+            if (index != -1) {
+                mDozeShakeThreshold.setSummary(mDozeShakeThreshold.getEntries()[index]);
+            }
+        }
     }
 
     @Override
@@ -148,11 +385,16 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
         super.onResume();
         updateState();
         updateDozeOptions();
+        updateDozeListMode();
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        mShakeSensorManager.disable();
+        if (mDialog != null) {
+            mDialog.dismiss();
+        }
     }
 
     private void updateState() {
@@ -166,7 +408,7 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == findPreference(KEY_DOZE_OVERWRITE_VALUE)) {
-            updateDozeOptions();
+            updateDozeListMode();
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
@@ -198,6 +440,20 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
             mDozePulseOut.setSummary(mDozePulseOut.getEntries()[index]);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.DOZE_PULSE_DURATION_OUT, dozePulseOut);
+        }
+        if (preference == mDozeShakeThreshold) {
+            int accValue = Integer.parseInt((String)objValue);
+            if (accValue != mAccValue) {
+                mOldAccValue = mAccValue;
+                mAccValue = accValue;
+                showAccDialog();
+            }
+        }
+        if (preference == mDozeListMode) {
+            int dozeListMode = Integer.parseInt((String)objValue);
+            updateDozeListModeValue(dozeListMode);
+            int index = mDozeListMode.findIndexOfValue((String) objValue);
+            mDozeListMode.setSummary(mDozeListMode.getEntries()[index]);
         }
         if (preference == mDozeBrightness) {
             int dozeBrightness = (Integer) objValue;
@@ -232,10 +488,22 @@ public class AmbientSettings extends SettingsPreferenceFragment implements
                     ArrayList<String> result = new ArrayList<String>();
                     if (!isDozeAvailable(context)) {
                         result.add(KEY_DOZE);
+                        result.add(KEY_DOZE_LIST_MODE);
+                        result.add(KEY_DOZE_TIME_MODE);
                         result.add(KEY_DOZE_OVERWRITE_VALUE);
+                        result.add(KEY_DOZE_PULSE_MODE);
                         result.add(KEY_DOZE_PULSE_IN);
                         result.add(KEY_DOZE_PULSE_VISIBLE);
                         result.add(KEY_DOZE_PULSE_OUT);
+                        result.add(KEY_DOZE_SHAKE_THRESHOLD);
+                    } else {
+                       if (!isAccelerometerAvailable(context)) {
+                           result.add(KEY_DOZE_LIST_MODE);
+                           result.add(KEY_DOZE_SHAKE_THRESHOLD);
+                       }
+                       if (isAccelerometerAvailable(context)) {
+                           result.add(KEY_DOZE_PULSE_MODE);
+                       }
                     }
                     return result;
                 }
