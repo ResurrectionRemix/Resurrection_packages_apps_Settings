cb5b3524280f334d022be70e0010f97eb481dcb9
Alex Cruz <mazdarider23@gmail.com>
2015-05-17 02:32:02 +0000

    WakeLock: Layout Improvements (See image below)
    - Moved switch to the right
    - Replaced refresh/save icons to match stock lollipop theme
    - Added padding to title/summary
    - Removed drawables / add vector
    
    - http://i.imgur.com/5HMpfQw.png
    
    Change-Id: Ibb71c8d6da906c0690b36d7ce271e223901c0eb0

--- src/com/android/settings/aicp/WakeLockBlocker.java
+++ src/com/android/settings/aicp/WakeLockBlocker.java
@@ -252,12 +252,12 @@ public class WakeLockBlocker extends SettingsPreferenceFragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(0, MENU_RELOAD, 0, R.string.wakelock_blocker_reload)
-                .setIcon(R.drawable.ic_menu_refresh_holo_dark)
+                .setIcon(com.android.internal.R.drawable.ic_menu_refresh)
                 .setAlphabeticShortcut('r')
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
                         MenuItem.SHOW_AS_ACTION_WITH_TEXT);
         menu.add(0, MENU_SAVE, 0, R.string.wakelock_blocker_save)
-                .setIcon(R.drawable.ic_menu_save)
+                .setIcon(R.drawable.ic_wakelockblocker_save)
                 .setAlphabeticShortcut('s')
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM |
                         MenuItem.SHOW_AS_ACTION_WITH_TEXT);
