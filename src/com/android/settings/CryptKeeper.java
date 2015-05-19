27456515155fadfca3422f7115bca1b323d6b234
Roman Birg <roman@cyngn.com>
2015-05-19 14:06:51 -0700

    CryptKeeper: layout whole screen in bounds
    Add flags to make the screen layout properly on devices with the
    navigation bar visible
    
    Change-Id: I8607c8e49ac3625a7fb6a13b4501ea7ac818b9ba
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/CryptKeeper.java
+++ src/com/android/settings/CryptKeeper.java
@@ -410,6 +410,13 @@ public class CryptKeeper extends Activity implements TextView.OnEditorActionList
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         // If we are not encrypted or encrypting, get out quickly.
         final String state = SystemProperties.get("vold.decrypt");
         if (!isDebugView() && ("".equals(state) || DECRYPT_STATE.equals(state))) {
