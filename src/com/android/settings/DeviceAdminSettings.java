33b3fdc49d19716f3565305891d8fd11b41719d3
Roman Birg <roman@cyngn.com>
2015-05-20 09:46:51 -0700

    Settings: add marquee to Device administrators screen
    Noticed while doing CTS testing that it was really hard to pick the
    correct receivers to enable when you cannot see their full names.
    
    Change-Id: I8f14d04f1037158c94d5501c115b5100456d7619
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/DeviceAdminSettings.java
+++ src/com/android/settings/DeviceAdminSettings.java
@@ -347,6 +347,7 @@ public class DeviceAdminSettings extends ListFragment {
             }
             vh.checkbox.setEnabled(enabled);
             vh.name.setEnabled(enabled);
+            vh.name.setSelected(true);
             vh.description.setEnabled(enabled);
             vh.icon.setEnabled(enabled);
         }
