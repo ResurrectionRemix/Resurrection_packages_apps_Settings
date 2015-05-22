19ad708ea932dc3b8b1389e112892764fde347ad
Brandon McAnsh <brandon.mcansh@gmail.com>
2015-05-22 16:10:23 -0400

    Profiles: Get Default profile with its UUID instead of name
    * getProfiles(name) is deprecated so get it with the UUID
    
    PS2: Use uuid added with I352cc2bfd8
    
    Should fix BUGDUMP-1936587
    
    Change-Id: Iefe823b12561a661da10cc8b8338c381876590ee
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- src/com/android/settings/profiles/SetupDefaultProfileReceiver.java
+++ src/com/android/settings/profiles/SetupDefaultProfileReceiver.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 
+import java.util.UUID;
+
 public class SetupDefaultProfileReceiver extends BroadcastReceiver {
 
     @Override
@@ -15,7 +17,8 @@ public class SetupDefaultProfileReceiver extends BroadcastReceiver {
                 Settings.System.SYSTEM_PROFILES_ENABLED, 1) == 1) {
             ProfileManager profileManager = (ProfileManager) context
                     .getSystemService(Context.PROFILE_SERVICE);
-            Profile defaultProfile = profileManager.getProfile("Default");
+            Profile defaultProfile = profileManager.getProfile(
+                    UUID.fromString("0230226d-0d05-494a-a9bd-d222a1117655"));
             if (defaultProfile != null) {
                 SetupActionsFragment.fillProfileWithCurrentSettings(context, defaultProfile);
                 profileManager.updateProfile(defaultProfile);
