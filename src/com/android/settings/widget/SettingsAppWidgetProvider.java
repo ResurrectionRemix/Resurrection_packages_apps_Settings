08c27b550d4b117b77e6ec8e2d3ffece57a29d34
longyu.huang <longyu.huang2014@gmail.com>
2015-05-23 04:03:45 -0700

    the status of brightness not refresh in power control widget after manual adjustment of brightnes
    Change-Id: I03606ad490e9298b761fcac0eeb15fb3ae92d6b2

--- src/com/android/settings/widget/SettingsAppWidgetProvider.java
+++ src/com/android/settings/widget/SettingsAppWidgetProvider.java
@@ -31,6 +31,7 @@ import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Handler;
+import android.os.UserHandle;
 import android.os.IPowerManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -940,10 +941,12 @@ public class SettingsAppWidgetProvider extends AppWidgetProvider {
         void startObserving() {
             ContentResolver resolver = mContext.getContentResolver();
             // Listen to brightness and brightness mode
-            resolver.registerContentObserver(Settings.System
-                    .getUriFor(Settings.System.SCREEN_BRIGHTNESS), false, this);
-            resolver.registerContentObserver(Settings.System
-                    .getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREEN_BRIGHTNESS), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREEN_BRIGHTNESS_MODE), false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.SCREEN_AUTO_BRIGHTNESS_ADJ), false, this, UserHandle.USER_ALL);
         }
 
         void stopObserving() {
