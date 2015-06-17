9da0a22f0140988e19d7f510186dd02b61c02890
eray orçunus <erayorcunus@gmail.com>
2015-06-17 16:25:45 +0300

    lockscreen: Add option to pass swipe-up-to-unlock (2/2)
    * Option appears on PIN,pattern and password methods
    * User should press back button to see notifications, clock etc.
    * Instantly hides keyguard if Smart Lock has unlocked phone.
    
    Patchset: 3
    http://review.cyanogenmod.org/#/c/101255/
    
    Change-Id: I31256770869b20842c69edb4a7a57b2bad7b3ea7
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -87,6 +87,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String KEY_SECURITY_CATEGORY = "security_category";
     private static final String KEY_DEVICE_ADMIN_CATEGORY = "device_admin_category";
     private static final String KEY_VISIBLE_ERROR_PATTERN = "visible_error_pattern";
+    private static final String KEY_DIRECTLY_SHOW = "directlyshow";
     private static final String KEY_VISIBLE_DOTS = "visibledots";
     private static final String KEY_LOCK_AFTER_TIMEOUT = "lock_after_timeout";
     private static final String KEY_OWNER_INFO_SETTINGS = "owner_info_settings";
@@ -120,7 +121,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String SWITCH_PREFERENCE_KEYS[] = { KEY_LOCK_AFTER_TIMEOUT,
             KEY_LOCK_ENABLED, KEY_VISIBLE_PATTERN, KEY_VISIBLE_GESTURE, KEY_VISIBLE_ERROR_PATTERN, KEY_VISIBLE_DOTS,
             KEY_BIOMETRIC_WEAK_LIVELINESS, KEY_POWER_INSTANTLY_LOCKS, KEY_SHOW_PASSWORD,
-            KEY_TOGGLE_INSTALL_APPLICATIONS };
+            KEY_DIRECTLY_SHOW, KEY_TOGGLE_INSTALL_APPLICATIONS, KEY_VISIBLE_GESTURE };
 
     // Only allow one trust agent on the platform.
     private static final boolean ONLY_ONE_TRUST_AGENT = false;
@@ -137,6 +138,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private SwitchPreference mVisiblePattern;
     private SwitchPreference mVisibleGesture;
     private SwitchPreference mVisibleErrorPattern;
+    private SwitchPreference mDirectlyShow;
     private SwitchPreference mVisibleDots;
     private SwitchPreference mShowPassword;
 
@@ -325,6 +327,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
         // visible error pattern
         mVisibleErrorPattern = (SwitchPreference) root.findPreference(KEY_VISIBLE_ERROR_PATTERN);
+
+        // directly show
+        mDirectlyShow = (SwitchPreference) root.findPreference(KEY_DIRECTLY_SHOW);
 
         // visible dots
         mVisibleDots = (SwitchPreference) root.findPreference(KEY_VISIBLE_DOTS);
@@ -669,6 +674,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (mVisibleErrorPattern != null) {
             mVisibleErrorPattern.setChecked(lockPatternUtils.isShowErrorPath());
         }
+        if (mDirectlyShow != null) {
+            mDirectlyShow.setChecked(lockPatternUtils.shouldPassToSecurityView());
+        }
         if (mVisibleDots != null) {
             mVisibleDots.setChecked(lockPatternUtils.isVisibleDotsEnabled());
         }
@@ -773,6 +781,8 @@ public class SecuritySettings extends SettingsPreferenceFragment
             lockPatternUtils.setLockPatternEnabled((Boolean) value);
         } else if (KEY_VISIBLE_PATTERN.equals(key)) {
             lockPatternUtils.setVisiblePatternEnabled((Boolean) value);
+        } else if (KEY_DIRECTLY_SHOW.equals(key)) {
+            lockPatternUtils.setPassToSecurityView((Boolean) value);
         } else if (KEY_VISIBLE_ERROR_PATTERN.equals(key)) {
             lockPatternUtils.setShowErrorPath((Boolean) value);
         } else if (KEY_VISIBLE_GESTURE.equals(key)) {
