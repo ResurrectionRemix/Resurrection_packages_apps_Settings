0ce17f50303a333b586c98b76d98292dfa887535
westcripp <korukaltan@gmail.com>
2015-06-22 12:37:06 +0300

    update arrays
    Change-Id: Id4a70507c3aac16565dc7e685c90e536d15f138a

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -202,7 +202,7 @@ public class QSTileHolder {
                 stringId = R.string.qs_tile_powermenu;
                 break;
             case QSConstants.TILE_HEADS_UP:
-                resourceName = "ic_qs_heads_up_on";
+                resourceName = "ic_headsup_toggle_on";
                 stringId = R.string.qs_tile_heads_up;
                 break;
             case QSConstants.TILE_SLIMACTION:
@@ -210,7 +210,7 @@ public class QSTileHolder {
                 stringId = R.string.qs_tile_slimaction;
                 break;
             case QSConstants.TILE_SYSTEMUI_RESTART:
-                resourceName = "ic_qs_reboot";
+                resourceName = "ic_qs_systemui_restart"";
                 stringId = R.string.qs_systemui_restart_tile;
                 break;
             default:
