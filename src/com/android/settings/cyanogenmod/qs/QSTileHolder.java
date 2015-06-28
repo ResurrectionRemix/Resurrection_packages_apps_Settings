8847a5fe3b73c5dae4abe81fd79b93407bcf782f
rogersb11 <brettrogers11@gmail.com>
2015-06-29 02:36:39 +0300

    SlimActions: Floating window mode switch ant tile (2/2)
    Change-Id: I6cea6a3a7f8463d2151050904475b3751f1f2cc8

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -212,6 +212,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_systemui_restart";
                 stringId = R.string.qs_systemui_restart_tile;
                 break;
+            case QSConstants.TILE_SLIM_FLOATS:
+                resourceName = "ic_qs_floating_on";
+                stringId = R.string.qs_tile_slim_floats;
+                break;
 
             // Dynamic tiles
             case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
