LineageOS/android_packages_apps_Settings

Settings: Point to classes within cmsdk.
Change-Id: I19a767e90d20bd6a8b4709416779590aabd9858f

src/com/android/settings/profiles/SetupActionsFragment.java
@@ -16,14 +16,14 @@
 package com.android.settings.profiles;
 
 import android.app.Activity;
-import android.app.AirplaneModeSettings;
+import cyanogenmod.app.AirplaneModeSettings;
 import android.app.AlertDialog;
-import android.app.BrightnessSettings;
-import android.app.ConnectionSettings;
+import cyanogenmod.app.BrightnessSettings;
+import cyanogenmod.app.ConnectionSettings;
 import android.app.Dialog;
 import android.app.NotificationGroup;
-import android.app.RingModeSettings;
-import android.app.StreamSettings;
+import cyanogenmod.app.RingModeSettings;
+import cyanogenmod.app.StreamSettings;
 import android.app.admin.DevicePolicyManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
@@ -88,15 +88,15 @@ import com.android.settings.Utils;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_2G3G4G;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_GPS;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_MOBILEDATA;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_NFC;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_SYNC;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIFI;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIFIAP;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIMAX;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_2G3G4G;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_GPS;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_MOBILEDATA;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_NFC;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_SYNC;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_WIFI;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_WIFIAP;
+import static cyanogenmod.app.ConnectionSettings.PROFILE_CONNECTION_WIMAX;
 
 public class SetupActionsFragment extends SettingsPreferenceFragment
         implements AdapterView.OnItemClickListener {

src/com/android/settings/profiles/actions/item/AirplaneModeItem.java
@@ -15,8 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.AirplaneModeSettings;
-import android.app.RingModeSettings;
+import cyanogenmod.app.AirplaneModeSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/BrightnessItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.BrightnessSettings;
+import cyanogenmod.app.BrightnessSettings;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;

src/com/android/settings/profiles/actions/item/ConnectionOverrideItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.ConnectionSettings;
+import cyanogenmod.app.ConnectionSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/RingModeItem.java
@@ -15,10 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.AlertDialog;
-import android.app.RingModeSettings;
-import android.content.Context;
-import android.content.DialogInterface;
+import cyanogenmod.app.RingModeSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/VolumeStreamItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.StreamSettings;
+import cyanogenmod.app.StreamSettings;
 import android.content.Context;
 import android.media.AudioManager;
 import android.view.LayoutInflater;

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/profiles/SetupActionsFragment.java
@@ -16,14 +16,14 @@
 package com.android.settings.profiles;
 
 import android.app.Activity;
-import android.app.AirplaneModeSettings;
+import cyanogenmod.profiles.AirplaneModeSettings;
 import android.app.AlertDialog;
-import android.app.BrightnessSettings;
-import android.app.ConnectionSettings;
+import cyanogenmod.profiles.BrightnessSettings;
+import cyanogenmod.profiles.ConnectionSettings;
 import android.app.Dialog;
 import android.app.NotificationGroup;
-import android.app.RingModeSettings;
-import android.app.StreamSettings;
+import cyanogenmod.profiles.RingModeSettings;
+import cyanogenmod.profiles.StreamSettings;
 import android.app.admin.DevicePolicyManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
@@ -88,15 +88,15 @@ import com.android.settings.Utils;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_2G3G4G;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_GPS;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_MOBILEDATA;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_NFC;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_SYNC;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIFI;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIFIAP;
-import static android.app.ConnectionSettings.PROFILE_CONNECTION_WIMAX;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_2G3G4G;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_BLUETOOTH;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_GPS;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_MOBILEDATA;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_NFC;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_SYNC;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_WIFI;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_WIFIAP;
+import static cyanogenmod.profiles.ConnectionSettings.PROFILE_CONNECTION_WIMAX;
 
 public class SetupActionsFragment extends SettingsPreferenceFragment
         implements AdapterView.OnItemClickListener {

src/com/android/settings/profiles/actions/item/AirplaneModeItem.java
@@ -15,8 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.AirplaneModeSettings;
-import android.app.RingModeSettings;
+import cyanogenmod.profiles.AirplaneModeSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/BrightnessItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.BrightnessSettings;
+import cyanogenmod.profiles.BrightnessSettings;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;

src/com/android/settings/profiles/actions/item/ConnectionOverrideItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.ConnectionSettings;
+import cyanogenmod.profiles.ConnectionSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/RingModeItem.java
@@ -15,10 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.AlertDialog;
-import android.app.RingModeSettings;
-import android.content.Context;
-import android.content.DialogInterface;
+import cyanogenmod.profiles.RingModeSettings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

src/com/android/settings/profiles/actions/item/VolumeStreamItem.java
@@ -15,7 +15,7 @@
  */
 package com.android.settings.profiles.actions.item;
 
-import android.app.StreamSettings;
+import cyanogenmod.profiles.StreamSettings;
 import android.content.Context;
 import android.media.AudioManager;
 import android.view.LayoutInflater;