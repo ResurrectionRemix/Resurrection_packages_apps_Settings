a6c75871aa4c251987c076019c07619275b9718f
Ricardo Cerqueira <ricardo@cyngn.com>
2015-07-21 14:59:44 -0700

    Don't include all resource densities
    There's no reason for this anymore, trim out the drawable fat
    
    Change-Id: Ie8c4b7c226470c0120eac54c831287e54a4ed075

--- Android.mk
+++ Android.mk
@@ -35,8 +35,8 @@ LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.v7.cardview
 
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_JAVA_LIBRARIES += org.cyanogenmod.hardware
+LOCAL_ASSET_DIR := $(LOCAL_PATH)/assets
 
 include frameworks/opt/setupwizard/navigationbar/common.mk
 include $(BUILD_PACKAGE)
