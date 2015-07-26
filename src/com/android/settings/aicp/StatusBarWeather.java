03b49ac10dfde5179f0e3afde13940dd7eefe6c8
eyosen <abittin@gmail.com>
2015-07-26 21:07:09 +0300

    Settings: statusbar weather temperature placement (1/2)
    Left or right options
    
    PS2: no more restartSystemUI
    PS3: remove unneeded explanation
    
    Change-Id: I26a5f129c17bf041a6e84dbb21cb3acedb1a4f0b

--- src/com/android/settings/aicp/StatusBarWeather.java
+++ src/com/android/settings/aicp/StatusBarWeather.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -37,9 +38,11 @@ import java.util.Map;
 public class StatusBarWeather extends SettingsPreferenceFragment
         implements OnPreferenceChangeListener {
 
+    private static final String STATUS_BAR_TEMPERATURE = "status_bar_temperature";
     private static final String STATUS_BAR_TEMPERATURE_STYLE = "status_bar_temperature_style";
 
     private ListPreference mStatusBarTemperature;
+    private ListPreference mStatusBarTemperatureStyle;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -48,24 +51,44 @@ public class StatusBarWeather extends SettingsPreferenceFragment
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mStatusBarTemperature = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE_STYLE);
-        int temperatureStyle = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0);
-        mStatusBarTemperature.setValue(String.valueOf(temperatureStyle));
+        mStatusBarTemperature = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE);
+        int temperatureShow = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+                UserHandle.USER_CURRENT);
+        mStatusBarTemperature.setValue(String.valueOf(temperatureShow));
         mStatusBarTemperature.setSummary(mStatusBarTemperature.getEntry());
         mStatusBarTemperature.setOnPreferenceChangeListener(this);
+
+        mStatusBarTemperatureStyle = (ListPreference) findPreference(STATUS_BAR_TEMPERATURE_STYLE);
+        int temperatureStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mStatusBarTemperatureStyle.setValue(String.valueOf(temperatureStyle));
+        mStatusBarTemperatureStyle.setSummary(mStatusBarTemperatureStyle.getEntry());
+        mStatusBarTemperatureStyle.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mStatusBarTemperature) {
-            int temperatureStyle = Integer.valueOf((String) newValue);
+            int temperatureShow = Integer.valueOf((String) newValue);
             int index = mStatusBarTemperature.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureStyle);
+            Settings.System.putIntForUser(
+                    resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, temperatureShow,
+                    UserHandle.USER_CURRENT);
             mStatusBarTemperature.setSummary(
                     mStatusBarTemperature.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarTemperatureStyle) {
+            int temperatureStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarTemperatureStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(
+                    resolver, Settings.System.STATUS_BAR_WEATHER_TEMP_STYLE, temperatureStyle,
+                    UserHandle.USER_CURRENT);
+            mStatusBarTemperatureStyle.setSummary(
+                    mStatusBarTemperatureStyle.getEntries()[index]);
             return true;
         }
         return false;
