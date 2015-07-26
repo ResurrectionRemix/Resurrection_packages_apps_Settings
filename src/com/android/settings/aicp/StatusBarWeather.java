1b3e5a0dc43813155edd4b20292a8fde2400a040
LorDClockaN <davor@losinj.com>
2015-07-26 21:49:17 +0200

    Disable weather settings if hidden
    Change-Id: I37a6775b33f172a7b6a9a94f5adc2ebb4f09ecb3

--- src/com/android/settings/aicp/StatusBarWeather.java
+++ src/com/android/settings/aicp/StatusBarWeather.java
@@ -80,6 +80,7 @@ public class StatusBarWeather extends SettingsPreferenceFragment
             mStatusBarTemperatureColor.setSummary(hexColor);
             mStatusBarTemperatureColor.setNewPreviewColor(intColor);
 
+        updateWeatherOptions();
     }
 
     @Override
@@ -93,6 +94,7 @@ public class StatusBarWeather extends SettingsPreferenceFragment
                     UserHandle.USER_CURRENT);
             mStatusBarTemperature.setSummary(
                     mStatusBarTemperature.getEntries()[index]);
+            updateWeatherOptions();
             return true;
         } else if (preference == mStatusBarTemperatureStyle) {
             int temperatureStyle = Integer.valueOf((String) newValue);
@@ -113,6 +115,17 @@ public class StatusBarWeather extends SettingsPreferenceFragment
             return true;
         }
         return false;
+    }
+
+    private void updateWeatherOptions() {
+        if (Settings.System.getInt(getActivity().getContentResolver(),
+            Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0) == 0) {
+            mStatusBarTemperatureStyle.setEnabled(false);
+            mStatusBarTemperatureColor.setEnabled(false);
+        } else {
+            mStatusBarTemperatureStyle.setEnabled(true);
+            mStatusBarTemperatureColor.setEnabled(true);
+        }
     }
 
 }
