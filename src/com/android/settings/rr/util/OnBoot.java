bf109975bc866b8cb5ae4b97d4246a3bc15fd391
maxwen <max.weninger@gmail.com>
2015-09-14 01:32:05 +0300

    Simplify OnBoot for selinux switch
    Change-Id: I08cab0d10bcde2f248cc7afb7cea54a910012658
    Signed-off-by: Varun <date.varun123@gmail.com>

--- src/com/android/settings/rr/util/OnBoot.java
+++ src/com/android/settings/rr/util/OnBoot.java
@@ -1,34 +1,42 @@
 package com.android.settings.rr.util;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import com.android.internal.logging.MetricsLogger;
+import android.widget.Toast;
+import java.io.IOException;
+import java.util.List;
 
 import com.android.settings.util.CMDProcessor;
-
-import java.io.IOException;
-
 
 public class OnBoot extends BroadcastReceiver {
 
     Context settingsContext = null;
     private static final String TAG = "RR_onboot";
+    Boolean mSetupRunning = false;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        try {
-            settingsContext = context.createPackageContext("com.android.settings", 0);
-        } catch (Exception e) {
-            Log.e(TAG, "Package not found", e);
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        for(int i = 0; i < procInfos.size(); i++)
+        {
+            if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+                mSetupRunning = true;
+            }
         }
-        SharedPreferences sharedpreferences = settingsContext.getSharedPreferences("com.android.settings_preferences", Context.MODE_PRIVATE);
-        if(sharedpreferences.getBoolean("selinux", true)) {
-            CMDProcessor.runSuCommand("setenforce 1");
-        } else if (!sharedpreferences.getBoolean("selinux", true)) {
-            CMDProcessor.runSuCommand("setenforce 0");
+        if(!mSetupRunning) {
+             SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
+             if(sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 1");
+             } else if (!sharedpreferences.getBoolean("selinux", true)) {
+                 CMDProcessor.runSuCommand("setenforce 0");
+             }
         }
     }
 
