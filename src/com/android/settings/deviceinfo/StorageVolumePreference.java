594bd7a46879340743c4422648cf2ed4a86118b7
George G <kreach3r@users.noreply.github.com>
2015-11-03 23:57:22 +0200

    Themes: Settings: Expose storage summary text
    forward ported and adapted to oreo
    
    This commit simply exposes the storage summary text hardcodes to allow
    themers to theme them without issue.
    
    nathanchance edit: updated for Nougat 7.1
    
    Change-Id: I10c124b6a1516a1be7ac234c1dd7dbe2cbe05bb2
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- src/com/android/settings/deviceinfo/StorageVolumePreference.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -45,6 +45,7 @@ public class StorageVolumePreference extends Preference {
     private final VolumeInfo mVolume;
 
     private int mColor;
+    private int mSecondaryColor;
     private int mUsedPercent = -1;
 
     // TODO: ideally, VolumeInfo should have a total physical size.
@@ -54,6 +55,8 @@ public class StorageVolumePreference extends Preference {
         mStorageManager = context.getSystemService(StorageManager.class);
         mVolume = volume;
         mColor = color;
+
+        mSecondaryColor = Utils.getColorAttr(context, android.R.attr.textColorSecondary);
 
         setLayoutResource(R.layout.storage_volume);
 
@@ -108,7 +111,7 @@ public class StorageVolumePreference extends Preference {
     public void onBindViewHolder(PreferenceViewHolder view) {
         final ImageView unmount = (ImageView) view.findViewById(R.id.unmount);
         if (unmount != null) {
-            unmount.setImageTintList(ColorStateList.valueOf(Color.parseColor("#8a000000")));
+            unmount.setImageTintList(ColorStateList.valueOf(mSecondaryColor));
             unmount.setOnClickListener(mUnmountListener);
         }
 
