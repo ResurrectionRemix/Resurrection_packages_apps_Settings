f14aaa7022652b38bf0a6f584f201b014c7ff9ed
Varun <date.varun123@gmail.com>
2015-11-16 11:01:18 +0530

    Recent Settings : Add RR Dependencies

--- src/com/android/settings/rr/RecentsSettings.java
+++ src/com/android/settings/rr/RecentsSettings.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2015 Benzo Rom
+* Copyright (C) 2015 ResurrectionRemix Rom
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-package com.android.settings.benzo;
+package com.android.settings.rr;
 
 
 import android.content.ContentResolver;
