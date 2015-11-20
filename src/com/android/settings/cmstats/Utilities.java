d51702e9cf20be5a0ce6453b43c4e20dad95d97f
Adnan Begovic <adnan@cyngn.com>
2015-11-20 15:36:11 -0800

    Settings: utilize STATS_COLLECTION from CMSettings.
    Change-Id: Id57336c400ebabcb525f1a5e741c49bfa11c2916

--- src/com/android/settings/cmstats/Utilities.java
+++ src/com/android/settings/cmstats/Utilities.java
@@ -23,6 +23,8 @@ import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 
+import cyanogenmod.providers.CMSettings;
+
 import java.math.BigInteger;
 import java.net.NetworkInterface;
 import java.security.MessageDigest;
@@ -83,8 +85,8 @@ public class Utilities {
      * @return Whether or not stats collection is enabled.
      */
     public static boolean isStatsCollectionEnabled(Context context) {
-        return Settings.Secure.getInt(context.getContentResolver(),
-                Settings.Secure.STATS_COLLECTION, 1) != 0;
+        return CMSettings.Secure.getInt(context.getContentResolver(),
+                CMSettings.Secure.STATS_COLLECTION, 1) != 0;
     }
 
     /**
@@ -94,7 +96,7 @@ public class Utilities {
      */
     public static void setStatsCollectionEnabled(Context context, boolean enabled) {
         int enable = (enabled) ? 1 : 0;
-        Settings.Secure.putInt(context.getContentResolver(),
-                Settings.Secure.STATS_COLLECTION, enable);
+        CMSettings.Secure.putInt(context.getContentResolver(),
+                CMSettings.Secure.STATS_COLLECTION, enable);
     }
 }
