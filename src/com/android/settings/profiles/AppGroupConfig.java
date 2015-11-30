949eea1235e3f1c769ec349fa65facf1c1449a72
lion0738 <lion0738@naver.com>
2015-11-30 23:53:22 +0900

    Settings: Fix IllegalArgumentException in AppGroupConfig
    Change-Id: I772d5f8e443699ff3f45e8d1c50debb2c7730b17

--- src/com/android/settings/profiles/AppGroupConfig.java
+++ src/com/android/settings/profiles/AppGroupConfig.java
@@ -43,6 +43,7 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.cyanogenmod.PackageListAdapter;
@@ -194,7 +195,7 @@ public class AppGroupConfig extends SettingsPreferenceFragment
 
     @Override
     protected int getMetricsCategory() {
-        return 0;
+        return MetricsLogger.DONT_TRACK_ME_BRO;
     }
 
     @Override
