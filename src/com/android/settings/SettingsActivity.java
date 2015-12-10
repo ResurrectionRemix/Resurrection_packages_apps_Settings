LineageOS/android_packages_apps_Settings

Settings: allow Profiles to be launched externally
Change-Id: I620be76139989722ac722f00259425c237a1c66e
Signed-off-by: Roman Birg <roman@cyngn.com>

src/com/android/settings/SettingsActivity.java
@@ -88,6 +88,7 @@ import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.notification.NotificationAppList;
 import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.profiles.NFCProfileTagCallback;
+import com.android.settings.profiles.ProfilesSettings;
 import com.android.settings.quicklaunch.QuickLaunchSettings;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
@@ -310,6 +311,7 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),
+            ProfilesSettings.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName()
     };
 

ResurrectionRemix/Resurrection_packages_apps_Settings

Signed-off-by: Varun <date.varun123@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -97,6 +97,7 @@ import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.livedisplay.LiveDisplay;
 import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.profiles.NFCProfileTagCallback;
+import com.android.settings.profiles.ProfilesSettings;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
@@ -372,7 +373,8 @@ public class SettingsActivity extends Activity
             com.android.settings.cyanogenmod.DisplayRotation.class.getName(),
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             BlacklistSettings.class.getName(),
-            ContributorsCloudFragment.class.getName()
+            ContributorsCloudFragment.class.getName(),
+            ProfilesSettings.class.getName()
     };
 
 