91a8759890a9edab503a3b40a2309ee2602b1366
westcripp <korukaltan@gmail.com>
2015-12-13 03:32:49 +0200

    Add back RR Path to changelog and features

--- src/com/android/settings/rr/ChangeLog.java
+++ src/com/android/settings/rr/ChangeLog.java
@@ -31,7 +31,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ChangeLog extends Fragment {
-    private static final String CHANGELOG_PATH = "/system/etc/Changelog.txt";
+    private static final String CHANGELOG_PATH = "/system/etc/RR/Changelog.txt";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
