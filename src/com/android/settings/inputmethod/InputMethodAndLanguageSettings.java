ef25f4c88ec0e6ec7f1ee77f42eb2f98f623bc45
Roman Birg <roman@cyngn.com>
2016-01-06 09:31:10 -0800

    Settings: remove voice wakeup link as necessary
    - Package check wasn't using the package constant
    - Hide link for secondary users
    
    Change-Id: Id3c8269311d5cfabd146af7ce1687cdbdb35a19c
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
+++ src/com/android/settings/inputmethod/InputMethodAndLanguageSettings.java
@@ -97,6 +97,7 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
     private static final String KEY_TRACKPAD_SETTINGS = "gesture_pad_settings";
     private static final String KEY_STYLUS_GESTURES = "stylus_gestures";
     private static final String KEY_STYLUS_ICON_ENABLED = "stylus_icon_enabled";
+    private static final String KEY_VOICE_CATEGORY = "voice_category";
     private static final String KEY_VOICE_WAKEUP = "voice_wakeup";
 
     private static final String VOICE_WAKEUP_PACKAGE = "com.cyanogenmod.voicewakeup";
@@ -274,10 +275,15 @@ public class InputMethodAndLanguageSettings extends SettingsPreferenceFragment
             showKeyboardLayoutDialog(identifier);
         }
 
-        if (!Utils.isPackageInstalled(getActivity(), KEY_VOICE_WAKEUP, false)) {
-            Preference wakeup = findPreference(KEY_VOICE_WAKEUP);
-            if (wakeup != null) {
-                getPreferenceScreen().removePreference(wakeup);
+        if (!Utils.isUserOwner() ||
+                !Utils.isPackageInstalled(getActivity(), VOICE_WAKEUP_PACKAGE, false)) {
+            PreferenceCategory voiceCategory = (PreferenceCategory)
+                    findPreference(KEY_VOICE_CATEGORY);
+            if (voiceCategory != null) {
+                Preference wakeup = voiceCategory.findPreference(KEY_VOICE_WAKEUP);
+                if (wakeup != null) {
+                    voiceCategory.removePreference(wakeup);
+                }
             }
         }
 
