cdbd3f6e6eeb10a8d984b74a9e87b3763ec35c4c
beanstown106 <beanstown106 nbenis106@gmail.com>
2016-01-30 20:30:28 +0100

    Lockscreen fonts: set correct default value [2/2]
    Change-Id: Id8325097ad4b0b84f87e3b53f2c0f1aac460ed1d

--- src/com/android/settings/rr/LockScreenSettings.java
+++ src/com/android/settings/rr/LockScreenSettings.java
@@ -74,7 +74,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment  implements O
 
             mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
             mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                    resolver, Settings.System.LOCK_CLOCK_FONTS, 0)));
+                    resolver, Settings.System.LOCK_CLOCK_FONTS, 4)));
             mLockClockFonts.setSummary(mLockClockFonts.getEntry());
             mLockClockFonts.setOnPreferenceChangeListener(this);
 
