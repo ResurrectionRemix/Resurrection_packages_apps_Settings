86b86ae3b6aa76ac35fb2cb698f2ea3b4088d41d
Varun <date.varun123@gmail.com>
2016-02-02 11:44:57 +0530

    Build Id 6.0.1_r10->6.0.1->r13
    Signed-off-by: Varun <date.varun123@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -134,7 +134,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_BASEBAND_VERSION, "gsm.version.baseband");
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
-        setStringSummary(KEY_BUILD_NUMBER, "Resurrection Remix Marshmallow 6.0.1_r10 - MMB29T");
+        setStringSummary(KEY_BUILD_NUMBER, "Resurrection Remix Marshmallow 6.0.1_r13 - MMB29U");
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.modversion");
