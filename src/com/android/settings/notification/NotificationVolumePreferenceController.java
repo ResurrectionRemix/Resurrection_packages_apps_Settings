38d554476a80865c5e01f022cbcdbb072fcb59c8
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-02-22 20:56:04 +0200

    Settings: Use correct icon for ring volume
    Change-Id: If145262562c1409cd8a6cb79ba46b6646c75d1a8
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/notification/NotificationVolumePreferenceController.java
+++ src/com/android/settings/notification/NotificationVolumePreferenceController.java
@@ -58,4 +58,18 @@ public class NotificationVolumePreferenceController extends
         return R.drawable.ic_notifications_off_24dp;
     }
 
+    @Override
+    protected void updatePreferenceIcon() {
+        if (mPreference != null) {
+            if (mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
+                mMuteIcon = R.drawable.ic_volume_ringer_vibrate;
+                mPreference.showIcon(R.drawable.ic_volume_ringer_vibrate);
+            } else if (mRingerMode == AudioManager.RINGER_MODE_SILENT) {
+                mMuteIcon = R.drawable.ic_notifications_off_24dp;
+                mPreference.showIcon(R.drawable.ic_notifications_off_24dp);
+            } else {
+                mPreference.showIcon(R.drawable.ic_notifications);
+            }
+        }
+    }
 }
