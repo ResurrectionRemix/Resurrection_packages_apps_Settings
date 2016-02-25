cf3173789786a1ee726b7994ebf2619590bea2c6
Danesh M <daneshm90@gmail.com>
2016-02-25 15:21:40 -0800

    Settings : Fix back button for sim card lock
    CYNGNOS-2151
    
    Change-Id: I5420c07931a6aa620328f9f8844512cac9bf1638

--- src/com/android/settings/IccLockSettings.java
+++ src/com/android/settings/IccLockSettings.java
@@ -212,6 +212,16 @@ public class IccLockSettings extends InstrumentedPreferenceActivity
         updatePreferences();
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+
     private void updatePreferences() {
         mPinDialog.setEnabled(mPhone != null);
         mPinToggle.setEnabled(mPhone != null);
