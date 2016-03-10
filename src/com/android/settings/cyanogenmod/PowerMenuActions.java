AOKP/packages_apps_Settings

Settings: Powermenu Torch option [2/2]
Thanks to Alex Cruz

Change-Id: Ie68a7737bd1f8138eb83c45058e973a35034b54e

src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -36,6 +36,7 @@ import android.provider.Settings;
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.aicp.AicpUtils;
 import com.android.internal.util.cm.PowerMenuConstants;
 
 import cyanogenmod.providers.CMSettings;
@@ -64,6 +65,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private CheckBoxPreference mSettingsPref;
     private CheckBoxPreference mLockdownPref;
     private CheckBoxPreference mBugReportPref;
+    private CheckBoxPreference mPowermenuTorch;
     private CheckBoxPreference mSilentPref;
     private CheckBoxPreference mVoiceAssistPref;
     private CheckBoxPreference mAssistPref;
@@ -128,6 +130,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment
                 mLockdownPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
             } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
                 mBugReportPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
+                mPowermenuTorch = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_SILENT)) {
                 mSilentPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SILENT);
             } else if (action.equals(GLOBAL_ACTION_KEY_VOICEASSIST)) {
@@ -207,6 +211,14 @@ public class PowerMenuActions extends SettingsPreferenceFragment
             mBugReportPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_BUGREPORT));
         }
 
+        if (mPowermenuTorch != null) {
+            if (!AicpUtils.deviceSupportsFlashLight(getActivity())) {
+                getPreferenceScreen().removePreference(mPowermenuTorch);
+            } else {
+                mPowermenuTorch.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_TORCH));
+            }
+        }
+
         if (mSilentPref != null) {
             mSilentPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SILENT));
         }
@@ -267,6 +279,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment
         } else if (preference == mBugReportPref) {
             value = mBugReportPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+
+        } else if (preference == mPowermenuTorch) {
+            value = mPowermenuTorch.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_TORCH);
 
         } else if (preference == mSilentPref) {
             value = mSilentPref.isChecked();

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -35,6 +35,7 @@ import android.provider.Settings;
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.util.aicp.NamelessUtils;
 import com.android.internal.util.cm.PowerMenuConstants;
 
 import cyanogenmod.providers.CMSettings;
@@ -66,6 +67,9 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private SwitchPreference mVoiceAssistPref;
     private SwitchPreference mAssistPref;
     private SwitchPreference mScreenrecordPref;
+    private SwitchPreference mPowermenuTorch;
+
+
 
     private static final String SCREENSHOT_DELAY = "screenshot_delay";
 
@@ -126,6 +130,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment
                 mLockdownPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_LOCKDOWN);
             } else if (action.equals(GLOBAL_ACTION_KEY_BUGREPORT)) {
                 mBugReportPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_BUGREPORT);
+            } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
+                mPowermenuTorch = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_SILENT)) {
                 mSilentPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SILENT);
             } else if (action.equals(GLOBAL_ACTION_KEY_VOICEASSIST)) {
@@ -201,6 +207,14 @@ public class PowerMenuActions extends SettingsPreferenceFragment
             mBugReportPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_BUGREPORT));
         }
 
+        if (mPowermenuTorch != null) {
+            if (!NamelessUtils.deviceSupportsFlashLight(getActivity())) {
+                getPreferenceScreen().removePreference(mPowermenuTorch);
+            } else {
+                mPowermenuTorch.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_TORCH));
+            }
+        }
+
         if (mSilentPref != null) {
             mSilentPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SILENT));
         }
@@ -261,6 +275,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment
         } else if (preference == mBugReportPref) {
             value = mBugReportPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_BUGREPORT);
+
+        } else if (preference == mPowermenuTorch) {
+            value = mPowermenuTorch.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_TORCH);
 
         } else if (preference == mSilentPref) {
             value = mSilentPref.isChecked();