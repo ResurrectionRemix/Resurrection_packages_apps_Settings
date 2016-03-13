0499a176ba2ac3d937eeba39cc23dae5b2d8d3cf
westcripp <korukaltan@gmail.com>
2016-03-13 20:13:00 +0200

    Settings: Optimizations and cleanups [2/2] - Add needed callbacks to refresh statusbar view and update contents - clean up do not stress (recreate statusbar) - Prevent to fix statusbar lag issues - Revert pie control (deprecated and breaking things) thanks to varun date - Revert edge gesture service (deprecated and breaking things) - Fix system ui NPE when device rotated - Don't refresh ui when screen off - org commit : eea9dcf - Fixed lockscreen clock and date color not updating after disable refresh ui when screen is off - Clean up scrollbars deprecated and breaking things

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -134,7 +134,6 @@ import com.android.settings.ButtonSettings;
 import com.android.settings.rr.MainSettings;
 import com.android.settings.rr.fragments.AppCircleBar;
 import com.android.settings.rr.gestureanywhere.GestureAnywhereSettings;
-import com.android.settings.rr.Pie.PieControl;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.widget.SwitchBar;
@@ -302,7 +301,6 @@ public class SettingsActivity extends Activity
 	    R.id.button_settings,
 	    R.id.app_circle_bar,
 	    R.id.gesture_anywhere,
-	    R.id.pie_control,
             R.id.dashboard,
             R.id.privacy_settings_cyanogenmod
     };
@@ -326,8 +324,7 @@ public class SettingsActivity extends Activity
             HomeSettings.class.getName(),
             MainSettings.class.getName(),
             AppCircleBar.class.getName(),
-	        GestureAnywhereSettings.class.getName(),	
-            PieControl.class.getName(),	
+	    GestureAnywhereSettings.class.getName(),	
             ButtonSettings.class.getName(),
             DisplaySettings.class.getName(),
             DeviceInfoSettings.class.getName(),
