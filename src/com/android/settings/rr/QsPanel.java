49957b889e8cffde1be74a0d4ce42c61fa2c6df2
Varun Date <date.varun123@gmail.com>
2016-03-30 10:13:31 +0530

    [Squashed] Add Back Qs Customizations After DragPanel Squash
    Credits to AICP,DU

--- src/com/android/settings/rr/QsPanel.java
+++ src/com/android/settings/rr/QsPanel.java
@@ -47,10 +47,12 @@ public class QsPanel extends SettingsPreferenceFragment  implements Preference.O
  private static final String PREF_BLOCK_ON_SECURE_KEYGUARD = "block_on_secure_keyguard";
  private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
  private static final String PREF_SMART_PULLDOWN = "smart_pulldown";
+ private static final String PREF_NUM_COLUMNS = "sysui_qs_num_columns";
 
     private SwitchPreference mBlockOnSecureKeyguard;
     private ListPreference mQuickPulldown;
     private ListPreference mSmartPulldown;
+    private ListPreference mNumColumns;	
     	
     private static final int MY_USER_ID = UserHandle.myUserId();
     @Override
@@ -94,6 +96,15 @@ int quickPulldown = CMSettings.System.getInt(resolver,
                 Settings.System.QS_SMART_PULLDOWN, 0);
         mSmartPulldown.setValue(String.valueOf(smartPulldown));
         updateSmartPulldownSummary(smartPulldown);
+        
+	 // Number of QS Columns 3,4,5
+            mNumColumns = (ListPreference) findPreference(PREF_NUM_COLUMNS);
+            int numColumns = Settings.System.getIntForUser(resolver,
+                    Settings.System.QS_NUM_TILE_COLUMNS, getDefaultNumColums(),
+                    UserHandle.USER_CURRENT);
+            mNumColumns.setValue(String.valueOf(numColumns));
+            updateNumColumnsSummary(numColumns);
+            mNumColumns.setOnPreferenceChangeListener(this);
 
     }
 
@@ -136,7 +147,13 @@ int quickPulldown = CMSettings.System.getInt(resolver,
             Settings.System.putInt(resolver, Settings.System.QS_SMART_PULLDOWN, smartPulldown);
             updateSmartPulldownSummary(smartPulldown);
             return true;
-	}
+	} else if (preference == mNumColumns) {
+                int numColumns = Integer.valueOf((String) newValue);
+                Settings.System.putIntForUser(resolver, Settings.System.QS_NUM_TILE_COLUMNS,
+                        numColumns, UserHandle.USER_CURRENT);
+                updateNumColumnsSummary(numColumns);
+                return true;
+         }       
          return false;
 	}
 
@@ -164,5 +181,24 @@ int quickPulldown = CMSettings.System.getInt(resolver,
             mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
         }
     }
+    
+      private void updateNumColumnsSummary(int numColumns) {
+            String prefix = (String) mNumColumns.getEntries()[mNumColumns.findIndexOfValue(String
+                    .valueOf(numColumns))];
+            mNumColumns.setSummary(getResources().getString(R.string.qs_num_columns_showing, prefix));
+        }
+
+        private int getDefaultNumColums() {
+            try {
+                Resources res = getActivity().getPackageManager()
+                        .getResourcesForApplication("com.android.systemui");
+                int val = res.getInteger(res.getIdentifier("quick_settings_num_columns", "integer",
+                        "com.android.systemui")); // better not be larger than 5, that's as high as the
+                                                  // list goes atm
+                return Math.max(1, val);
+            } catch (Exception e) {
+                return 3;
+            }
+        }
 }
 
