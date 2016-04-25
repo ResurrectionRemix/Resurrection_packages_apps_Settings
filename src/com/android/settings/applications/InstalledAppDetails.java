83725a8a8d28f938ef1246e3df955659700a7610
Roman Birg <roman@cyngn.com>
2016-04-25 12:00:02 -0700

    Refresh UI on force stop/disable
    Fixes the storage summary not updating after force closing, enabling,
    or disabling the app.
    
    Change-Id: I000459d27d06071a36ab0ac6761c1af56c132dde
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -898,6 +898,9 @@ public class InstalledAppDetails extends AppInfoBase
 
     @VisibleForTesting
     void checkForceStop() {
+        if (getActivity() == null || getActivity().isFinishing()) {
+            return;
+        }
         if (mDpm.packageHasActiveAdmins(mPackageInfo.packageName)) {
             // User can't force stop device admin.
             Log.w(LOG_TAG, "User can't force stop device admin");
@@ -1407,6 +1410,13 @@ public class InstalledAppDetails extends AppInfoBase
             mPm.setApplicationEnabledSetting(mInfo.packageName, mState, 0);
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            if (mActivity.get() !=  null) {
+                mActivity.get().refreshUi();
+            }
+        }
     }
 
     private final LoaderCallbacks<ChartData> mDataCallbacks = new LoaderCallbacks<ChartData>() {
@@ -1435,6 +1445,7 @@ public class InstalledAppDetails extends AppInfoBase
             Log.d(LOG_TAG, "Got broadcast response: Restart status for "
                     + mAppEntry.info.packageName + " " + enabled);
             updateForceStopButton(enabled);
+            refreshUi();
         }
     };
 
