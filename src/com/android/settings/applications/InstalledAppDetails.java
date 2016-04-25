e03e87689b6917178af3ca7090d3f0a057b7fb80
Roman Birg <roman@cyngn.com>
2016-04-25 12:00:02 -0700

    Settings: refresh ui on foce stop/diable
    Fixes the storage summary not updating after force closing, enabling,
    or disabling the app.
    
    Ticket: CYNGNOS-2527
    
    Change-Id: I000459d27d06071a36ab0ac6761c1af56c132dde
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/applications/InstalledAppDetails.java
+++ src/com/android/settings/applications/InstalledAppDetails.java
@@ -709,6 +709,10 @@ public class InstalledAppDetails extends AppInfoBase
     }
 
     private void checkForceStop() {
+        if (getActivity() == null || getActivity().isFinishing()) {
+            return;
+        }
+
         if (mDpm.packageHasActiveAdmins(mPackageInfo.packageName)) {
             // User can't force stop device admin.
             updateForceStopButton(false);
@@ -990,6 +994,13 @@ public class InstalledAppDetails extends AppInfoBase
             mPm.setApplicationEnabledSetting(mInfo.packageName, mState, 0);
             return null;
         }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            if (mActivity.get() !=  null) {
+                mActivity.get().refreshUi();
+            }
+        }
     }
 
     private final LoaderCallbacks<ChartData> mDataCallbacks = new LoaderCallbacks<ChartData>() {
@@ -1015,6 +1026,7 @@ public class InstalledAppDetails extends AppInfoBase
         @Override
         public void onReceive(Context context, Intent intent) {
             updateForceStopButton(getResultCode() != Activity.RESULT_CANCELED);
+            refreshUi();
         }
     };
 
