7b977f23c43d65715a622c1bb56ff274debb0196
Roman Birg <roman@cyngn.com>
2016-04-26 15:53:43 -0700

    Settings: restore proper live display color profile
    Select currently selected, not default, profile.
    
    Ticket: CYNGNOS-2575
    
    Change-Id: I8db23feefd88776949fe0af356b320b8b4a7b8cb
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/com/android/settings/livedisplay/LiveDisplay.java
+++ src/com/android/settings/livedisplay/LiveDisplay.java
@@ -232,7 +232,8 @@ public class LiveDisplay extends SettingsPreferenceFragment implements
             return false;
         }
 
-        final DisplayMode cur = mHardware.getDefaultDisplayMode();
+        final DisplayMode cur = mHardware.getCurrentDisplayMode() != null
+                ? mHardware.getCurrentDisplayMode() : mHardware.getDefaultDisplayMode();
         int curId = -1;
         String[] entries = new String[modes.length];
         String[] values = new String[modes.length];
@@ -252,7 +253,7 @@ public class LiveDisplay extends SettingsPreferenceFragment implements
             mColorProfileSummaries[i] = summary;
 
             if (cur != null && modes[i].id == cur.id) {
-                curId = -1;
+                curId = cur.id;
             }
         }
         mColorProfile.setEntries(entries);
@@ -270,7 +271,8 @@ public class LiveDisplay extends SettingsPreferenceFragment implements
         }
 
         if (value == null) {
-            DisplayMode cur = mHardware.getDefaultDisplayMode();
+            DisplayMode cur = mHardware.getCurrentDisplayMode() != null
+                    ? mHardware.getCurrentDisplayMode() : mHardware.getDefaultDisplayMode();
             if (cur != null && cur.id >= 0) {
                 value = String.valueOf(cur.id);
             }
