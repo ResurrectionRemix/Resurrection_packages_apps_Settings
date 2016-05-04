78aaa916c0e370aac1b4e9e97f16d4e4410aac97
Matadeen Mishra <matade@codeaurora.org>
2016-05-04 09:55:49 +0530

    Bluetooth: GAP: Handled Null check for getbluetoothclass
    Handled Null check for getbluetoothclass in BluetoothPairingDialog
    
    Change-Id: I549b3cd997b2749c7f0d0dd01a842267cdd20311
    CRs-Fixed: 920191

--- src/com/android/settings/bluetooth/BluetoothPairingDialog.java
+++ src/com/android/settings/bluetooth/BluetoothPairingDialog.java
@@ -201,8 +201,8 @@ public final class BluetoothPairingDialog extends AlertActivity implements
         } else if (mDevice.getPhonebookAccessPermission() == BluetoothDevice.ACCESS_REJECTED){
             contactSharing.setChecked(false);
         } else {
-            if (mDevice.getBluetoothClass().getDeviceClass()
-                    == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE) {
+            if ((mDevice.getBluetoothClass() != null) && (mDevice.getBluetoothClass().getDeviceClass()
+                    == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE)) {
                 contactSharing.setChecked(true);
                 mDevice.setPhonebookAccessPermission(BluetoothDevice.ACCESS_ALLOWED);
             } else {
