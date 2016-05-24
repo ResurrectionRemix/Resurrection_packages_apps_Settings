00b2286abb2f3567a04d0e7277dbb236966a2db8
Keith Mok <kmok@cyngn.com>
2016-05-24 08:46:20 -0700

    Settings: Update oem unlocking status when dismiss
    When oem unlocking confirm dialog dismiss, need
    to update the checkbox status also.
    
    FEIJ-780
    
    Change-Id: I9b5c4446ecd9736051fb7b6c12ad22aa0d985a85

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1755,7 +1755,9 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         DialogInterface.OnClickListener onConfirmListener = new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Utils.setOemUnlockEnabled(getActivity(), true);
+                if (which == DialogInterface.BUTTON_POSITIVE) {
+                    Utils.setOemUnlockEnabled(getActivity(), true);
+                }
                 updateAllOptions();
             }
         };
@@ -1764,7 +1766,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                 .setTitle(R.string.confirm_enable_oem_unlock_title)
                 .setMessage(R.string.confirm_enable_oem_unlock_text)
                 .setPositiveButton(R.string.enable_text, onConfirmListener)
-                .setNegativeButton(android.R.string.cancel, null)
+                .setNegativeButton(android.R.string.cancel, onConfirmListener)
                 .create()
                 .show();
     }
