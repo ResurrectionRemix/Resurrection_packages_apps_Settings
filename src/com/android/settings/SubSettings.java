f89dcdd5ffce503b7772d9bf05f4d53a75c03785
jianhuac <jianhuac@codeaurora.org>
2016-07-06 16:38:36 +0800

    Settings: Fix for crashed in saved networks popup when rotate.
    Don't create new activity when rotate device.
    
    Change-Id: Id514d2b22ed77d075f18e7e62ffaf82a4be24704

--- src/com/android/settings/SubSettings.java
+++ src/com/android/settings/SubSettings.java
@@ -36,4 +36,5 @@ public class SubSettings extends SettingsActivity {
         return true;
     }
     public static class BluetoothSubSettings extends SubSettings { /* empty */ }
+    public static class SavedAccessPointsSubSettings extends SubSettings { /* empty */ }
 }
--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -93,6 +93,7 @@ import com.android.internal.app.UnlaunchableAppActivity;
 import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.UserIcons;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -586,7 +587,10 @@ public final class Utils extends com.android.settingslib.Utils {
         if (BluetoothSettings.class.getName().equals(fragmentName)) {
             intent.setClass(context, SubSettings.BluetoothSubSettings.class);
             intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_AS_SUBSETTING, true);
-         } else {
+         } else if(SavedAccessPointsWifiSettings.class.getName().equals(fragmentName)) {
+            intent.setClass(context, SubSettings.SavedAccessPointsSubSettings.class);
+            intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_AS_SUBSETTING, true);
+        }else {
              intent.setClass(context, SubSettings.class);
          }
         intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT, fragmentName);
