48e584dd8f34f09091713601418ce810f43b1cd0
Randall Rushing <randall.rushing@gmail.com>
2016-07-06 16:26:41 +0200

    Smartbar: save/restore profiles
    Change-Id: I7d1209a58e0131494968345a3c7f651f98e83df9
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/SmartbarSettings.java
+++ src/com/android/settings/rr/SmartbarSettings.java
@@ -16,6 +16,13 @@
 
 package com.android.settings.rr;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import android.app.AlertDialog;
@@ -26,15 +33,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.UserHandle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import android.provider.SearchIndexableResource;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -70,7 +88,18 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
     static final int DEFAULT = 0xffffffff;
 
     private static final int MENU_RESET = Menu.FIRST;
+    private static final int MENU_SAVE = Menu.FIRST + 1;
+    private static final int MENU_RESTORE = Menu.FIRST + 2;
+
     private static final int DIALOG_RESET_CONFIRM = 1;
+    private static final int DIALOG_RESTORE_PROFILE = 2;
+    private static final int DIALOG_SAVE_PROFILE = 3;
+    private static final String CONFIG_STORAGE = Environment.getExternalStorageDirectory()
+            + File.separator
+            + "smartbar_configs";
+    private static final String SMARTBAR_CONFIGS_PREFIX = "smartbar_config_";
+    private static final String KEY_SMARTBAR_BACKUP = "smartbar_profile_save";
+    private static final String KEY_SMARTBAR_RESTORE = "smartbar_profile_restore";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -126,6 +155,58 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
                 alertDialog.setNegativeButton(R.string.write_settings_off, null);
                 dialog = alertDialog.create();
                 break;
+            case DIALOG_RESTORE_PROFILE:
+                final ConfigAdapter configAdapter = new ConfigAdapter(getActivity(),
+                        getConfigFiles(CONFIG_STORAGE));
+                AlertDialog.Builder configDialog = new AlertDialog.Builder(getActivity());
+                configDialog.setTitle(R.string.smartbar_config_dialog_title);
+                configDialog.setNegativeButton(getString(android.R.string.cancel), null);
+                configDialog.setAdapter(configAdapter, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        String resultMsg;
+                        try {
+                            File configFile = (File) configAdapter.getItem(item);
+                            String config = getSmartbarConfigFromStorage(configFile);
+                            restoreConfig(getActivity(), config);
+                            resultMsg = getString(R.string.smartbar_config_restore_success_toast);
+                        } catch (Exception e) {
+                            resultMsg = getString(R.string.smartbar_config_restore_error_toast);
+                        }
+                        Toast.makeText(getActivity(), resultMsg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog = configDialog.create();
+                break;
+            case DIALOG_SAVE_PROFILE:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                final EditText input = new EditText(getActivity());
+                builder.setTitle(getString(R.string.smartbar_config_name_edit_dialog_title));
+                builder.setMessage(R.string.smartbar_config_name_edit_dialog_message);
+                builder.setView(input);
+                builder.setNegativeButton(getString(android.R.string.cancel), null);
+                builder.setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String inputText = input.getText().toString();
+                                if (TextUtils.isEmpty(inputText)) {
+                                    inputText = String.valueOf(android.text.format.DateFormat
+                                            .format(
+                                                    "yyyy-MM-dd_hh:mm:ss", new java.util.Date()));
+                                }
+                                String resultMsg;
+                                try {
+                                    String currentConfig = getCurrentConfig(getActivity());
+                                    backupSmartbarConfig(currentConfig, inputText);
+                                    resultMsg = getString(R.string.smartbar_config_backup_success_toast);
+                                } catch (Exception e) {
+                                    resultMsg = getString(R.string.smartbar_config_backup_error_toast);
+                                }
+                                Toast.makeText(getActivity(), resultMsg, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog = builder.create();
+                dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                break;
         }
         return dialog;
     }
@@ -144,6 +225,12 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
         menu.add(0, MENU_RESET, 0, R.string.reset)
                 .setIcon(com.android.internal.R.drawable.ic_menu_refresh)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, MENU_SAVE, 0, R.string.smartbar_backup_current_config_title)
+                .setIcon(R.drawable.ic_smartbar_save_profile)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(0, MENU_RESTORE, 0, R.string.smartbar_restore_config_title)
+                .setIcon(R.drawable.ic_smartbar_restore_profile)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
     }
 
     @Override
@@ -151,6 +238,12 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
         switch (item.getItemId()) {
             case MENU_RESET:
                 showDialog(DIALOG_RESET_CONFIRM);
+                return true;
+            case MENU_SAVE:
+                showDialog(DIALOG_SAVE_PROFILE);
+                return true;
+            case MENU_RESTORE:
+                showDialog(DIALOG_RESTORE_PROFILE);
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -211,6 +304,115 @@ public class SmartbarSettings extends SettingsPreferenceFragment implements
             return true;
          } 
         return false;
+	}
+
+    static class ConfigAdapter extends ArrayAdapter<File> {
+        private final ArrayList<File> mConfigFiles;
+        private final Context mContext;
+
+        public ConfigAdapter(Context context, ArrayList<File> files) {
+            super(context, android.R.layout.select_dialog_item, files);
+            mContext = context;
+            mConfigFiles = files;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemRow = convertView;
+            File f = mConfigFiles.get(position);
+            itemRow = ((LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                    .inflate(android.R.layout.select_dialog_item, null);
+            String name = f.getName();
+            if (name.startsWith(SMARTBAR_CONFIGS_PREFIX)) {
+                name = f.getName().substring(SMARTBAR_CONFIGS_PREFIX.length(), f.getName().length());
+            }
+            ((TextView) itemRow.findViewById(android.R.id.text1)).setText(name);
+
+            return itemRow;
+        }
+    }
+
+    private static class StartsWithFilter implements FileFilter {
+        private String[] mStartsWith;
+
+        public StartsWithFilter(String[] startsWith) {
+            mStartsWith = startsWith;
+        }
+
+        @Override
+        public boolean accept(File file) {
+            for (String extension : mStartsWith) {
+                if (file.getName().toLowerCase().startsWith(extension)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    static String getCurrentConfig(Context ctx) {
+        String config = Settings.Secure.getStringForUser(
+                ctx.getContentResolver(), ActionConstants.getDefaults(ActionConstants.SMARTBAR)
+                        .getUri(),
+                UserHandle.USER_CURRENT);
+        if (TextUtils.isEmpty(config)) {
+            config = ActionConstants.getDefaults(ActionConstants.SMARTBAR).getDefaultConfig();
+        }
+        return config;
+    }
+
+    static void restoreConfig(Context context, String config) {
+        Settings.Secure.putStringForUser(context.getContentResolver(),
+                ActionConstants.getDefaults(ActionConstants.SMARTBAR)
+                        .getUri(), config,
+                UserHandle.USER_CURRENT);
+        Intent intent = new Intent("intent_navbar_edit");
+        intent.putExtra("extra_navbar_edit_reset_layout", "resetMePlox");
+        context.sendBroadcastAsUser(intent, UserHandle.CURRENT);
+    }
+
+    static void backupSmartbarConfig(String config, String suffix) {
+        File dir = new File(CONFIG_STORAGE);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File configFile = new File(dir, SMARTBAR_CONFIGS_PREFIX + suffix);
+        FileOutputStream stream;
+        try {
+            stream = new FileOutputStream(configFile);
+            stream.write(config.getBytes());
+            stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getSmartbarConfigFromStorage(File file) {
+        int length = (int) file.length();
+        byte[] bytes = new byte[length];
+        FileInputStream in;
+        try {
+            in = new FileInputStream(file);
+            in.read(bytes);
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String contents = new String(bytes);
+        return contents;
+    }
+
+    public static ArrayList<File> getConfigFiles(String path) {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        ArrayList<File> list = new ArrayList<File>();
+        for (File tmp : dir.listFiles(new StartsWithFilter(new String[] {
+                SMARTBAR_CONFIGS_PREFIX
+        }))) {
+            list.add(tmp);
+        }
+        return list;
     }
 
     @Override
