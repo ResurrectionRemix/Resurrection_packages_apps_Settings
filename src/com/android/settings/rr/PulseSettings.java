f2e36df71f2bbd8b7d18f15fdbc2b34539f51b61
Varun Date <date.varun123@gmail.com>
2016-07-13 16:04:09 +0530

    Settings: Pulse Solid Bar Sanity Levels & Lavalamp Anim Speed [3/3]
    --Make sure either Category is disabled when one is enabled
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/PulseSettings.java
+++ src/com/android/settings/rr/PulseSettings.java
@@ -26,6 +26,7 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import android.app.ActionBar;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -38,6 +39,8 @@ import android.provider.Settings;
 import android.provider.SearchIndexableResource;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
+
+import com.android.settings.rr.SeekBarPreference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +55,9 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private static final String FUDGE_FACOR = "pulse_custom_fudge_factor";
     private static final int RENDER_STYLE_FADING_BARS = 0;
     private static final int RENDER_STYLE_SOLID_LINES = 1;
+    private static final String SOLID_FUDGE = "pulse_solid_fudge_factor";
+    private static final String SOLID_LAVAMP_SPEED = "lavamp_solid_speed";
+    private static final String FADING_LAVAMP_SPEED = "fling_pulse_lavalamp_speed";
 
     SwitchPreference mShowPulse;
     ListPreference mRenderMode;
@@ -62,6 +68,9 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     ListPreference mFilled;
     ListPreference mEmpty;
     ListPreference mFudge;
+    ListPreference mSolidFudge;
+    SeekBarPreference mSolidSpeed;
+    SeekBarPreference mFadingSpeed;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -138,6 +147,31 @@ public class PulseSettings extends SettingsPreferenceFragment implements
         mFudge.setSummary(mFudge.getEntry());
         mFudge.setOnPreferenceChangeListener(this);
 
+        PreferenceCategory solidBarsCat = (PreferenceCategory)findPreference("pulse_2");
+        solidBarsCat.setEnabled(renderMode == RENDER_STYLE_SOLID_LINES);
+
+        mSolidFudge = (ListPreference) findPreference(SOLID_FUDGE);
+        int solidfudge = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_SOLID_FUDGE_FACTOR, 0,
+                    UserHandle.USER_CURRENT);
+        mSolidFudge.setValue(String.valueOf(solidfudge));
+        mSolidFudge.setSummary(mSolidFudge.getEntry());
+        mSolidFudge.setOnPreferenceChangeListener(this);
+
+        mSolidSpeed =
+                    (SeekBarPreference) findPreference(SOLID_LAVAMP_SPEED);
+        int speed = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LAVAMP_SOLID_SPEED, 10000);
+        mSolidSpeed.setValue(speed / 1);
+        mSolidSpeed.setOnPreferenceChangeListener(this);
+
+        mFadingSpeed =
+                    (SeekBarPreference) findPreference(FADING_LAVAMP_SPEED);
+        int fspeed = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, 10000);
+        mFadingSpeed.setValue(fspeed / 1);
+        mFadingSpeed.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -148,6 +182,8 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                     Settings.Secure.PULSE_RENDER_STYLE_URI, mode, UserHandle.USER_CURRENT);
             PreferenceCategory fadingBarsCat = (PreferenceCategory)findPreference("pulse_1");
             fadingBarsCat.setEnabled(mode == RENDER_STYLE_FADING_BARS);
+            PreferenceCategory solidBarsCat = (PreferenceCategory)findPreference("pulse_2");
+            solidBarsCat.setEnabled(mode == RENDER_STYLE_SOLID_LINES);
             return true;
         } else if (preference.equals(mShowPulse)) {
             boolean enabled = ((Boolean) newValue).booleanValue();
@@ -215,7 +251,29 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 mFudge.setSummary(
                         mFudge.getEntries()[index]);
                 return true;
-	}
+	}  else if (preference == mSolidFudge) {
+                int fudge = Integer.valueOf((String) newValue);
+                int index = mSolidFudge.findIndexOfValue((String) newValue);
+                Settings.Secure.putIntForUser(
+                       getContentResolver(), 
+		Settings.Secure.PULSE_SOLID_FUDGE_FACTOR, fudge,
+                        UserHandle.USER_CURRENT);
+                mSolidFudge.setSummary(
+                        mSolidFudge.getEntries()[index]);
+                return true;
+	} else if (preference == mSolidSpeed) {
+		ContentResolver mResolver = getActivity().getContentResolver();
+                int val = (Integer) newValue;
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.LAVAMP_SOLID_SPEED, val * 1);
+                return true;
+        } else if (preference == mFadingSpeed) {
+		ContentResolver mResolver = getActivity().getContentResolver();
+                int val = (Integer) newValue;
+                Settings.Secure.putInt(mResolver,
+                        Settings.Secure.FLING_PULSE_LAVALAMP_SPEED, val * 1);
+                return true;
+        }
         return false;
     }
 
