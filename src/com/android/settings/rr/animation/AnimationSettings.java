83a5aac98a88b0c61d6760c8ff7b58e33bd171f8
rogersb11 <brettrogers11@gmail.com>
2016-08-05 12:35:40 -0600

    Custom Toast Color Options [1/2]
    - Toast Text Color
    https://github.com/CyanideL/android_packages_apps_Settings/commit/d35408d42498d7bab39c203b03aa6b17f325cee3
    
    - Toast Icon Color
    https://github.com/CyanideL/android_packages_apps_Settings/commit/89cabb50d7b920f537387c86dcb3eaf62cacb231

--- src/com/android/settings/rr/animation/AnimationSettings.java
+++ src/com/android/settings/rr/animation/AnimationSettings.java
@@ -38,6 +38,8 @@ import android.widget.Toast;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 import com.android.internal.logging.MetricsLogger;
 
 import java.util.ArrayList;
@@ -51,6 +53,8 @@ public class AnimationSettings extends SettingsPreferenceFragment
     private static final String TAG = "AnimationSettings";
 
     private static final String KEY_TOAST_ANIMATION = "toast_animation";
+    private static final String TOAST_ICON_COLOR = "toast_icon_color";
+    private static final String TOAST_TEXT_COLOR = "toast_text_color";
     private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
     private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
@@ -60,6 +64,8 @@ public class AnimationSettings extends SettingsPreferenceFragment
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
     private ListPreference mPowerMenuAnimations;
+    private ColorPickerPreference mIconColor;
+    private ColorPickerPreference mTextColor;
 
 
     @Override
@@ -74,12 +80,33 @@ public class AnimationSettings extends SettingsPreferenceFragment
         ContentResolver resolver = getActivity().getContentResolver();
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        int intColor = 0xffffffff;
+        String hexColor = String.format("#%08x", (0xffffffff & 0xffffffff));
+
         mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
         mToastAnimation.setSummary(mToastAnimation.getEntry());
         int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
         mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
         mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
         mToastAnimation.setOnPreferenceChangeListener(this);
+
+        mIconColor =
+                (ColorPickerPreference) findPreference(TOAST_ICON_COLOR);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.TOAST_ICON_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColor.setNewPreviewColor(intColor);
+        mIconColor.setSummary(hexColor);
+        mIconColor.setOnPreferenceChangeListener(this);
+
+        mTextColor =
+                (ColorPickerPreference) findPreference(TOAST_TEXT_COLOR);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.TOAST_TEXT_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mTextColor.setNewPreviewColor(intColor);
+        mTextColor.setSummary(hexColor);
+        mTextColor.setOnPreferenceChangeListener(this);
 
         mListViewAnimation = (ListPreference) prefSet.findPreference(KEY_LISTVIEW_ANIMATION);
         int listviewanimation = Settings.System.getInt(getContentResolver(),
@@ -138,6 +165,22 @@ public class AnimationSettings extends SettingsPreferenceFragment
             mPowerMenuAnimations.setValue(String.valueOf(newValue));
             mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
             return true;
+        } else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TOAST_ICON_COLOR, intHex);
+             return true;
+        } else if (preference == mTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TOAST_TEXT_COLOR, intHex);
+             return true;
         }
         return false;
     }
