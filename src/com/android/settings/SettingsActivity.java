d0f136527522d1106a2a6a471ce68762a32d87b4
Steve Kondik <steve@cyngn.com>
2016-09-01 21:43:22 -0700

    Revert "Telephony(MSIM): Support to launch SimSettings from vendor space"
     * This is silly to be delegated to a blob.
    
    This reverts commit 188be8eef8f3bd3235798fb8a864ee3b29439e10.
    
    Change-Id: If76a4cf992e9368b21d4d6d42d085020cd122c33

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1048,15 +1048,6 @@ public class SettingsActivity extends SettingsDrawerActivity
      */
     private Fragment switchToFragment(String fragmentName, Bundle args, boolean validate,
             boolean addToBackStack, int titleResId, CharSequence title, boolean withTransition) {
-        if (fragmentName.equals(SimSettings.class.getName())){
-            Log.i(LOG_TAG, "switchToFragment, launch simSettings  ");
-            Intent provisioningIntent =
-                    new Intent("com.android.settings.sim.SIM_SUB_INFO_SETTINGS");
-            startActivity(provisioningIntent);
-            finish();
-            return null;
-        }
-
         if (LTE_4G_FRAGMENT.equals(fragmentName)) {
             Intent newIntent = new Intent("android.settings.SETTINGS");
             newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
