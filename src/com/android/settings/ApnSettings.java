SlimRoms/packages_apps_Settings

MSIM: Select preferred apn based on subscription id.
While querying and setting preferred apn in APN settings,
use the PREFERRED_APN_URI based on current subscription id.
This will make sure that the preferred apn is selected
correctly based on the SIM card selected.

Change-Id: I826295f993d99b6f1fafbab9ad7dba9cad04ffbb
CRs-Fixed: 958420

src/com/android/settings/ApnSettings.java
@@ -156,6 +156,7 @@ public class ApnSettings extends SettingsPreferenceFragment implements
         final Activity activity = getActivity();
         final int subId = activity.getIntent().getIntExtra(SUB_ID,
                 SubscriptionManager.INVALID_SUBSCRIPTION_ID);
+        Log.d(TAG, "onCreate: subId = " + subId);
 
         mUm = (UserManager) getSystemService(Context.USER_SERVICE);
 
@@ -408,13 +409,13 @@ public class ApnSettings extends SettingsPreferenceFragment implements
 
         ContentValues values = new ContentValues();
         values.put(APN_ID, mSelectedKey);
-        resolver.update(PREFERAPN_URI, values, null, null);
+        resolver.update(getUri(PREFERAPN_URI), values, null, null);
     }
 
     private String getSelectedApnKey() {
         String key = null;
 
-        Cursor cursor = getContentResolver().query(PREFERAPN_URI, new String[] {"_id"},
+        Cursor cursor = getContentResolver().query(getUri(PREFERAPN_URI), new String[] {"_id"},
                 null, null, Telephony.Carriers.DEFAULT_SORT_ORDER);
         if (cursor.getCount() > 0) {
             cursor.moveToFirst();
@@ -483,7 +484,7 @@ public class ApnSettings extends SettingsPreferenceFragment implements
             switch (msg.what) {
                 case EVENT_RESTORE_DEFAULTAPN_START:
                     ContentResolver resolver = getContentResolver();
-                    resolver.delete(DEFAULTAPN_URI, null, null);
+                    resolver.delete(getUri(DEFAULTAPN_URI), null, null);
                     mRestoreApnUiHandler
                         .sendEmptyMessage(EVENT_RESTORE_DEFAULTAPN_COMPLETE);
                     break;
@@ -500,5 +501,14 @@ public class ApnSettings extends SettingsPreferenceFragment implements
             return dialog;
         }
         return null;
+    }
+
+    private Uri getUri(Uri uri) {
+        int subId = SubscriptionManager.getDefaultDataSubId();
+        if (mSubscriptionInfo != null && SubscriptionManager.isValidSubscriptionId(
+                mSubscriptionInfo.getSubscriptionId())) {
+            subId = mSubscriptionInfo.getSubscriptionId();
+        }
+        return Uri.withAppendedPath(uri, "/subId/" + subId);
     }
 }

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/ApnSettings.java
@@ -168,6 +168,8 @@ public class ApnSettings extends RestrictedSettingsFragment implements
         final int subId = activity.getIntent().getIntExtra(SUB_ID,
                 SubscriptionManager.INVALID_SUBSCRIPTION_ID);
         fillOperatorIccidset();
+        Log.d(TAG, "onCreate: subId = " + subId);
+
 
         mMobileStateFilter = new IntentFilter(
                 TelephonyIntents.ACTION_ANY_DATA_CONNECTION_STATE_CHANGED);
@@ -519,13 +521,13 @@ public class ApnSettings extends RestrictedSettingsFragment implements
 
         ContentValues values = new ContentValues();
         values.put(APN_ID, mSelectedKey);
-        resolver.update(PREFERAPN_URI, values, null, null);
+        resolver.update(getUri(PREFERAPN_URI), values, null, null);
     }
 
     private String getSelectedApnKey() {
         String key = null;
 
-        Cursor cursor = getContentResolver().query(PREFERAPN_URI, new String[] {"_id"},
+        Cursor cursor = getContentResolver().query(getUri(PREFERAPN_URI), new String[] {"_id"},
                 null, null, Telephony.Carriers.DEFAULT_SORT_ORDER);
         if (cursor.getCount() > 0) {
             cursor.moveToFirst();
@@ -596,7 +598,7 @@ public class ApnSettings extends RestrictedSettingsFragment implements
             switch (msg.what) {
                 case EVENT_RESTORE_DEFAULTAPN_START:
                     ContentResolver resolver = getContentResolver();
-                    resolver.delete(DEFAULTAPN_URI, null, null);
+                    resolver.delete(getUri(DEFAULTAPN_URI), null, null);
                     mRestoreApnUiHandler
                         .sendEmptyMessage(EVENT_RESTORE_DEFAULTAPN_COMPLETE);
                     break;
@@ -617,5 +619,14 @@ public class ApnSettings extends RestrictedSettingsFragment implements
             return dialog;
         }
         return null;
+    }
+
+    private Uri getUri(Uri uri) {
+        int subId = SubscriptionManager.getDefaultDataSubscriptionId();
+        if (mSubscriptionInfo != null && SubscriptionManager.isValidSubscriptionId(
+                mSubscriptionInfo.getSubscriptionId())) {
+            subId = mSubscriptionInfo.getSubscriptionId();
+        }
+        return Uri.withAppendedPath(uri, "/subId/" + subId);
     }
 }