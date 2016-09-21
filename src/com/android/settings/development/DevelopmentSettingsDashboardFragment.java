fb39ab8702643cd8bb9127bf4d1bbf99a731b93c
ezio84 <brabus84@gmail.com>
2016-09-21 13:06:12 +0200

    Settings: Move custom DP settings under display submenu
    Change-Id: I303ca5a00e555e5cf3bd3b488aba0314ec7bba1b

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -521,7 +521,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new DefaultLaunchPreferenceController(context, "feature_flags_dashboard"));
         controllers.add(
             new DefaultLaunchPreferenceController(context, "default_usb_configuration"));
-        controllers.add(new DefaultLaunchPreferenceController(context, "density"));
+        //controllers.add(new DefaultLaunchPreferenceController(context, "density"));
         controllers.add(new DefaultLaunchPreferenceController(context, "background_check"));
         controllers.add(new DefaultLaunchPreferenceController(context, "inactive_apps"));
         controllers.add(new AutofillLoggingLevelPreferenceController(context, lifecycle));
