b80fcc63e4a6ac56bb0ac721f8d7b3561f0bb09c
Varun Date <date.varun123@gmail.com>
2016-10-17 17:14:50 +0530

    Fix Mis Merge
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/MainSettingsLayout.java
+++ src/com/android/settings/rr/MainSettingsLayout.java
@@ -182,12 +182,10 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
             frags[5] = new RRGestures();
             frags[6] = new ButtonSettings();
             frags[7] = new AnimationSettings();
-            frags[8] = new UISettings();
-            frags[9] = new MiscSettings();
-			frags[10] = new About();
+            frags[8] = new MiscSettings();
+			frags[9] = new About();
         	} else {
 		    frags[0] = new MainSettings();
-			frags[1] = new About();
 			}
 		}
 
@@ -214,20 +212,18 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
 		if (which == 0) {
         titleString = new String[]{
                 getString(R.string.rr_statusbar_title),
-                getString(R.string.rr_notification_panel_title),
+                getString(R.string.rr_np_title),
                 getString(R.string.recents_settings_title),
-				getString(R.string.rr_qs_title),
+				getString(R.string.rr_qs_panel_title),
 				getString(R.string.rr_lockscreen_title),
                 getString(R.string.gestures_settings),
                 getString(R.string.button_pref_title),
-                getString(R.string.animation_title),
-                getString(R.string.rr_ui_title),
+                getString(R.string.rr_animation_title),
                 getString(R.string.rr_misc_title),
                 getString(R.string.about_rr_settings)};
 		} else {
 				titleString = new String[]{
-				getString(R.string.rr_title),
-				getString(R.string.about_rr_settings) };
+				getString(R.string.rr_title)};
 		}
         return titleString;
     }
--- src/com/android/settings/rr/input/ButtonSettings.java
+++ src/com/android/settings/rr/input/ButtonSettings.java
@@ -211,6 +211,28 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         final CMHardwareManager hardware = CMHardwareManager.getInstance(getActivity());
 
+ 		// Only visible on devices that does not have a navigation bar already,
+        // and don't even try unless the existing keys can be disabled
+        boolean needsNavigationBar = false;
+        if (hardware.isSupported(CMHardwareManager.FEATURE_KEY_DISABLE)) {
+            try {
+                IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+                needsNavigationBar = wm.needsNavigationBar();
+             } catch (RemoteException e) {
+             }
+ 
+             if (needsNavigationBar) {
+                 prefScreen.removePreference(mDisableNavigationKeys);
+             } else {
+                 // Remove keys that can be provided by the navbar
+                 updateDisableNavkeysOption();
+                 updateDisableNavkeysCategories(mDisableNavigationKeys.isChecked());
+             }
+         } else {
+             prefScreen.removePreference(mDisableNavigationKeys);
+         }
+
+
         if (hasPowerKey) {
             if (!TelephonyUtils.isVoiceCapable(getActivity())) {
                 powerCategory.removePreference(mPowerEndCall);
