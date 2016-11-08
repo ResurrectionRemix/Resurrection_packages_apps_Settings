17d5758188eb7eed1a615152b8669fe390d22378
Alex Cruz <mazdarider23@gmail.com>
2016-11-08 11:30:29 +0530

    {SQUASHED} ScreenShot Delay and Type [2/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/UISettings.java
+++ src/com/android/settings/rr/UISettings.java
@@ -13,10 +13,12 @@
 */
 package com.android.settings.rr;
 
-import android.os.Bundle;
-import android.content.ContentResolver;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
@@ -31,12 +33,18 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
+import com.android.settings.rr.SeekBarPreference;
+
 public class UISettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "UI";
     private static final String KEY_FONT_SIZE = "font_size";
+    private static final String SCREENSHOT_TYPE = "screenshot_type";
+    private static final String SCREENSHOT_DELAY = "screenshot_delay";
 
     private Preference mFontSizePref;
+    private ListPreference mScreenshotType;
+    private SeekBarPreference mScreenshotDelay;
 
     @Override
     protected int getMetricsCategory() {
@@ -47,12 +55,41 @@ public class UISettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+		ContentResolver resolver = getActivity().getContentResolver();
+
         addPreferencesFromResource(R.xml.rr_ui_settings);
  		mFontSizePref = findPreference(KEY_FONT_SIZE);
+
+        mScreenshotType = (ListPreference) findPreference(SCREENSHOT_TYPE);
+        int mScreenshotTypeValue = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.SCREENSHOT_TYPE, 0);
+        mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
+        mScreenshotType.setSummary(mScreenshotType.getEntry());
+        mScreenshotType.setOnPreferenceChangeListener(this);
+
+        mScreenshotDelay = (SeekBarPreference) findPreference(SCREENSHOT_DELAY);
+        int screenshotDelay = Settings.System.getInt(resolver,
+                Settings.System.SCREENSHOT_DELAY, 1000);
+        mScreenshotDelay.setValue(screenshotDelay / 1);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return true;
+        if  (preference == mScreenshotType) {
+            int mScreenshotTypeValue = Integer.parseInt(((String) objValue).toString());
+            mScreenshotType.setSummary(
+                    mScreenshotType.getEntries()[mScreenshotTypeValue]);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.SCREENSHOT_TYPE, mScreenshotTypeValue);
+            mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
+            return true;
+        } else if (preference == mScreenshotDelay) {
+            int screenshotDelay = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+         }
+        return false;
     }
 
     private void updateFontSizeSummary() {
