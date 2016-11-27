447d9ad63171025d4b14b99ed2dff30689703993
Meninblack007 <sanyam.53jain@gmail.com>
2016-11-27 13:53:16 +0000

    Settings: Fix selinux switch
    * Cleanup whitespaces
    * Use SharedPreference to save values
    
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- src/com/android/settings/rr/util/OnBoot.java
+++ src/com/android/settings/rr/util/OnBoot.java
@@ -5,38 +5,49 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.Toast;
-import java.io.IOException;
 import java.util.List;
 
 import com.android.settings.util.CMDProcessor;
 
 public class OnBoot extends BroadcastReceiver {
 
-    Context settingsContext = null;
     private static final String TAG = "RR_onboot";
-    Boolean mSetupRunning = false;
+    private boolean mSetupRunning = false;
+    private Context mContext;
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        mContext = context;
         ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
-        for(int i = 0; i < procInfos.size(); i++)
-        {
+        for(int i = 0; i < procInfos.size(); i++) {
             if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
                 mSetupRunning = true;
             }
         }
         if(!mSetupRunning) {
-             SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(context);
-             if(sharedpreferences.getBoolean("selinux", true)) {
-                 CMDProcessor.runShellCommand("setenforce 1");
-             } else if (!sharedpreferences.getBoolean("selinux", true)) {
-                 CMDProcessor.runSuCommand("setenforce 0");
-             }
+            SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref", Context.MODE_PRIVATE);
+            String isSelinuxEnforcing = sharedpreferences.getString("selinux", null);
+            if (isSelinuxEnforcing != null) {
+                if (isSelinuxEnforcing.equals("true")) {
+                    CMDProcessor.runSuCommand("setenforce 1");
+                } else if (isSelinuxEnforcing.equals("false")) {
+                    CMDProcessor.runSuCommand("setenforce 0");
+                }
+            } else {
+                if (CMDProcessor.runShellCommand("getenforce").getStdout().contains("Enforcing")) {
+                    setSelinuxEnabled("true");
+                } else {
+                    setSelinuxEnabled("false");
+                }
+            }
         }
     }
 
+    private void setSelinuxEnabled(String status) {
+        SharedPreferences.Editor editor = mContext.getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+        editor.putString("selinux", status);
+        editor.apply();
+    }
 }
