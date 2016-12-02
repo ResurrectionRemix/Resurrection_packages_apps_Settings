5bed03506a6b95a53c94839e08c9d72046a5575b
martincz <martincz@mokeedev.com>
2016-12-02 19:54:43 +0530

    Add volume button call answer switch
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/input/ButtonSettings.java
+++ src/com/android/settings/rr/input/ButtonSettings.java
@@ -379,6 +379,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                 volumeCategory.removePreference(findPreference(CMSettings.System.VOLUME_WAKE_SCREEN));
             }
 
+            if (!TelephonyUtils.isVoiceCapable(getActivity())) {
+                volumeCategory.removePreference(
+                        findPreference(CMSettings.System.VOLUME_ANSWER_CALL));
+            }
+
             int cursorControlAction = Settings.System.getInt(resolver,
                     Settings.System.VOLUME_KEY_CURSOR_CONTROL, 0);
             mVolumeKeyCursorControl = initActionList(KEY_VOLUME_KEY_CURSOR_CONTROL,
