LineageOS/android_packages_apps_Settings

fingerpint: Allow devices to configure sensor location
The text for enrolling a fingerprint always assumes the sensor is
located on the back.  This patch adds a config integer that defines
where the sensor is located on the device.
  0 = back
  1 = front
  2 = left side
  3 = right side

This patch also adds a new drawable that shows the front of the device
when applicable.

Change-Id: Ia8f5f8ff4ba4d13d35aed3052f60ff665dd4f294
TICKET: CYNGNOS-3089

src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
@@ -19,6 +19,8 @@ package com.android.settings.fingerprint;
 import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.settings.ChooseLockSettingsHelper;
@@ -31,6 +33,10 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
 
     private static final int CONFIRM_REQUEST = 1;
     private static final int ENROLLING = 2;
+    private static final int SENSOR_LOCATION_BACK = 0;
+    private static final int SENSOR_LOCATION_FRONT = 1;
+    private static final int SENSOR_LOCATION_LEFT = 2;
+    private static final int SENSOR_LOCATION_RIGHT = 3;
     public static final String EXTRA_KEY_LAUNCHED_CONFIRM = "launched_confirm_lock";
 
     private FingerprintLocationAnimationView mAnimation;
@@ -48,6 +54,21 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
         }
         mAnimation = (FingerprintLocationAnimationView) findViewById(
                 R.id.fingerprint_sensor_location_animation);
+
+        int sensorLocation = getResources().getInteger(R.integer.config_fingerprintSensorLocation);
+        if (sensorLocation < SENSOR_LOCATION_BACK || sensorLocation > SENSOR_LOCATION_RIGHT) {
+            sensorLocation = SENSOR_LOCATION_BACK;
+        }
+        final String location = getResources().getStringArray(
+                R.array.security_settings_fingerprint_sensor_locations)[sensorLocation];
+        TextView message = (TextView) findViewById(R.id.find_sensor_message);
+        message.setText(getString(
+                R.string.security_settings_fingerprint_enroll_find_sensor_message,
+                location));
+        if (sensorLocation != SENSOR_LOCATION_BACK) {
+            findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                    .setVisibility(View.VISIBLE);
+        }
     }
 
     @Override

ResurrectionRemix/Resurrection_packages_apps_Settings

Conflicts:
	res/values/cm_arrays.xml
	res/values/cm_strings.xml

src/com/android/settings/fingerprint/FingerprintEnrollFindSensor.java
@@ -20,6 +20,8 @@ import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.ChooseLockSettingsHelper;
@@ -33,6 +35,10 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
 
     private static final int CONFIRM_REQUEST = 1;
     private static final int ENROLLING = 2;
+    private static final int SENSOR_LOCATION_BACK = 0;
+    private static final int SENSOR_LOCATION_FRONT = 1;
+    private static final int SENSOR_LOCATION_LEFT = 2;
+    private static final int SENSOR_LOCATION_RIGHT = 3;
     public static final String EXTRA_KEY_LAUNCHED_CONFIRM = "launched_confirm_lock";
 
     private FingerprintFindSensorAnimation mAnimation;
@@ -57,6 +63,21 @@ public class FingerprintEnrollFindSensor extends FingerprintEnrollBase {
         }
         mAnimation = (FingerprintFindSensorAnimation) findViewById(
                 R.id.fingerprint_sensor_location_animation);
+
+        int sensorLocation = getResources().getInteger(R.integer.config_fingerprintSensorLocation);
+        if (sensorLocation < SENSOR_LOCATION_BACK || sensorLocation > SENSOR_LOCATION_RIGHT) {
+            sensorLocation = SENSOR_LOCATION_BACK;
+        }
+        final String location = getResources().getStringArray(
+                R.array.security_settings_fingerprint_sensor_locations)[sensorLocation];
+        TextView message = (TextView) findViewById(R.id.find_sensor_message);
+        message.setText(getString(
+                R.string.security_settings_fingerprint_enroll_find_sensor_message_cm,
+                location));
+        if (sensorLocation != SENSOR_LOCATION_BACK) {
+            findViewById(R.id.fingerprint_sensor_location_front_overlay)
+                    .setVisibility(View.VISIBLE);
+        }
     }
 
     protected int getContentView() {