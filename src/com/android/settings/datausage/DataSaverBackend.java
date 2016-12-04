13d4602dac230e5c2a0e37147a17872466455091
Uldiniad <olivercscott@gmail.com>
2016-12-03 20:11:00 -0500

    Settings: per-app cellular data, vpn and wifi restrictions
    *) Add options to disable all cellular, vpn and wifi data
       in app data usage settings.
    
    *) Disable the existing background data and unrestricted
       data usage options when all cellular data access is
       disabled.
    
    *) The vpn data option can be selected independently from state of
       all Wi-Fi and mobile data access enable/disable.
    
    *) Prevent DataSaverBackend from overwriting uid policies
    
    This is a replacement for the appops menu based cell/wifi data
    restriction settings in cm-13.0:
    Author: Danesh M <daneshm90@gmail.com>
    Date:   Mon Mar 7 15:17:59 2016 -0800
        Settings : Add per app internet/data control
        CYAN-3976
        CRACKLING-834
        Change-Id: I13192df837c057b5cadde8f31532e12daaf3c1b0
    
    Change-Id: Ic087c27a5ed0bdb84bb8f297c425c6bcbffec848
    (cherry picked from commit d4a2eea698cbc4636a635d60f2a52ec1bbc36ba2)

--- src/com/android/settings/datausage/DataSaverBackend.java
+++ src/com/android/settings/datausage/DataSaverBackend.java
@@ -82,12 +82,15 @@ public class DataSaverBackend {
 
     public void setIsWhitelisted(int uid, String packageName, boolean whitelisted) {
         final int policy = whitelisted ? POLICY_ALLOW_METERED_BACKGROUND : POLICY_NONE;
-        mPolicyManager.setUidPolicy(uid, policy);
         mUidPolicies.put(uid, policy);
         if (whitelisted) {
+            mPolicyManager.addUidPolicy(uid, POLICY_ALLOW_METERED_BACKGROUND);
             mMetricsFeatureProvider.action(
                     mContext, SettingsEnums.ACTION_DATA_SAVER_WHITELIST, packageName);
+        } else {
+            mPolicyManager.removeUidPolicy(uid, POLICY_ALLOW_METERED_BACKGROUND);
         }
+        mPolicyManager.removeUidPolicy(uid, POLICY_REJECT_METERED_BACKGROUND);
     }
 
     public boolean isWhitelisted(int uid) {
@@ -112,12 +115,15 @@ public class DataSaverBackend {
 
     public void setIsBlacklisted(int uid, String packageName, boolean blacklisted) {
         final int policy = blacklisted ? POLICY_REJECT_METERED_BACKGROUND : POLICY_NONE;
-        mPolicyManager.setUidPolicy(uid, policy);
         mUidPolicies.put(uid, policy);
         if (blacklisted) {
+            mPolicyManager.addUidPolicy(uid, POLICY_REJECT_METERED_BACKGROUND);
             mMetricsFeatureProvider.action(
                     mContext, SettingsEnums.ACTION_DATA_SAVER_BLACKLIST, packageName);
+        } else {
+            mPolicyManager.removeUidPolicy(uid, POLICY_REJECT_METERED_BACKGROUND);
         }
+        mPolicyManager.removeUidPolicy(uid, POLICY_ALLOW_METERED_BACKGROUND);
     }
 
     public boolean isBlacklisted(int uid) {
@@ -156,6 +162,10 @@ public class DataSaverBackend {
     private void handleUidPoliciesChanged(int uid, int newPolicy) {
         loadWhitelist();
         loadBlacklist();
+
+        // We only care about allow/reject metered background policy here.
+        newPolicy &= (POLICY_ALLOW_METERED_BACKGROUND
+                | POLICY_REJECT_METERED_BACKGROUND);
 
         final int oldPolicy = mUidPolicies.get(uid, POLICY_NONE);
         if (newPolicy == POLICY_NONE) {
