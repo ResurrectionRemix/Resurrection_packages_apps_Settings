f7cd8e46c41e66eaa29a18fb4a6767b3e1cf2a64
rogersb11 <brettrogers11@gmail.com>
2016-12-24 11:36:10 +0530

    Settings: Custom Toast Color Options
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/ToastAnimations.java
+++ src/com/android/settings/rr/ToastAnimations.java
@@ -26,6 +26,7 @@ import android.support.v7.preference.PreferenceScreen;
 import android.widget.Toast;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,10 +35,15 @@ import com.android.settings.Utils;
 public class ToastAnimations extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "ToastAnimations";
-	private static final String KEY_TOAST_ANIMATION = "toast_animation";
-	private ListPreference mToastAnimation;
-	protected Context mContext;
-	protected ContentResolver mContentRes;
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
+    private static final String TOAST_ICON_COLOR = "toast_icon_color";
+    private static final String TOAST_TEXT_COLOR = "toast_text_color";
+
+    private ListPreference mToastAnimation;
+    private ColorPickerPreference mIconColor;
+    private ColorPickerPreference mTextColor;
+    protected Context mContext;
+    protected ContentResolver mContentRes;
 
     @Override
     protected int getMetricsCategory() {
@@ -49,10 +55,31 @@ public class ToastAnimations extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.rr_toast);
-		mContext = getActivity().getApplicationContext();
-		mContentRes = getActivity().getContentResolver();
+	mContext = getActivity().getApplicationContext();
+	mContentRes = getActivity().getContentResolver();
 
- 		mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        int intColor = 0xffffffff;
+        String hexColor = String.format("#%08x", (0xffffffff & 0xffffffff));
+
+        mIconColor =
+                (ColorPickerPreference) findPreference(TOAST_ICON_COLOR);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.TOAST_ICON_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mIconColor.setNewPreviewColor(intColor);
+        mIconColor.setSummary(hexColor);
+        mIconColor.setOnPreferenceChangeListener(this);
+
+        mTextColor =
+                (ColorPickerPreference) findPreference(TOAST_TEXT_COLOR);
+        intColor = Settings.System.getInt(getContentResolver(),
+                Settings.System.TOAST_TEXT_COLOR, 0xffffffff);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mTextColor.setNewPreviewColor(intColor);
+        mTextColor.setSummary(hexColor);
+        mTextColor.setOnPreferenceChangeListener(this);
+
+ 	mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
         mToastAnimation.setSummary(mToastAnimation.getEntry());
         int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
         mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
@@ -60,16 +87,32 @@ public class ToastAnimations extends SettingsPreferenceFragment implements
         mToastAnimation.setOnPreferenceChangeListener(this);
     }
 
-	@Override
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue){
-		  boolean result = false;
+	   boolean result = false;
            if (preference == mToastAnimation) {
               int index = mToastAnimation.findIndexOfValue((String) newValue);
               Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) newValue);
               mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
               Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
               return true;
-          }
+          }  else if (preference == mIconColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TOAST_ICON_COLOR, intHex);
+             return true;
+        } else if (preference == mTextColor) {
+            String hex = ColorPickerPreference.convertToARGB(Integer
+                    .valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.TOAST_TEXT_COLOR, intHex);
+             return true;
+       }
         return false;
     }
 }
