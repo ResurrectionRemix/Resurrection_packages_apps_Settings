73ee6a27c98796f2f0d30024812e7d1b6e558f70
huiwan <huiwan@codeaurora.org>
2017-01-18 16:07:03 +0800

    Settings: fix vibrate for calls in Setting app displayed incorrect issue
    "vibrate for calls" status isn't updated after change by call setting, update
    the status onResume while back to Settings.
    
    CRs-Fixed: 1112360
    
    Change-Id: Idd7eb25cf6abac25bcf43327361eb495dd19be9c

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -192,6 +192,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements Indexab
         mReceiver.register(true);
         updateRingOrNotificationPreference();
         updateEffectsSuppressor();
+        updateVibrateWhenRinging();
         for (VolumeSeekBarPreference volumePref : mVolumePrefs) {
             volumePref.onActivityResume();
         }
