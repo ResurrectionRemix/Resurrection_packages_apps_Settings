841d40b414e92bf9972cddb5b1a9eaf5c39bae36
Park Ju Hyung <qkrwngud825@gmail.com>
2017-01-19 00:35:34 +0900

    Avoid a NullPointerException in security
    This is called when user enters Security -> Screen lock.
    
    Intent data might be null, causing a NullPointerException.
    
    This is reproducible by forcing a gatekeeper reset.
    
    Change-Id: I6af913a79e2c223ddbb37271536b826518d088b7
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- src/com/android/settings/ChooseLockGeneric.java
+++ src/com/android/settings/ChooseLockGeneric.java
@@ -359,7 +359,8 @@ public class ChooseLockGeneric extends SettingsActivity {
             mWaitingForConfirmation = false;
             if (requestCode == CONFIRM_EXISTING_REQUEST && resultCode == Activity.RESULT_OK) {
                 mPasswordConfirmed = true;
-                mUserPassword = data.getStringExtra(ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD);
+                mUserPassword = data == null ? null :
+                        data.getStringExtra(ChooseLockSettingsHelper.EXTRA_KEY_PASSWORD);
                 updatePreferencesOrFinish();
                 if (mForChangeCredRequiredForBoot) {
                     if (!TextUtils.isEmpty(mUserPassword)) {
