2443104e8dedcac97a3bb318d852f73095ab2de0
westcripp <korukaltan@gmail.com>
2017-01-22 20:11:13 +0300

    Revert "Settings: Remove Dashboard summary Switch from Dashboard Settings"
    This reverts commit 02d5ca441c40b424dd67d39092c693e7ccc3c651.

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -249,7 +249,7 @@ public class SettingsActivity extends SettingsDrawerActivity
 
     private static final String ACTION_TIMER_SWITCH = "qualcomm.intent.action.TIMER_SWITCH";
 
-    public static final String KEY_HIDE_SUMMARY = "remove_tile_summary";
+    public static final String KEY_HIDE_SUMMARY = "hide_summary";
     public static final String KEY_COLUMNS_COUNT = "columns_count";
     public static final String APP_PREFERENCES_NAME = "app_settings";
 
@@ -1294,6 +1294,18 @@ public class SettingsActivity extends SettingsDrawerActivity
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (super.onOptionsItemSelected(item)) {
+            return true;
+        }
+        if (mDisplayHomeAsUpEnabled && item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public boolean onMenuItemActionExpand(MenuItem item) {
         if (item.getItemId() == mSearchMenuItem.getItemId()) {
             switchToSearchResultsFragmentIfNeeded();
@@ -1402,14 +1414,7 @@ public class SettingsActivity extends SettingsDrawerActivity
                 mAppPreferences.edit().putBoolean(KEY_HIDE_SUMMARY, !hideSummary).commit();
                 return true;
         }
-        if (super.onOptionsItemSelected(item)) {
-            return true;
-        }
-        if (mDisplayHomeAsUpEnabled && item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
