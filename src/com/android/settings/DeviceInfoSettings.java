LineageOS/android_packages_apps_Settings

DeviceInfo: Set is_lineage intent when launching PlatLogo
We use this to decide whether to launch the LineageOS PlatLogo.

Change-Id: I23dfc7b6aa6b8d96d45a757a4b44f2399e62196c

src/com/android/settings/DeviceInfoSettings.java
@@ -254,6 +254,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 }
 
                 Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setExtra("is_lineage", preference.getKey().equals(KEY_MOD_VERSION));
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
                 try {

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/DeviceInfoSettings.java
@@ -248,6 +248,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
                 }
 
                 Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.putExtra("is_lineage", preference.getKey().equals(KEY_MOD_VERSION));
                 intent.setClassName("android",
                         com.android.internal.app.PlatLogoActivity.class.getName());
                 try {