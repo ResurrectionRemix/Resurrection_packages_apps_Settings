5d41e3c5e62e60455850cfe5932470039f23cb88
maxwen <max.weninger@gmail.com>
2017-02-26 03:11:00 +0300

    Settings: add OmniJaws config
    Change-Id: Ib67acad79af985a65f450f9dabb24df38aa961e8
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ src/com/android/settings/rr/Weather.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.android.settings.rr.fragments;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.rr.Preferences.CustomSeekBarPreference;
+import com.android.settings.rr.utils.Util;
+import com.android.settings.R;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Weather extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "OmniJawsSettings";
+    private static final String CATEGORY_WEATHER = "weather_category";
+    private static final String WEATHER_ICON_PACK = "weather_icon_pack";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+    private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
+    private static final String CHRONUS_ICON_PACK_INTENT = "com.dvtonder.chronus.ICON_PACK";
+
+    private PreferenceCategory mWeatherCategory;
+    private ListPreference mWeatherIconPack;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.RESURRECTED;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.weather);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+        Activity activity = getActivity();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PackageManager pm = getActivity().getPackageManager();
+
+        mWeatherCategory = (PreferenceCategory) prefSet.findPreference(CATEGORY_WEATHER);
+        if (mWeatherCategory != null && (!Util.isPackageInstalled(WEATHER_SERVICE_PACKAGE, pm))) {
+            prefSet.removePreference(mWeatherCategory);
+        } else {
+            String settingsJaws = Settings.System.getString(resolver,
+                    Settings.System.OMNIJAWS_WEATHER_ICON_PACK);
+            if (settingsJaws == null) {
+                settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
+            }
+            mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
+
+            List<String> entries = new ArrayList<String>();
+            List<String> values = new ArrayList<String>();
+            getAvailableWeatherIconPacks(entries, values);
+            mWeatherIconPack.setEntries(entries.toArray(new String[entries.size()]));
+            mWeatherIconPack.setEntryValues(values.toArray(new String[values.size()]));
+
+            int valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
+            if (valueJawsIndex == -1) {
+                // no longer found
+                settingsJaws = DEFAULT_WEATHER_ICON_PACKAGE;
+                Settings.System.putString(resolver,
+                        Settings.System.OMNIJAWS_WEATHER_ICON_PACK, settingsJaws);
+                valueJawsIndex = mWeatherIconPack.findIndexOfValue(settingsJaws);
+            }
+            mWeatherIconPack.setValueIndex(valueJawsIndex >= 0 ? valueJawsIndex : 0);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntry());
+            mWeatherIconPack.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mWeatherIconPack) {
+            String value = (String) objValue;
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.OMNIJAWS_WEATHER_ICON_PACK, value);
+            int valueIndex = mWeatherIconPack.findIndexOfValue(value);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntries()[valueIndex]);
+        }
+        return true;
+    }
+
+    private boolean isOmniJawsServiceInstalled() {
+        return Util.isPackageInstalled(WEATHER_SERVICE_PACKAGE, getActivity().getPackageManager());
+    }
+
+    private void getAvailableWeatherIconPacks(List<String> entries, List<String> values) {
+        Intent i = new Intent();
+        PackageManager packageManager = getActivity().getPackageManager();
+        i.setAction("org.omnirom.WeatherIconPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                values.add(0, r.activityInfo.name);
+            } else {
+                values.add(r.activityInfo.name);
+            }
+            String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            if (packageName.equals(DEFAULT_WEATHER_ICON_PACKAGE)) {
+                entries.add(0, label);
+            } else {
+                entries.add(label);
+            }
+        }
+        i = new Intent(Intent.ACTION_MAIN);
+        i.addCategory(CHRONUS_ICON_PACK_INTENT);
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            values.add(packageName + ".weather");
+            String label = r.activityInfo.loadLabel(getActivity().getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            entries.add(label);
+        }
+    }
+
+    private boolean isOmniJawsEnabled() {
+        final Uri SETTINGS_URI
+            = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
+
+        final String[] SETTINGS_PROJECTION = new String[] {
+            "enabled"
+        };
+
+        final Cursor c = getActivity().getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                null, null, null);
+        if (c != null) {
+            int count = c.getCount();
+            if (count == 1) {
+                c.moveToPosition(0);
+                boolean enabled = c.getInt(0) == 1;
+                return enabled;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/com/android/settings/rr/utils/Util.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.android.settings.rr.utils;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.SystemProperties;
+import android.os.Vibrator;
+import android.text.TextUtils;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.PreferenceFragment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.android.settings.R;
+import com.android.settings.SettingsActivity;
+
+public abstract class Util {
+
+    private static final String TAG = Util.class.getSimpleName();
+
+    public static final String PROPERTY_DEVICE = "ro.rr.device";
+    public static final String PROPERTY_DEVICE_EXT = "ro.product.device";
+
+    public static boolean onPreferenceTreeClick(PreferenceFragment fragment,
+                                                Preference preference) {
+        Activity activity = fragment.getActivity();
+        if (activity instanceof SettingsActivity) {
+            return ((SettingsActivity) activity).onPreferenceClick(preference);
+        } else {
+            Log.w(TAG, "Activity not instanceof SettingsActivity, ignoring preference click");
+            return false;
+        }
+    }
+
+
+    public static void setSummaryToValue(ListPreference pref) {
+        pref.setSummary(pref.getEntry());
+    }
+
+    public static void setSummaryToValue(ListPreference pref, Object newValue) {
+        try {
+            int index = pref.findIndexOfValue((String) newValue);
+            pref.setSummary(pref.getEntries()[index]);
+        } catch (Exception e) {
+            Log.w(TAG, "setSummaryToValue: " + newValue + " caused exception " + e);
+        }
+    }
+
+    public static boolean isPackageInstalled(String packageName, PackageManager pm) {
+        try {
+            String mVersion = pm.getPackageInfo(packageName, 0).versionName;
+            return mVersion != null;
+        } catch (PackageManager.NameNotFoundException notFound) {
+            return false;
+        }
+    }
+
+    public static boolean isPackageEnabled(String packageName, PackageManager pm) {
+        try {
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, 0);
+            return ai.enabled;
+        } catch (PackageManager.NameNotFoundException notFound) {
+            return false;
+        }
+    }
+
+    public static String getDevice(Context context) {
+        String device = SystemProperties.get(PROPERTY_DEVICE);
+        if (TextUtils.isEmpty(device)) {
+            device = SystemProperties.get(PROPERTY_DEVICE_EXT);
+            //device = translateDeviceName(context, device);
+        }
+        return device == null ? "" : device.toLowerCase();
+   }
+
+   public static boolean hasVibrator(Context context) {
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        return v.hasVibrator();
+   }
+
+   public static String readStringFromFile(File inputFile) throws IOException {
+        FileReader fileReader = new FileReader(inputFile);
+        StringBuffer stringBuffer = new StringBuffer();
+        int numCharsRead;
+        char[] charArray = new char[1024];
+        while ((numCharsRead = fileReader.read(charArray)) > 0) {
+            stringBuffer.append(charArray, 0, numCharsRead);
+        }
+        fileReader.close();
+        return stringBuffer.toString();
+    }
+
+    /**
+     * This can not reliably detect whether the user has root access,
+     * but it can detect some cases when the user hasn't.
+     */
+    public static boolean hasSu() {
+        Process p = null;
+        try {
+            p = Runtime.getRuntime().exec(new String[] { "which", "su" });
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            return br.readLine() != null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (p != null) p.destroy();
+        }
+        return false;
+    }
+
+    public static void requireRoot(Preference preference) {
+        if (!hasSu()) {
+            preference.getParent().removePreference(preference);
+        }
+    }
+
+    public static void restartSystemUi(Context context) {
+        new RestartSystemUiTask(context).execute();
+    }
+
+    public static void showSystemUiRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSystemUi(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private static class RestartSystemUiTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public RestartSystemUiTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.systemui".equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+                //Class ActivityManagerNative = Class.forName("android.app.ActivityManagerNative");
+                //Method getDefault = ActivityManagerNative.getDeclaredMethod("getDefault", null);
+                //Object amn = getDefault.invoke(null, null);
+                //Method killApplicationProcess = amn.getClass().getDeclaredMethod("killApplicationProcess", String.class, int.class);
+                //mContext.stopService(new Intent().setComponent(new ComponentName("com.android.systemui", "com.android.systemui.SystemUIService")));
+                //am.killBackgroundProcesses("com.android.systemui");
+                //for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                //    if ("com.android.systemui".equals(app.processName)) {
+                //        killApplicationProcess.invoke(amn, app.processName, app.uid);
+                //        break;
+                //    }
+                //}
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
