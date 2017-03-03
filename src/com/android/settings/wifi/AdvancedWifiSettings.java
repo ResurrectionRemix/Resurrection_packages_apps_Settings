LineageOS/android_packages_apps_Settings

Settings: Add overlay for hiding wifi region code
CYNGNOS-1637

Change-Id: Ieb1f6553a2addb17ec6b93b51ab8fb19f6c5ca49

src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -199,12 +199,18 @@ public class AdvancedWifiSettings extends RestrictedSettingsFragment
 
         ListPreference ccodePref = (ListPreference) findPreference(KEY_COUNTRY_CODE);
         if (ccodePref != null) {
-            ccodePref.setOnPreferenceChangeListener(this);
-            String value = mWifiManager.getCountryCode();
-            if (value != null) {
-                ccodePref.setValue(value);
+            boolean hideWifiRegion = getResources()
+                    .getBoolean(R.bool.config_hide_wifi_region_code);
+            if (hideWifiRegion) {
+                removePreference(KEY_COUNTRY_CODE);
             } else {
-                Log.e(TAG, "Failed to fetch country code");
+                ccodePref.setOnPreferenceChangeListener(this);
+                String value = mWifiManager.getCountryCode();
+                if (value != null) {
+                    ccodePref.setValue(value);
+                } else {
+                    Log.e(TAG, "Failed to fetch country code");
+                }
             }
         }
 

ResurrectionRemix/Resurrection_packages_apps_Settings

src/com/android/settings/wifi/AdvancedWifiSettings.java
@@ -199,12 +199,18 @@ public class AdvancedWifiSettings extends RestrictedSettingsFragment
 
         ListPreference ccodePref = (ListPreference) findPreference(KEY_COUNTRY_CODE);
         if (ccodePref != null) {
-            ccodePref.setOnPreferenceChangeListener(this);
-            String value = mWifiManager.getCountryCode();
-            if (value != null) {
-                ccodePref.setValue(value);
+            boolean hideWifiRegion = getResources()
+                    .getBoolean(R.bool.config_hideWifiRegionCode);
+            if (hideWifiRegion) {
+                removePreference(KEY_COUNTRY_CODE);
             } else {
-                Log.e(TAG, "Failed to fetch country code");
+                ccodePref.setOnPreferenceChangeListener(this);
+                String value = mWifiManager.getCountryCode();
+                if (value != null) {
+                    ccodePref.setValue(value);
+                } else {
+                    Log.e(TAG, "Failed to fetch country code");
+                }
             }
         }
 