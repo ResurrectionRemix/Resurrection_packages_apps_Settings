623a12a9b16eb00171cdb10872908e8098d6abab
Varun Date <date.varun123@gmail.com>
2017-04-03 11:19:21 +0530

    Add Reset Button to Pixel Animation Duration Settings
    Reset Button to reset to default values
    Extra button to reset to RR recommended values
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/navbar/PixelAnimDurationSettings.java
+++ src/com/android/settings/rr/navbar/PixelAnimDurationSettings.java
@@ -14,8 +14,13 @@
 package com.android.settings.rr.navbar;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.res.Resources;
 import android.provider.Settings;
 import android.os.Bundle;
@@ -25,12 +30,18 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.rr.SeekBarPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PixelAnimDurationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -54,6 +65,20 @@ public class PixelAnimDurationSettings extends SettingsPreferenceFragment implem
     protected Context mContext;
     protected ContentResolver mContentRes;
 
+    private static final int COLLAPSE_ANIMATION_DURATION_RY = 83;
+    private static final int COLLAPSE_ANIMATION_DURATION_BG = 100;
+    private static final int LINE_ANIMATION_DURATION_Y = 275;
+    private static final int LINE_ANIMATION_DURATION_X = 133;
+    private static final int RETRACT_ANIMATION_DURATION = 300;
+    private static final int DIAMOND_ANIMATION_DURATION = 200;
+    private static final int HALO_ANIMATION_DURATION = 100;
+
+    private static final int DOTS_RESIZE_DURATION = 200;
+    private static final int HOME_RESIZE_DURATION = 83;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET = 0;
+
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.RESURRECTED;
@@ -62,6 +87,16 @@ public class PixelAnimDurationSettings extends SettingsPreferenceFragment implem
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
+
         addPreferencesFromResource(R.xml.pixel_anim_duration);
 
 		mContext = getActivity().getApplicationContext();
@@ -175,5 +210,125 @@ public class PixelAnimDurationSettings extends SettingsPreferenceFragment implem
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_action_reset) // use the KitKat reset icon
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
+   }
+
+
+   private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+   }
+
+   public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+
+        PixelAnimDurationSettings getOwner() {
+            return (PixelAnimDurationSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.animation_settings_reset_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.dlg_reset_android,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.OPA_ANIM_DURATION_X, 
+                                    LINE_ANIMATION_DURATION_X);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.OPA_ANIM_DURATION_Y, 
+                                    LINE_ANIMATION_DURATION_Y);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.COLLAPSE_ANIMATION_DURATION_RY, 
+                                    COLLAPSE_ANIMATION_DURATION_RY);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.COLLAPSE_ANIMATION_DURATION_BG,
+                                    COLLAPSE_ANIMATION_DURATION_BG);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.RETRACT_ANIMATION_DURATION,
+                                    RETRACT_ANIMATION_DURATION);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.DIAMOND_ANIMATION_DURATION,
+                                    DIAMOND_ANIMATION_DURATION);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.DOTS_RESIZE_DURATION, 
+                                    DOTS_RESIZE_DURATION);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.HOME_RESIZE_DURATION, 
+                                    HOME_RESIZE_DURATION);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_reset_rr,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.OPA_ANIM_DURATION_X, 
+                                    400);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.OPA_ANIM_DURATION_Y, 
+                                   400);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.COLLAPSE_ANIMATION_DURATION_RY, 
+                                    500);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.COLLAPSE_ANIMATION_DURATION_BG,
+                                    300);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.RETRACT_ANIMATION_DURATION,
+                                    600);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.DIAMOND_ANIMATION_DURATION,
+                                    450);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.DOTS_RESIZE_DURATION, 
+                                    500);
+                            Settings.System.putInt(getOwner().mContentRes,
+                                    Settings.System.HOME_RESIZE_DURATION, 
+                                    367);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
     }
 }
