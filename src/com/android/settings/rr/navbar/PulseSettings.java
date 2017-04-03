61354e2de89477ec687b518a8492f309b9321ee0
ezio84 <brabus84@gmail.com>
2017-04-03 09:26:42 +0530

    SmartBar: allow set custom buttons opacity when Pulse is active [2/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/navbar/PulseSettings.java
+++ src/com/android/settings/rr/navbar/PulseSettings.java
@@ -50,6 +50,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     private static final String FADING_LAVAMP_SPEED = "fling_pulse_lavalamp_speed";
     private static final String PULSE_SOLID_UNITS_COUNT = "pulse_solid_units_count";
     private static final String PULSE_SOLID_UNITS_OPACITY = "pulse_solid_units_opacity";
+    private static final String PULSE_CUSTOM_BUTTONS_OPACITY = "pulse_custom_buttons_opacity";
 
     SwitchPreference mShowPulse;
     ListPreference mRenderMode;
@@ -67,6 +68,7 @@ public class PulseSettings extends SettingsPreferenceFragment implements
     SeekBarPreference mFadingSpeed;
     SeekBarPreference mSolidCount;
     SeekBarPreference mSolidOpacity;
+    SeekBarPreference mNavButtonsOpacity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -177,6 +179,13 @@ public class PulseSettings extends SettingsPreferenceFragment implements
                 (SeekBarPreference) findPreference(PULSE_SOLID_UNITS_OPACITY);
         mSolidOpacity.setValue(opacity);
         mSolidOpacity.setOnPreferenceChangeListener(this);
+
+        int buttonsOpacity = Settings.Secure.getIntForUser(getContentResolver(),
+                Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY, 200, UserHandle.USER_CURRENT);
+        mNavButtonsOpacity =
+                (CustomSeekBarPreference) findPreference(PULSE_CUSTOM_BUTTONS_OPACITY);
+        mNavButtonsOpacity.setValue(buttonsOpacity);
+        mNavButtonsOpacity.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -268,7 +277,12 @@ public class PulseSettings extends SettingsPreferenceFragment implements
             Settings.Secure.putIntForUser(getContentResolver(),
                     Settings.Secure.PULSE_SOLID_UNITS_OPACITY, val, UserHandle.USER_CURRENT);
             return true;
-        }
+        } else if (preference == mNavButtonsOpacity) {
+            int val = (Integer) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                    Settings.Secure.PULSE_CUSTOM_BUTTONS_OPACITY, val, UserHandle.USER_CURRENT);
+            return true;
+         }
         return false;
     }
 
