65592fcd49bccde3d5582f3ec1003be10f64fdf0
Alex Cruz <du.alexcruz@gmail.com>
2017-10-26 20:18:26 -0400

    Simplify the structure, make changes to integrate with Settings
    This commit not only simplified the structure of DU-Tweaks while keeping the
    same look and feel but it makes it easier for contributors. In the old layout
    the app was mainly launched from an activity. This takes us back to using Settings
    preference fragment across the board which is what we used since kitkat and easier
    for AOSP code.
    
    - Removed snackbar checks
    
    - Removed adaptive icons since is now part of Settings
    
    - Removed OmniSwitch, Statusbar Items and Device Extra classes. We launch all three
    using intents. Keeping the layouts for when we add them back.
    
    - Simpify the check for Device Extras, using a bool
    
    - Update bottom navigation classes, rename the names so we can better update them if
    need in the future versions.
    
    Change-Id: I867bd8e050b06d222b742e355d0d4b0c44adebad
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ src/com/dirtyunicorns/tweaks/fragments/Ticker.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2017 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dirtyunicorns.tweaks.fragments;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class Ticker extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.ticker);
+
+        getActivity().getActionBar().setTitle(R.string.ticker_title);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DIRTYTWEAKS;
+    }
+}
