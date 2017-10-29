5552617770c34dea401001a31d48a33c3b361edf
SpiritCroc <spiritcroc@gmail.com>
2017-10-29 13:39:45 +0100

    MasterSwitchPreference: isChecked doesn't rely on existence of switch
    When checking for isChecked in e.g. a preferenceFragment, we want to
    know the setting value, independent of whether the preference already
    has been inflated or not
    
    Change-Id: Ie3e777388805d329fea6f56ef306a4c941993938

--- src/com/android/settings/widget/MasterSwitchPreference.java
+++ src/com/android/settings/widget/MasterSwitchPreference.java
@@ -90,7 +90,7 @@ public class MasterSwitchPreference extends TwoTargetPreference {
     }
 
     public boolean isChecked() {
-        return mSwitch != null && mSwitch.isEnabled() && mChecked;
+        return mChecked;
     }
 
     public void setChecked(boolean checked) {
