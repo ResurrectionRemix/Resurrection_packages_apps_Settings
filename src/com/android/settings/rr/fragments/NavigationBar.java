e2e442bf7d9c74524dd74ab79b56775e34f664d8
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-11-29 13:13:05 -0500

    Protect against SystemUI FC when toggling navbar too fast
    i know this is the noobest hacky fix ever and you'd like
    to come here and kill me with napalm but meh, it works well
    till we find a more pro solution :P
    
    Change-Id: I1856fec4a07f01d9bd960da645e3e8cc44392d48

--- src/com/android/settings/rr/fragments/NavigationBar.java
+++ src/com/android/settings/rr/fragments/NavigationBar.java
@@ -23,6 +23,7 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v14.preference.SwitchPreference;
@@ -66,6 +67,9 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
     private CustomSeekBarPreference mBarHeightLand;
     private CustomSeekBarPreference mBarWidth;
     private PreferenceScreen mPulseSettings;
+
+    private boolean mIsNavSwitchingMode = false;
+    private Handler mHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -115,6 +119,8 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
             mBarHeightLand.setValue(size);
             mBarHeightLand.setOnPreferenceChangeListener(this);
         }
+
+        mHandler = new Handler();
     }
 
     private void updateBarModeSettings(int mode) {
@@ -168,10 +174,20 @@ public class NavigationBar extends SettingsPreferenceFragment implements Prefere
             updateBarModeSettings(mode);
             return true;
         } else if (preference.equals(mNavbarVisibility)) {
+            if (mIsNavSwitchingMode) {
+                return false;
+            }
+            mIsNavSwitchingMode = true;
             boolean showing = ((Boolean)newValue);
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
                     showing ? 1 : 0);
             updateBarVisibleAndUpdatePrefs(showing);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsNavSwitchingMode = false;
+                }
+            }, 1500);
             return true;
         } else if (preference == mBarHeightPort) {
             int val = (Integer) newValue;
