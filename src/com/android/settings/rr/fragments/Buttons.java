ef51af67500107556243b8be513b858a32d7cc57
bigrushdog <randall.rushing@gmail.com>
2017-12-05 23:50:58 -0800

    Hardware key disable support
    Change-Id: I42f8b3b9423e218eb612bb8ff57baa58280392e6
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/fragments/Buttons.java
+++ src/com/android/settings/rr/fragments/Buttons.java
@@ -17,6 +17,7 @@
 package com.android.settings.rr.fragments;
 
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -31,9 +32,13 @@ import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.utils.du.ActionConstants;
+import com.android.internal.utils.du.DUActionUtils;
 
 public class Buttons extends ActionFragment implements Preference.OnPreferenceChangeListener {
+    private static final String HWKEY_DISABLE = "hardware_keys_disable";
+
     // category keys
+    private static final String CATEGORY_HWKEY = "hardware_keys";
     private static final String CATEGORY_BACK = "back_key";
     private static final String CATEGORY_HOME = "home_key";
     private static final String CATEGORY_MENU = "menu_key";
@@ -52,12 +57,29 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
     public static final int KEY_MASK_CAMERA = 0x20;
     public static final int KEY_MASK_VOLUME = 0x40;
 
+    private SwitchPreference mHwKeyDisable;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.buttons);
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final boolean needsNavbar = DUActionUtils.hasNavbarByDefault(getActivity());
+        final PreferenceCategory hwkeyCat = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HWKEY);
+        int keysDisabled = 0;
+        if (!needsNavbar) {
+            mHwKeyDisable = (SwitchPreference) findPreference(HWKEY_DISABLE);
+            keysDisabled = Settings.Secure.getIntForUser(getContentResolver(),
+                    Settings.Secure.HARDWARE_KEYS_DISABLE, 0,
+                    UserHandle.USER_CURRENT);
+            mHwKeyDisable.setChecked(keysDisabled != 0);
+            mHwKeyDisable.setOnPreferenceChangeListener(this);
+        } else {
+            prefScreen.removePreference(hwkeyCat);
+        }
 
         // bits for hardware keys present on device
         final int deviceKeys = getResources().getInteger(
@@ -110,6 +132,9 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
 
         // let super know we can load ActionPreferences
         onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
+
+        // load preferences first
+        setActionPreferencesEnabled(keysDisabled == 0);
     }
 
     @Override
@@ -119,6 +144,13 @@ public class Buttons extends ActionFragment implements Preference.OnPreferenceCh
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHwKeyDisable) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.HARDWARE_KEYS_DISABLE,
+                    value ? 1 : 0);
+            setActionPreferencesEnabled(!value);
+            return true;
+        }
         return false;
     }
 
