7dd03d57e1961765c9180e46e2d30312567e468e
Varun Date <date.varun123@gmail.com>
2018-01-17 14:26:15 +1100

    Welcome Ten: RR Configurations Bringup  Thanks a lot to DU ,Screwd , AICP and all previous contributors
    Settings layout is similar to N
    
    About RR brought to you by @westcripp
    
    Change-Id: I9aee4621c7632a8128945b694896e39403d8521f
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,4 +177,6 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    public static class MainSettingsLayoutActivity extends SettingsActivity { /* empty */ }
+
 }
