f4ebbeb0d6750d2495966222edab476d41deab79
Varun Date <date.varun123@gmail.com>
2018-01-17 14:26:15 +1100

    Welcome Ten: RR Configurations Bringup  Thanks a lot to DU ,Screwd , AICP and all previous contributors
    Settings layout is similar to N
    
    About RR brought to you by @westcripp
    
    Change-Id: I9aee4621c7632a8128945b694896e39403d8521f
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.android.settings.rr.MainSettingsLayout;
+
 public class SettingsGateway {
 
     /**
@@ -284,6 +286,7 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName()
+            MainSettingsLayout.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -322,5 +325,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.MainSettingsLayoutActivity.class.getName(),
     };
 }
