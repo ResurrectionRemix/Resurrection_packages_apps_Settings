07db2ae39e753153e3f2b6bdfe0b820094d63716
SpiritCroc <spiritcroc@gmail.com>
2018-01-19 08:18:16 +1100

    Battery bar [2/2]
    Change-Id: Ic4d58273d145eb044fa9dc6062f419badb3ef1d8
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ src/com/android/settings/rr/BatterySettings.java
@@ -0,0 +1,86 @@
+/*Copyright (C) 2015 The ResurrectionRemix Project
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+package com.android.settings.rr;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import cyanogenmod.providers.CMSettings;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+public class BatterySettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private ListPreference mBatteryBarPosition;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.rr_battery);
+
+
+        mBatteryBarPosition =
+                (ListPreference) findPreference(Settings.System.STATUSBAR_BATTERY_BAR);
+        mBatteryBarPosition.setOnPreferenceChangeListener(this);
+
+        updateBatteryBarDependencies(Integer.parseInt(mBatteryBarPosition.getValue()));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBatteryBarPosition) {
+            updateBatteryBarDependencies(Integer.parseInt((String) newValue));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void updateBatteryBarDependencies(int batteryBarPosition) {
+        // All preferences within this screen that don't have explicitely set a dependency
+        // except the position preference depend on the battery bar position preference
+        boolean enabled = batteryBarPosition != 0;
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        for (int i = 0; i < preferenceScreen.getPreferenceCount(); i) {
+            Preference preference = preferenceScreen.getPreference(i);
+            if (preference != mBatteryBarPosition && preference.getDependency() == null) {
+                preference.setEnabled(enabled);
+            }
+        }
+    }
+
+}
