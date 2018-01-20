e2482faaa0eef45d359d063f89d26244c6bbcaa5
xyyx <xyyx@mail.ru>
2018-01-20 14:36:50 +0800

    Vectorize ic_live_help icon and fix theming for SimDialogActivity
    Change-Id: I86d1bfdb2120a53bf1dcd21181ecdf2bd9e663e8
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/sim/SimDialogActivity.java
+++ src/com/android/settings/sim/SimDialogActivity.java
@@ -286,7 +286,6 @@ public class SimDialogActivity extends Activity {
         private Context mContext;
         private int mResId;
         private int mDialogId;
-        private final float OPACITY = 0.54f;
         private List<SubscriptionInfo> mSubInfoList;
 
         public SelectAccountListAdapter(List<SubscriptionInfo> subInfoList,
@@ -322,9 +321,8 @@ public class SimDialogActivity extends Activity {
             if (sir == null) {
                 holder.title.setText(getItem(position));
                 holder.summary.setText("");
-                holder.icon.setImageDrawable(getResources()
+                holder.icon.setImageDrawable(mContext
                         .getDrawable(R.drawable.ic_live_help));
-                holder.icon.setAlpha(OPACITY);
             } else {
                 holder.title.setText(sir.getDisplayName());
                 holder.summary.setText(sir.getNumber());
