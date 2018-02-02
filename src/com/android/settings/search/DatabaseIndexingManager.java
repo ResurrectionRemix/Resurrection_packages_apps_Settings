6aa4e2ed09769cf4755768c706b9a0696219c79a
maxwen <max.weninger@gmail.com>
2018-02-02 02:42:54 +0100

    Settings: refresh search index check on build change
    Change-Id: I7199d9f812d8cb645f3abca815c62098248b53c4

--- src/com/android/settings/search/DatabaseIndexingManager.java
+++ src/com/android/settings/search/DatabaseIndexingManager.java
@@ -101,7 +101,7 @@ public class DatabaseIndexingManager {
                 mContext.getPackageManager().queryIntentContentProviders(intent, 0);
 
         final String localeStr = Locale.getDefault().toString();
-        final String fingerprint = Build.FINGERPRINT;
+        final String fingerprint = Build.DATE;
         final String providerVersionedNames =
                 IndexDatabaseHelper.buildProviderVersionedNames(providers);
 
