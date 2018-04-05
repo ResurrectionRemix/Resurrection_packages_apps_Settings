a830d66037d019a9c5b30eb38526d961eae883aa
EndLess728 <aryanmk.mk5@gmail.com>
2018-04-05 15:08:25 +0000

    Revert "Quick settings panel : Set transparency [2/2]"
    This reverts commit 9b6a96cd0d79015d6fd8369c58a2f9d61e2b01e4.

--- src/com/android/settings/rr/QsLayout.java
+++ src/com/android/settings/rr/QsLayout.java
@@ -15,70 +15,44 @@ package com.android.settings.rr;
 
 import android.app.Activity;
 import android.content.Context;
-import android.os.Bundle;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
-import java.util.Locale;
-import android.text.TextUtils;
-import android.view.View;
-
-import java.util.List;
-import java.util.ArrayList;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.rr.Preferences.CustomSeekBarPreference;
 
 public class QsLayout extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String QS_PANEL_ALPHA = "qs_panel_alpha";
-
-    private CustomSeekBarPreference mQsPanelAlpha;
-
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.RESURRECTED;
+        return MetricsEvent.RESURRECTED;
     }
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rr_qs_layout);
 
-        PreferenceScreen prefScreen = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
-
-         mQsPanelAlpha = (CustomSeekBarPreference) findPreference(QS_PANEL_ALPHA);
-        int qsPanelAlpha = Settings.System.getIntForUser(resolver,
-                Settings.System.QS_PANEL_BG_ALPHA, 255, UserHandle.USER_CURRENT);
-        mQsPanelAlpha.setValue(qsPanelAlpha);
-        mQsPanelAlpha.setOnPreferenceChangeListener(this);
+		final Resources res = getResources();
+		int defaultValue;
+		PreferenceScreen prefs = getPreferenceScreen();
+		ContentResolver resolver = getActivity().getContentResolver();
     }
 
-     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mQsPanelAlpha) {
-            int bgAlpha = (Integer) newValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.QS_PANEL_BG_ALPHA, bgAlpha,
-                    UserHandle.USER_CURRENT);
-            return true;
-        }
-
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
         return false;
     }
 }
