LineageOS/android_packages_apps_Settings

DisplaySettings: Allow devices to opt out from lift to wake detection
Change-Id: Ia1b6737a02777dfcd3c80cab62f128a3090807fa
Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

src/com/android/settings/display/LiftToWakePreferenceController.java
@@ -37,7 +37,9 @@ public class LiftToWakePreferenceController extends AbstractPreferenceController
     @Override
     public boolean isAvailable() {
         SensorManager sensors = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
-        return sensors != null && sensors.getDefaultSensor(Sensor.TYPE_WAKE_GESTURE) != null;
+        return mContext.getResources().getBoolean(
+                com.android.settings.R.bool.config_supportLiftToWake) &&
+                sensors != null && sensors.getDefaultSensor(Sensor.TYPE_WAKE_GESTURE) != null;
     }
 
     @Override

ResurrectionRemix/Resurrection_packages_apps_Settings

 * Some devices provide TYPE_WAKE_GESTURE sensor that isn't being used
   for lift to wake therefore we need to hide this feature on these devices.

src/com/android/settings/display/LiftToWakePreferenceController.java
@@ -37,7 +37,9 @@ public class LiftToWakePreferenceController extends AbstractPreferenceController
     @Override
     public boolean isAvailable() {
         SensorManager sensors = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
-        return sensors != null && sensors.getDefaultSensor(Sensor.TYPE_WAKE_GESTURE) != null;
+        return mContext.getResources().getBoolean(
+                com.android.settings.R.bool.config_supportsLiftToWake) &&
+                sensors != null && sensors.getDefaultSensor(Sensor.TYPE_WAKE_GESTURE) != null;
     }
 
     @Override