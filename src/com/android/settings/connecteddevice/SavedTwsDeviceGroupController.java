9c32827df4de24ba3b113c3de93ea7a6ecba4fa0
Jiyong Park <jiyong@google.com>
2018-08-14 20:15:39 +0900

    Fix build error
    android.support.* are renamed to androidx.
    
    Test: m -j Settings
    Change-Id: I6048ba8b014d2804c624113bd214f10ddee8c13a

--- src/com/android/settings/connecteddevice/SavedTwsDeviceGroupController.java
+++ src/com/android/settings/connecteddevice/SavedTwsDeviceGroupController.java
@@ -51,9 +51,9 @@ package com.android.settings.connecteddevice;
 import android.content.pm.PackageManager;
 import android.content.Context;
 
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceGroup;
-import android.support.v7.preference.PreferenceScreen;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.bluetooth.BluetoothDeviceUpdater;
 import com.android.settings.bluetooth.SavedBluetoothTwsDeviceUpdater;
