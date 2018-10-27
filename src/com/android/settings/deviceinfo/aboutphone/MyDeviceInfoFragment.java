e59c1d7abc40f5dde2eb0bf23288607cd6575f77
Stephen Smalley <sds@tycho.nsa.gov>
2018-10-26 19:33:49 -0500

    Bring back selinux version
    This commit reverts the commit below
    https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/b255b3a6ed4dbda26eb649767134e038045254a3
    
    - Adapted to P
    
    This includes the work previously done by Fan Zhang <zhfan@google.com> since it was introduced in 2012.
    
    Change-Id: Ic371a9ef0055b559eb0b6ff2791291fb82e408cf
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -46,6 +46,7 @@ import com.android.settings.deviceinfo.ManualPreferenceController;
 import com.android.settings.deviceinfo.PhoneNumberPreferenceController;
 import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
+import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
@@ -127,6 +128,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new ManualPreferenceController(context));
         controllers.add(new FeedbackPreferenceController(fragment, context));
         controllers.add(new FccEquipmentIdPreferenceController(context));
+        controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
         return controllers;
