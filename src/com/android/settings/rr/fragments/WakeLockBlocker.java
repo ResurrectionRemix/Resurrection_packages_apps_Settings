dd3d0de617b8f88f29dacb5354a29b2aded4b47a
LorDClockaN <davor@losinj.com>
2018-11-22 15:14:03 +0100

    Volume dialog timeout (2/2)
    Change-Id: I48a2d66ff653c510c28ae498fb012a65022c49ee
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- src/com/android/settings/rr/fragments/WakeLockBlocker.java
+++ src/com/android/settings/rr/fragments/WakeLockBlocker.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.rr.settings.fragments;
+package com.android.settings.rr.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
