4a3a031a092cd9c6f54760e29b92fc08d01489dd
Arghya Chanda <arghyac35@gmail.com>
2018-12-10 16:32:41 -0700

    Add edge gestures (2/2)
    Change-Id: I2fb2a5b5ab446a8ac648bf10b797ac2d4bf19e4a
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

---
+++ src/com/android/settings/rr/fragments/EdgeGesturesSettings.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2018 CarbonROM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fragments;
+
+import android.app.ActionBar; 
+import android.os.Bundle; 
+import android.os.UserHandle; 
+import android.provider.Settings; 
+import android.support.v14.preference.SwitchPreference; 
+import android.support.v7.preference.Preference; 
+ 
+import com.android.internal.logging.nano.MetricsProto; 
+import com.android.internal.utils.ActionUtils; 
+import com.android.settings.R; 
+import com.android.settings.SettingsPreferenceFragment; 
+import com.android.settings.rr.Preferences.SecureSettingSeekBarPreference; 
+ 
+public class EdgeGesturesSettings extends SettingsPreferenceFragment implements 
+        Preference.OnPreferenceChangeListener { 
+ 
+    public static final String EDGE_GESTURES_ENABLED = "edge_gestures_enabled"; 
+    public static final String EDGE_GESTURES_SCREEN_PERCENT = "edge_gestures_back_screen_percent"; 
+ 
+    private String previousTitle; 
+ 
+    private SwitchPreference enabledPreference; 
+    private SecureSettingSeekBarPreference screenPercentPreference; 
+ 
+    @Override 
+    public void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState); 
+ 
+        addPreferencesFromResource(R.xml.edge_gestures); 
+ 
+        enabledPreference = (SwitchPreference) findPreference(EDGE_GESTURES_ENABLED); 
+        /*enabledPreference.setChecked((Settings.System.getInt(getContentResolver(), 
+                Settings.Secure.EDGE_GESTURES_ENABLED, 0) == 1));*/ 
+        enabledPreference.setOnPreferenceChangeListener(this); 
+ 
+        screenPercentPreference = (SecureSettingSeekBarPreference) findPreference(EDGE_GESTURES_SCREEN_PERCENT); 
+        int percent = Settings.Secure.getIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_BACK_SCREEN_PERCENT, 60, UserHandle.USER_CURRENT); 
+        screenPercentPreference.setValue(percent); 
+    } 
+ 
+    @Override 
+    public void onStart() { 
+        super.onStart(); 
+ 
+        ActionBar actionBar = getActivity().getActionBar(); 
+        previousTitle = actionBar.getTitle().toString(); 
+        actionBar.setTitle(R.string.edge_gestures_title); 
+    } 
+ 
+    @Override 
+    public void onStop() { 
+        super.onStop(); 
+ 
+        ActionBar actionBar = getActivity().getActionBar(); 
+        actionBar.setTitle(previousTitle); 
+    } 
+ 
+    @Override 
+    public int getMetricsCategory() { 
+        return MetricsProto.MetricsEvent.RESURRECTED; 
+    } 
+ 
+    @Override 
+    public boolean onPreferenceChange(Preference preference, Object newValue) { 
+        if (preference == enabledPreference) { 
+            int enabled = ((boolean) newValue) ? 1 : 0; 
+            //Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_ENABLED, enabled, UserHandle.USER_CURRENT); 
+ 
+            if (enabled == 1) { 
+                Settings.Secure.putInt(getContentResolver(), 
+                        Settings.Secure.NAVIGATION_BAR_VISIBLE, 
+                        0); 
+            } else { 
+                if (ActionUtils.hasNavbarByDefault(getPrefContext())) { 
+                    Settings.Secure.putInt(getContentResolver(), 
+                            Settings.Secure.NAVIGATION_BAR_VISIBLE, 
+                            1); 
+                } 
+            } 
+            return true; 
+        } /*else if (preference == hapticFeedbackDurationPreference) { 
+            int hapticFeedbackValue = Integer.valueOf((String) newValue); 
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_FEEDBACK_DURATION, hapticFeedbackValue, UserHandle.USER_CURRENT); 
+            return true; 
+        } else if (preference == longPressDurationPreference) { 
+            int longPressValue = Integer.valueOf((String) newValue); 
+            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_LONG_PRESS_DURATION, longPressValue, UserHandle.USER_CURRENT); 
+            return true; 
+        }*/ 
+ 
+        return false; 
+    } 
+} 
+
