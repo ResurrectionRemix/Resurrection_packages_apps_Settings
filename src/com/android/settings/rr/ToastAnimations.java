03e771a34b1d51c69298dc7379a6f6c47f2803aa
varund7726 <date.varun123@gmail.com>
2018-12-11 11:25:23 +0530

    Fix Toast animation fragment FC
    Signed-off-by: varund7726 <date.varun123@gmail.com>

--- src/com/android/settings/rr/ToastAnimations.java
+++ src/com/android/settings/rr/ToastAnimations.java
@@ -11,7 +11,7 @@
      See the License for the specific language governing permissions and
      limitations under the License.
 */
-package com.android.settings.animation;
+package com.android.settings.rr;
 
 import android.app.Activity;
 import android.content.Context;
