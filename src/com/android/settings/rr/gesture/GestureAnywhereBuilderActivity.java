1d2923861e0905b70e036332da41e04f2fde4a8b
0xD34D <clark@cyanogen.com>
2018-12-19 13:47:43 +0530

    Fix up GA Fragment
    Change-Id: Id6e0ee0270d7df38fa39e45a3c5e523648a779a0
    Signed-off-by: varund7726 <date.varun123@gmail.com>

--- src/com/android/settings/rr/gesture/GestureAnywhereBuilderActivity.java
+++ src/com/android/settings/rr/gesture/GestureAnywhereBuilderActivity.java
@@ -43,7 +43,7 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.BitmapDrawable;
 import com.android.settings.R;
-import com.android.settings.rr.ShortcutPickHelper;
+import com.android.settings.smartnav.ShortcutPickHelper;
 
 import java.util.Map;
 import java.util.Collections;
