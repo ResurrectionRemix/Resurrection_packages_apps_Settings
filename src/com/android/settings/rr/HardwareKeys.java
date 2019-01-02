71f88cafcf2b91de3f1730ebbd4d91023f820391
varund7726 <date.varun123@gmail.com>
2019-01-02 10:49:32 +0530

    Fix Hw keys fragment FC
    Change-Id: I17566f7c830873be47ba60be4c987286e0104a5b
    Signed-off-by: varund7726 <date.varun123@gmail.com>

--- src/com/android/settings/rr/HardwareKeys.java
+++ src/com/android/settings/rr/HardwareKeys.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.rr.fragments;
+package com.android.settings.rr;
 
 import android.content.ContentResolver;
 import android.content.Context;
