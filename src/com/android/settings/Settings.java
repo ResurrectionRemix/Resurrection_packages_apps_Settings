BenzoRom/packages_apps_Settings

Settings: Global Actions with Advanced Reboot [3/3]
Change-Id: I601b4fb4a25e528767bcad7861446c2e8cdd377d
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/Settings.java
@@ -192,4 +192,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
 
     public static class BenzoSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class GlobalActionsSettingsActivity extends SettingsActivity { /* empty */ }
 }

ResurrectionRemix/Resurrection_packages_apps_Settings

Signed-off-by: mracar07 <umutcan@umutcanacar.me>
 Conflicts:
	AndroidManifest.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/core/gateway/SettingsGateway.java

src/com/android/settings/Settings.java
@@ -169,6 +169,7 @@ public class Settings extends SettingsActivity {
     public static class SmartbarSettingsActivity extends SettingsActivity {}
     public static class FlingSettingsActivity extends SettingsActivity {}
     public static class PulseSettingsActivity extends SettingsActivity {}
+    public static class PowerMenuSettings extends SettingsActivity { /* empty */ }
 
     public static class MainSettingsLayoutActivity extends SettingsActivity { /* empty */ }
     public static class MainSettingsActivity extends SettingsActivity { /* empty */ }