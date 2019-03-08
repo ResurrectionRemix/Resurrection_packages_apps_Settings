b53f5043eb0e96d69b75b5c4773a4a4aec7b1c7c
Felipe Leon <fglfgl27@gmail.com>
2019-03-08 17:12:39 -0300

    Settings: move Rounded Corner to misc
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- src/com/android/settings/rr/Misc.java
+++ src/com/android/settings/rr/Misc.java
@@ -16,11 +16,14 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.RemoteException;
+import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
@@ -31,11 +34,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.provider.Settings;
-import android.content.Context;
-import android.os.UserHandle;
-
 import com.android.settings.R;
+import com.android.settings.rr.Preferences.SecureSettingSwitchPreference;
+import com.android.settings.rr.Preferences.CustomSeekBarPreference;
 import com.android.settings.rr.utils.RRUtils;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable.SearchIndexProvider;
@@ -48,9 +49,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Misc extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+Preference.OnPreferenceChangeListener {
 
     private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+    private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+    private static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
+
+    private CustomSeekBarPreference mCornerRadius;
+    private CustomSeekBarPreference mContentPadding;
+    private SecureSettingSwitchPreference mRoundedFwvals;
 
     private ListPreference mFlashlightOnCall;
 
@@ -59,29 +67,86 @@ public class Misc extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rr_misc);
 
- 	Context mContext = getActivity().getApplicationContext();
+        Context mContext = getActivity().getApplicationContext();
 
         PreferenceScreen prefScreen = getPreferenceScreen();
         PreferenceScreen prefSet = getPreferenceScreen();
-		ContentResolver resolver = getActivity().getContentResolver();
+        ContentResolver resolver = getActivity().getContentResolver();
 
         mFlashlightOnCall = (ListPreference) findPreference(FLASHLIGHT_ON_CALL);
         if (!RRUtils.deviceSupportsFlashLight(mContext))
             prefScreen.removePreference(mFlashlightOnCall);
 
+        Resources res = null;
+        Context ctx = getContext();
+        float density = Resources.getSystem().getDisplayMetrics().density;
+
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Rounded Corner Radius
+        mCornerRadius = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
+        mCornerRadius.setOnPreferenceChangeListener(this);
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int cornerRadius = Settings.Secure.getInt(ctx.getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
+            (int)(res.getDimension(resourceIdRadius) / density));
+        mCornerRadius.setValue(cornerRadius / 1);
+
+        // Rounded Content Padding
+        mContentPadding = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
+        mContentPadding.setOnPreferenceChangeListener(this);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null,
+            null);
+        int contentPadding = Settings.Secure.getInt(ctx.getContentResolver(),
+            Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+            (int)(res.getDimension(resourceIdPadding) / density));
+        mContentPadding.setValue(contentPadding / 1);
+
+        // Rounded use Framework Values
+        mRoundedFwvals = (SecureSettingSwitchPreference) findPreference(SYSUI_ROUNDED_FWVALS);
+        mRoundedFwvals.setOnPreferenceChangeListener(this);
+
     }
 
+    private void restoreCorners() {
+        Resources res = null;
+        float density = Resources.getSystem().getDisplayMetrics().density;
+
+        try {
+            res = getContext().getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null,
+            null);
+        mCornerRadius.setValue((int)(res.getDimension(resourceIdRadius) / density));
+        mContentPadding.setValue((int)(res.getDimension(resourceIdPadding) / density));
+    }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-	ContentResolver resolver = getActivity().getContentResolver();
-	return false;
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCornerRadius) {
+            Settings.Secure.putInt(resolver, Settings.Secure.SYSUI_ROUNDED_SIZE,
+                ((int) newValue) * 1);
+        } else if (preference == mContentPadding) {
+            Settings.Secure.putInt(resolver, Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+                ((int) newValue) * 1);
+        } else if (preference == mRoundedFwvals) {
+            restoreCorners();
+        }
+        return true;
     }
 
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         Settings.System.putIntForUser(resolver,
-                Settings.System.FLASHLIGHT_ON_CALL, 0, UserHandle.USER_CURRENT);
+            Settings.System.FLASHLIGHT_ON_CALL, 0, UserHandle.USER_CURRENT);
     }
 
 
@@ -105,10 +170,10 @@ public class Misc extends SettingsPreferenceFragment implements
 
             @Override
             public List < String > getNonIndexableKeys(Context context) {
-                 List<String> keys = super.getNonIndexableKeys(context);
+                List < String > keys = super.getNonIndexableKeys(context);
 
-                 if (!RRUtils.deviceSupportsFlashLight(context))
-                     keys.add(FLASHLIGHT_ON_CALL);
+                if (!RRUtils.deviceSupportsFlashLight(context))
+                    keys.add(FLASHLIGHT_ON_CALL);
 
                 return keys;
             }
