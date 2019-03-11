de09d59bd87e5a4157eb1c842e5e9ea2b0135e3c
mracar07 <umutcan@umutcanacar.me>
2019-03-11 09:07:36 +0000

    Revert "Settings: Only show gesture navigation if device has a software nav bar"
    This reverts commit f52a30fd2b48d2bd504f10067ee8b6d79861a33c.

--- src/com/android/settings/gestures/SwipeUpPreferenceController.java
+++ src/com/android/settings/gestures/SwipeUpPreferenceController.java
@@ -20,12 +20,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.RemoteException;
 import android.os.UserManager;
 import android.provider.Settings;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
-import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
 
@@ -44,15 +42,7 @@ public class SwipeUpPreferenceController extends GesturePreferenceController {
     }
 
     static boolean isGestureAvailable(Context context) {
-        boolean hasNav = false;
-        final boolean configEnabled =
-                context.getResources().getBoolean(R.bool.config_swipe_up_gesture_setting_available);
-        try {
-            hasNav = WindowManagerGlobal.getWindowManagerService().hasNavigationBar();
-        } catch (RemoteException ex) {
-            // no window manager? good luck with that
-        }
-        if (!hasNav || !configEnabled) {
+        if (!context.getResources().getBoolean(R.bool.config_swipe_up_gesture_setting_available)) {
             return false;
         }
 
