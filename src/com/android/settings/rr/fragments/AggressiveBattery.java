syberia-project/platform_packages_apps_SyberiaSettings

Implement Aggressive Battery [2/2]
Aggressive Idle is a feature that makes the device idle constants
more aggressive and ignores device motion when entering doze mode.

Aggressive Standby is a feature that stops apps from running in
the background as much.

Change-Id: Ia50819daad84a79900635e49f98646e381ebd47e
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/syberia/settings/fragments/AggressiveBattery.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright © 2019 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.syberia.settings.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class AggressiveBattery extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.aggressive_battery);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.aggressive_battery_warning_text);
+
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.SYBERIA;
+    }
+}

ResurrectionRemix/Resurrection_packages_apps_Settings

Signed-off-by: mracar07 <umutcan@umutcanacar.me>

src/com/android/settings/rr/fragments/AggressiveBattery.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright © 2019 Syberia Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rr.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class AggressiveBattery extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.aggressive_battery);
+
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.aggressive_battery_warning_text);
+
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.RESURRECTED;
+    }
+}