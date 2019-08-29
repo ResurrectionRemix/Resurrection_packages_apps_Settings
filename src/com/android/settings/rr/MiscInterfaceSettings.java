faea7fdb2398beabd26c4a76eb7fcc69d468373d
Varun Date <date.varun123@gmail.com>
2019-08-29 14:17:10 +1000

    Configurations: Move some options here n there Move signal icons to system icons make other missing options available to nav settings Change-Id: I68830eed9503c5181c27a6034a8d5c60379e735e Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/MiscInterfaceSettings.java
+++ src/com/android/settings/rr/MiscInterfaceSettings.java
@@ -13,36 +13,58 @@
 */
 package com.android.settings.rr;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.settings.R;
+import com.android.settings.rr.Preferences.SecureSettingSwitchPreference;
+import com.android.settings.rr.Preferences.CustomSeekBarPreference;
+import com.android.settings.rr.utils.RRUtils;
+import com.android.settings.search.Indexable.SearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.Utils;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-import com.android.settings.R;
-import com.android.settings.rr.utils.RRUtils;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable.SearchIndexProvider;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
     private static final String TAG = "MiscInterfaceSettings";
     private static final String RR_OTA = "rr_ota_fab";
+    private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+    private static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
+
+    private CustomSeekBarPreference mCornerRadius;
+    private CustomSeekBarPreference mContentPadding;
+    private SecureSettingSwitchPreference mRoundedFwvals;
+
 
     private SwitchPreference mConfig;
 
@@ -57,9 +79,37 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PreferenceScreen prefScreen = getPreferenceScreen();
-
         addPreferencesFromResource(R.xml.rr_interface_other_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        Resources res;
+        ContentResolver resolver = getActivity().getContentResolver();
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        try {
+            res = getContext().getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Rounded Corner Radius
+        mCornerRadius = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
+        mCornerRadius.setOnPreferenceChangeListener(this);
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int cornerRadius = Settings.Secure.getInt(resolver, Settings.Secure.SYSUI_ROUNDED_SIZE,
+            (int)(res.getDimension(resourceIdRadius) / density));
+        mCornerRadius.setValue(cornerRadius / 1);
+
+        // Rounded Content Padding
+        mContentPadding = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
+        mContentPadding.setOnPreferenceChangeListener(this);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null, null);
+        int contentPadding = Settings.Secure.getInt(resolver, Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+            (int)(res.getDimension(resourceIdPadding) / density));
+        mContentPadding.setValue(contentPadding / 1);
+
+        // Rounded use Framework Values
+        mRoundedFwvals = (SecureSettingSwitchPreference) findPreference(SYSUI_ROUNDED_FWVALS);
+        mRoundedFwvals.setOnPreferenceChangeListener(this);
         mConfig = (SwitchPreference) findPreference(RR_OTA);
         mConfig.setChecked((Settings.System.getInt(getContentResolver(), Settings.System.RR_OTA_FAB, 1) == 1));
         mConfig.setOnPreferenceChangeListener(this);
@@ -77,10 +127,29 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
         }
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+
+    private void restoreCorners() {
+        Resources res;
+        float density = Resources.getSystem().getDisplayMetrics().density;
+
+        try {
+            res = getContext().getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        int resourceIdRadius = res.getIdentifier("com.android.systemui:dimen/rounded_corner_radius", null, null);
+        int resourceIdPadding = res.getIdentifier("com.android.systemui:dimen/rounded_corner_content_padding", null,
+            null);
+        mCornerRadius.setValue((int)(res.getDimension(resourceIdRadius) / density));
+        mContentPadding.setValue((int)(res.getDimension(resourceIdPadding) / density));
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mConfig) {
-            boolean newvalue = (Boolean) objValue;
+            boolean newvalue = (Boolean) newValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.RR_OTA_FAB, newvalue ? 1 : 0);
             finish();
@@ -89,13 +158,20 @@ public class MiscInterfaceSettings extends SettingsPreferenceFragment implements
             startActivity(fabIntent);
             return true;
         } else if (preference.equals(mAnnoyingNotification)) {
-            int mode = Integer.parseInt(((String) objValue).toString());
+            int mode = Integer.parseInt(((String) newValue).toString());
             Settings.System.putIntForUser(getContentResolver(),
                     Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, mode, UserHandle.USER_CURRENT);
             return true;
+        } else if (preference == mCornerRadius) {
+            Settings.Secure.putInt(resolver, Settings.Secure.SYSUI_ROUNDED_SIZE,
+                ((int) newValue) * 1);
+        } else if (preference == mContentPadding) {
+            Settings.Secure.putInt(resolver, Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING,
+                ((int) newValue) * 1);
+        } else if (preference == mRoundedFwvals) {
+            restoreCorners();
         }
-
-        return false;
+        return true;
     }
 
     public static void reset(Context mContext) {
