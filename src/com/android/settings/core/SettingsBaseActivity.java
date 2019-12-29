6d4a8ca848c80a70d6f19074ebe8d4b454f2d65e
Lucchetto <lucchetto.tie@live.com>
2019-12-29 13:18:36 +0100

    Settings: Blur the action bar
    Change-Id: I3b95562b8a53f2b09f35d95f01eaf5adb8dedcb6
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- src/com/android/settings/core/SettingsBaseActivity.java
+++ src/com/android/settings/core/SettingsBaseActivity.java
@@ -25,6 +25,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -34,7 +35,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.FrameLayout;
 import android.widget.Toolbar;
 
 import androidx.fragment.app.FragmentActivity;
@@ -44,6 +44,9 @@ import com.android.settings.SubSettings;
 import com.android.settings.dashboard.CategoryManager;
 
 import com.google.android.setupcompat.util.WizardManagerHelper;
+
+import eightbitlab.com.blurview.BlurView;
+import eightbitlab.com.blurview.RenderScriptBlur;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,6 +86,12 @@ public class SettingsBaseActivity extends FragmentActivity {
 
         final View decorView = getWindow().getDecorView();
         final ViewGroup root = (ViewGroup) decorView.findViewById(android.R.id.content);
+        final ViewGroup contentFrame = findViewById(R.id.content_frame);
+        final Drawable windowBackground = decorView.getBackground();
+
+        final BlurView actionBarBlur = findViewById(R.id.action_bar_blur);
+        actionBarBlur.setupWith(contentFrame).setFrameClearDrawable(windowBackground)
+                .setBlurAlgorithm(new RenderScriptBlur(this)).setHasFixedTransformationMatrix(true);
 
         final Toolbar toolbar = findViewById(R.id.action_bar);
         if (theme.getBoolean(android.R.styleable.Theme_windowNoTitle, false)) {
@@ -95,14 +104,14 @@ public class SettingsBaseActivity extends FragmentActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         root.setOnApplyWindowInsetsListener((v, insets) -> {
-            final FrameLayout contentFrame = findViewById(R.id.content_frame);
             final TypedArray styledAttributes = getTheme().obtainStyledAttributes(
                 new int[] { android.R.attr.actionBarSize });
-            final int actionBarSize = (int) styledAttributes.getDimension(0, 0);
+            final int actionBarSize = (int) styledAttributes.getDimension(0, 0)
+                    + insets.getSystemWindowInsetTop();
             styledAttributes.recycle();
 
             toolbar.setPadding(0, insets.getSystemWindowInsetTop(), 0, 0);
-            contentFrame.setPadding(0, insets.getSystemWindowInsetTop() + actionBarSize, 0, 0);
+            contentFrame.setPadding(0, actionBarSize, 0, 0);
             return insets;
         });
 
