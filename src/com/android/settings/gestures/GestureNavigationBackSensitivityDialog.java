8aeaa53222a5d3fcb74241a16f8980e1021539e5
Pranav Vashi <neobuddy89@gmail.com>
2020-02-23 20:14:25 +0300

    settings: add optional back gesture vibration tick [1/2]
    Change-Id: Id4d3741ef3380b330bda465b3ce10eeddbfd0cb1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -38,6 +38,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
 
     private boolean mArrowSwitchChecked;
     private boolean mGesturePillSwitchChecked;
+    private boolean mGestureHapticChecked;
 
     private static final String TAG = "GestureNavigationBackSensitivityDialog";
     private static final String KEY_BACK_SENSITIVITY = "back_sensitivity";
@@ -91,6 +92,16 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                 mGesturePillSwitchChecked = gesturePillSwitch.isChecked() ? true : false;
             }
         });
+        final Switch GestureHapticSwitch = view.findViewById(R.id.back_gesture_haptic);
+        mGestureHapticChecked = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.BACK_GESTURE_HAPTIC, 0) == 1;
+        GestureHapticSwitch.setChecked(mGestureHapticChecked);
+        GestureHapticSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGestureHapticChecked = GestureHapticSwitch.isChecked() ? true : false;
+            }
+        });
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.back_options_dialog_title)
                 .setMessage(R.string.back_sensitivity_dialog_message)
@@ -107,6 +118,8 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                             Settings.Secure.SHOW_BACK_ARROW_GESTURE, mArrowSwitchChecked ? 1 : 0);
                     Settings.System.putInt(getActivity().getContentResolver(),
                             Settings.System.GESTURE_PILL_TOGGLE, mGesturePillSwitchChecked ? 1 : 0);
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.BACK_GESTURE_HAPTIC, mGestureHapticChecked ? 1 : 0);
                     SystemNavigationGestureSettings.setBackGestureOverlaysToUse(getActivity());
                     SystemNavigationGestureSettings.setCurrentSystemNavigationMode(getActivity(),
                             getOverlayManager(), SystemNavigationGestureSettings.getCurrentSystemNavigationMode(getActivity()));
