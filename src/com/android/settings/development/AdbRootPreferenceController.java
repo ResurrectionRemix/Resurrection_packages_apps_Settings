379dcc761a73663da5e576ac7ad2b222ddafd9ef
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-03-13 23:05:25 +0100

    Settings: Hide ADB Root preference on user builds
    Change-Id: I8c9251ddda0eb15e1134d4fafbf3d972f5fa8809

--- src/com/android/settings/development/AdbRootPreferenceController.java
+++ src/com/android/settings/development/AdbRootPreferenceController.java
@@ -18,6 +18,7 @@ package com.android.settings.development;
 
 import android.adb.ADBRootService;
 import android.content.Context;
+import android.os.Build;
 import android.os.UserManager;
 
 import androidx.preference.Preference;
@@ -48,6 +49,11 @@ public class AdbRootPreferenceController extends DeveloperOptionsPreferenceContr
     @Override
     public String getPreferenceKey() {
         return PREF_KEY;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Build.IS_DEBUGGABLE;
     }
 
     @Override
