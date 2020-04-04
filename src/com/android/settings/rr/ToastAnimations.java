b8d7074e1d8b3366b71cf2b5333f8f4841de19b8
rogersb11 <brettrogers11@gmail.com>
2020-04-04 12:21:29 +1100

    RRtools:Toast Animations [2/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    Change-Id: I33a41354bb3fe875b31b4507d10880ad023e3ec5
    Signed-off-by: Varun Date <date.varun123@gmail.com>

---
+++ src/com/android/settings/rr/ToastAnimations.java
@@ -0,0 +1,77 @@
+/*Copyright (C) 2015 The ResurrectionRemix Project
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+package com.android.settings.rr;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.provider.Settings;
+import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.rr.utils.RRUtils;
+import com.android.settings.search.Indexable.SearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class ToastAnimations extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "ToastAnimations";
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
+    private ListPreference mToastAnimation;
+    private Context mContext;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.RESURRECTED;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.rr_toast);
+	    mContext = getActivity().getApplicationContext();
+
+ 	    mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mToastAnimation) {
+              int index = mToastAnimation.findIndexOfValue((String) newValue);
+              Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) newValue);
+              mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+              Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+              return true;
+        } 
+        return false;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        RRUtils.addSearchIndexProvider(R.xml.rr_toast);
+}
