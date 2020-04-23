cd50effb3814a5abe5ea75309a051dfa74d15541
maxwen <max.weninger@gmail.com>
2020-04-24 00:28:35 +1000

    Clean up Edge Lightings a bit
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/LockSettings.java
+++ src/com/android/settings/rr/LockSettings.java
@@ -41,58 +41,62 @@ import com.android.settings.rr.Preferences.CustomSeekBarPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.android.settings.rr.Preferences.SystemSettingColorPickerPreference;
 import com.android.settingslib.search.SearchIndexable;
 @SearchIndexable
 public class LockSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String PULSE_AMBIENT_LIGHT_COLOR = "pulse_ambient_light_color";
+    private static final String PULSE_AMBIENT_LIGHT_CUSTOM_COLOR = "ambient_light_custom_color";
+    private static final String PULSE_AMBIENT_LIGHT_COLOR = "ambient_light_color";
 	
-    private ColorPickerPreference mEdgeLightColorPreference;
+    private SystemSettingColorPickerPreference mEdgeLightColorPreference;
+    private ListPreference mColorType;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rr_lock);
+        ContentResolver resolver = getActivity().getContentResolver();
 
-        mEdgeLightColorPreference = (ColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
+        mEdgeLightColorPreference = (SystemSettingColorPickerPreference) findPreference(PULSE_AMBIENT_LIGHT_CUSTOM_COLOR);
         mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
 
-        int edgeLightColor = Settings.System.getInt(getContentResolver(),
-                Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
-        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
-        if (edgeLightColorHex.equals("#ff3980ff")) {
-            mEdgeLightColorPreference.setSummary(R.string.default_string);
-        } else {
-            mEdgeLightColorPreference.setSummary(edgeLightColorHex);
-        }
-        mEdgeLightColorPreference.setNewPreviewColor(edgeLightColor);
-
+        mColorType = (ListPreference) findPreference(PULSE_AMBIENT_LIGHT_COLOR);
+        int type = Settings.System.getInt(resolver,
+            Settings.System.AMBIENT_LIGHT_COLOR, 0);
+        mColorType.setValue(String.valueOf(type));
+        mColorType.setSummary(mColorType.getEntry());
+        updateprefs(type);
+        mColorType.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mEdgeLightColorPreference) {
-            String hex = ColorPickerPreference.convertToARGB(
-                   Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#ff3980ff")) {
-                preference.setSummary(R.string.default_string);
-            } else {
-                preference.setSummary(hex);
-            }
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.PULSE_AMBIENT_LIGHT_COLOR, intHex);
+        if (preference == mColorType) {
+            int val = Integer.parseInt((String) newValue);
+            int index = mColorType.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.AMBIENT_LIGHT_COLOR, val);
+            mColorType.setSummary(mColorType.getEntries()[index]);
+            updateprefs(val);
             return true;
-         }
+        }
        return false;
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.RESURRECTED;
+    }
+
+    public void updateprefs(int type) {
+         if (type == 3 ) {
+             mEdgeLightColorPreference.setEnabled(true);
+         } else {
+             mEdgeLightColorPreference.setEnabled(false);
+         }
     }
 
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
