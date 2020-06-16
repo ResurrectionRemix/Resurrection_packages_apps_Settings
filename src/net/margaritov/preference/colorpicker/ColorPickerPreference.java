d6df09f95e1cc65005b3428e79faf0bf24477398
bigrushdog <randall.rushing@gmail.com>
2020-06-16 09:06:59 -0700

    ColorPickerPreference: Use material design layout
    This fixes the left aligned preference title
    
    Change-Id: Ie5095b2f8160070440a9d392f5f7dbcd9de87b25

--- src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerPreference.java
@@ -88,6 +88,7 @@ public class ColorPickerPreference extends Preference implements
     public ColorPickerPreference(Context context, AttributeSet attrs, int defStyleAttr,
             int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        setLayoutResource(R.layout.preference_material_settings);
         init(context, attrs);
     }
 
