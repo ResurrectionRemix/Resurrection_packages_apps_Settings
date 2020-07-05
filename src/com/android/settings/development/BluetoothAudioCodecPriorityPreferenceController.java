18733ebdc703394cebcf0eaeb34d563bb319fbe4
Pranav Vashi <neobuddy89@gmail.com>
2020-07-05 22:51:02 +0530

    A2DP codec priority: Rewrite using BT controller
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/development/BluetoothAudioCodecPriorityPreferenceController.java
+++ src/com/android/settings/development/BluetoothAudioCodecPriorityPreferenceController.java
@@ -13,32 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.settings.development;
 
-import android.content.ContentResolver;
+import android.bluetooth.BluetoothCodecConfig;
 import android.content.Context;
 import android.provider.Settings;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-
 import com.android.settings.R;
-import com.android.settings.core.PreferenceControllerMixin;
-import com.android.settingslib.development.DeveloperOptionsPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
-public class BluetoothAudioCodecPriorityPreferenceController extends DeveloperOptionsPreferenceController
-        implements Preference.OnPreferenceChangeListener, PreferenceControllerMixin {
+public class BluetoothAudioCodecPriorityPreferenceController extends
+        AbstractBluetoothA2dpPreferenceController {
 
+    private static final int DEFAULT_INDEX = 0;
     private static final String A2DP_SOURCE_CODEC_PRIORITY = "a2dp_source_codec_priority";
 
-    private final String[] mListValues;
-    private final String[] mListSummaries;
-
-    public BluetoothAudioCodecPriorityPreferenceController(Context context) {
-        super(context);
-
-        mListValues = context.getResources().getStringArray(R.array.a2dp_source_codec_priority_values);
-        mListSummaries = context.getResources().getStringArray(R.array.a2dp_source_codec_priority_entries);
+    public BluetoothAudioCodecPriorityPreferenceController(Context context, Lifecycle lifecycle,
+            BluetoothA2dpConfigStore store) {
+        super(context, lifecycle, store);
     }
 
     @Override
@@ -47,36 +40,71 @@ public class BluetoothAudioCodecPriorityPreferenceController extends DeveloperOp
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        writeBluetoothAudioCodecPriorityOptions(newValue);
-        updateBluetoothAudioCodecPriorityOptions();
-        return true;
+    protected String[] getListValues() {
+        return mContext.getResources().getStringArray(
+                R.array.a2dp_source_codec_priority_values);
     }
 
     @Override
-    public void updateState(Preference preference) {
-        updateBluetoothAudioCodecPriorityOptions();
+    protected String[] getListSummaries() {
+        return mContext.getResources().getStringArray(
+                R.array.a2dp_source_codec_priority_entries);
     }
 
-    private void updateBluetoothAudioCodecPriorityOptions() {
-        final int value = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.A2DP_SOURCE_CODEC_PRIORITY, 0);
-        int index = 0; // default
-        for (int i = 0; i < mListValues.length; i++) {
-            int val = Integer.parseInt(mListValues[i]);
-            if (val >= value) {
-                index = i;
-                break;
-            }
-        }
-        final ListPreference listPreference = (ListPreference) mPreference;
-        listPreference.setValue(mListValues[index]);
-        listPreference.setSummary(mListSummaries[index]);
+    @Override
+    protected int getDefaultIndex() {
+        return DEFAULT_INDEX;
     }
 
-    private void writeBluetoothAudioCodecPriorityOptions(Object newValue) {
+    @Override
+    protected void writeConfigurationValues(Object newValue) {
         final int value = Integer.valueOf((String) newValue);
         Settings.System.putInt(mContext.getContentResolver(),
                 Settings.System.A2DP_SOURCE_CODEC_PRIORITY, value);
+
+        int codecTypeValue = BluetoothCodecConfig.SAMPLE_RATE_NONE; // default
+        int codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_DEFAULT;
+        switch (value) {
+            case 0:
+            default:
+                // do nothing
+                break;
+            case 1:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_SBC;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 2:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_AAC;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 3:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 4:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_HD;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 5:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_LDAC;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 6:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_ADAPTIVE;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+            case 7:
+                codecTypeValue = BluetoothCodecConfig.SOURCE_CODEC_TYPE_APTX_TWSP;
+                codecPriorityValue = BluetoothCodecConfig.CODEC_PRIORITY_HIGHEST;
+                break;
+        }
+        mBluetoothA2dpConfigStore.setCodecType(codecTypeValue);
+        mBluetoothA2dpConfigStore.setCodecPriority(codecPriorityValue);
+    }
+
+    @Override
+    protected int getCurrentA2dpSettingIndex(BluetoothCodecConfig config) {
+        return Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.A2DP_SOURCE_CODEC_PRIORITY, 0);
     }
 }
