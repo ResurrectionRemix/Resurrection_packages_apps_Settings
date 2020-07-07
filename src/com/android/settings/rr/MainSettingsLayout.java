f656a316481b266ae1fb99c9eb665f94ba42418f
Pranav Vashi <neobuddy89@gmail.com>
2020-07-08 03:38:21 +0530

    Add switch to select SBC HD by default [3/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/MainSettingsLayout.java
+++ src/com/android/settings/rr/MainSettingsLayout.java
@@ -580,6 +580,8 @@ public class MainSettingsLayout extends SettingsPreferenceFragment {
                 Settings.System.LOCKSCREEN_WEATHER_STYLE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.A2DP_SOURCE_CODEC_PRIORITY, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SBC_HD_PRIORITY, 0, UserHandle.USER_CURRENT);
     }
 
     private static boolean isAudioPanelOnLeftSide(Context context) {
