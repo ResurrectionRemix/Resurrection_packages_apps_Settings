5ea420a0d7b23bbee71bd9968a78b5c3808ba293
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-08-09 06:23:05 +1000

    Add Configurable sensor block package [2/2]
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/rr/BatterySettings.java
+++ src/com/android/settings/rr/BatterySettings.java
@@ -13,15 +13,25 @@
 */
 package com.android.settings.rr;
 
-import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import androidx.preference.ListPreference;
 import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceScreen;
 import com.android.settings.Utils;
-
+import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
@@ -31,15 +41,41 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
+import androidx.preference.PreferenceGroup;
 import android.provider.Settings;
-import android.util.Log;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.os.UserHandle;
 import java.util.Arrays;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.rr.Preferences.PackageListAdapter;
+import com.android.settings.rr.Preferences.PackageListAdapter.PackageItem;
 @SearchIndexable
 public class BatterySettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
-    private static final String TAG = "BatterySettings";
+        Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+    private static final int DIALOG_BLOCKED_APPS = 1;
+    private static final String SENSOR_BLOCK = "sensor_block";
+
+    private PackageListAdapter mPackageAdapter;
+    private PackageManager mPackageManager;
+    private PreferenceGroup mSensorBlockPrefList;
+    private Preference mAddSensorBlockPref;
+
+    private String mBlockedPackageList;
+    private Map<String, Package> mBlockedPackages;
+    private Context mContext;
 
     @Override
     public int getMetricsCategory() {
@@ -50,6 +86,23 @@ public class BatterySettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.aggressive_battery);
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.add_sensor_block_package_summary);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPackageManager = getPackageManager();
+        mPackageAdapter = new PackageListAdapter(getActivity());
+
+        mSensorBlockPrefList = (PreferenceGroup) findPreference("sensor_block_applications");
+        mSensorBlockPrefList.setOrderingAsAdded(false);
+
+        mBlockedPackages = new HashMap<String, Package>();
+
+        mAddSensorBlockPref = findPreference("add_sensor_block_packages");
+
+        mAddSensorBlockPref.setOnPreferenceClickListener(this);
+
+        mContext = getActivity().getApplicationContext();
 
         int anim = Settings.System.getInt(getActivity().getContentResolver(),
                 Settings.System.RR_CONFIG_ANIM, 0);
@@ -67,6 +120,222 @@ public class BatterySettings extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object objValue) 		{
         return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshCustomApplicationPrefs();
+    }
+
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_BLOCKED_APPS) {
+            return MetricsProto.MetricsEvent.RESURRECTED;
+        }
+        return 0;
+    }
+
+    /**
+     * Utility classes and supporting methods
+     */
+    @Override
+    public Dialog onCreateDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final Dialog dialog;
+        final ListView list = new ListView(getActivity());
+        list.setAdapter(mPackageAdapter);
+        list.setDivider(null);
+
+        builder.setTitle(R.string.profile_choose_app);
+        builder.setView(list);
+        dialog = builder.create();
+
+        switch (id) {
+            case DIALOG_BLOCKED_APPS:
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        addCustomApplicationPref(info.packageName, mBlockedPackages);
+                        dialog.cancel();
+                    }
+                });
+        }
+        return dialog;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SENSOR_BLOCK, 1, UserHandle.USER_CURRENT);
+        Settings.System.putString(resolver,
+                Settings.System.SENSOR_BLOCKED_APP, null);
+    }
+
+    /**
+     * Application class
+     */
+    private static class Package {
+        public String name;
+        /**
+         * Stores all the application values in one call
+         * @param name
+         */
+        public Package(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+            builder.append(name);
+            return builder.toString();
+        }
+
+        public static Package fromString(String value) {
+            if (TextUtils.isEmpty(value)) {
+                return null;
+            }
+
+            try {
+                Package item = new Package(value);
+                return item;
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    };
+
+    private void refreshCustomApplicationPrefs() {
+        if (!parsePackageList()) {
+            return;
+        }
+
+        // Add the Application Preferences
+        if (mSensorBlockPrefList != null) {
+            mSensorBlockPrefList.removeAll();
+
+            for (Package pkg : mBlockedPackages.values()) {
+                try {
+                    Preference pref = createPreferenceFromInfo(pkg);
+                    mSensorBlockPrefList.addPreference(pref);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // Do nothing
+                }
+            }
+
+            // Keep these at the top
+            mAddSensorBlockPref.setOrder(0);
+            // Add 'add' options
+            mSensorBlockPrefList.addPreference(mAddSensorBlockPref);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        if (preference == mAddSensorBlockPref) {
+            showDialog(DIALOG_BLOCKED_APPS);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.dialog_delete_title)
+                    .setMessage(R.string.dialog_delete_message)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (preference == mSensorBlockPrefList.findPreference(preference.getKey())) {
+                                removeApplicationPref(preference.getKey(), mBlockedPackages);
+                            }
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, null);
+
+            builder.show();
+        }
+        return true;
+    }
+
+    private void addCustomApplicationPref(String packageName, Map<String,Package> map) {
+        Package pkg = map.get(packageName);
+        if (pkg == null) {
+            pkg = new Package(packageName);
+            map.put(packageName, pkg);
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private Preference createPreferenceFromInfo(Package pkg)
+            throws PackageManager.NameNotFoundException {
+        PackageInfo info = mPackageManager.getPackageInfo(pkg.name,
+                PackageManager.GET_META_DATA);
+        Preference pref =
+                new Preference(getActivity());
+
+        pref.setKey(pkg.name);
+        pref.setTitle(info.applicationInfo.loadLabel(mPackageManager));
+        pref.setIcon(info.applicationInfo.loadIcon(mPackageManager));
+        pref.setPersistent(false);
+        pref.setOnPreferenceClickListener(this);
+        return pref;
+    }
+
+    private void removeApplicationPref(String packageName, Map<String,Package> map) {
+        if (map.remove(packageName) != null) {
+            savePackageList(false, map);
+            refreshCustomApplicationPrefs();
+        }
+    }
+
+    private boolean parsePackageList() {
+        boolean parsed = false;
+
+        String sensorBlockString = Settings.System.getString(getContentResolver(),
+                Settings.System.SENSOR_BLOCKED_APP);
+
+        if (sensorBlockString != null &&
+                !TextUtils.equals(mBlockedPackageList, sensorBlockString)) {
+            mBlockedPackageList = sensorBlockString;
+            mBlockedPackages.clear();
+            parseAndAddToMap(sensorBlockString, mBlockedPackages);
+            parsed = true;
+        }
+
+        return parsed;
+    }
+
+    private void parseAndAddToMap(String baseString, Map<String,Package> map) {
+        if (baseString == null) {
+            return;
+        }
+
+        final String[] array = TextUtils.split(baseString, "\\|");
+        for (String item : array) {
+            if (TextUtils.isEmpty(item)) {
+                continue;
+            }
+            Package pkg = Package.fromString(item);
+            map.put(pkg.name, pkg);
+        }
+    }
+
+    private void savePackageList(boolean preferencesUpdated, Map<String,Package> map) {
+        String setting = map == mBlockedPackages ? Settings.System.SENSOR_BLOCKED_APP : Settings.System.SENSOR_BLOCKED_APP_DUMMY;
+
+        List<String> settings = new ArrayList<String>();
+        for (Package app : map.values()) {
+            settings.add(app.toString());
+        }
+        final String value = TextUtils.join("|", settings);
+        if (preferencesUpdated) {
+            if (TextUtils.equals(setting, Settings.System.SENSOR_BLOCKED_APP)) {
+                mBlockedPackageList = value;
+            }
+        }
+        Settings.System.putString(getContentResolver(),
+                setting, value);
     }
 
     /**
