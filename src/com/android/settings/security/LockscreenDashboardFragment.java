7361e5247f6a381ef2af210a2e3f1d8a98b9910d
Varun Date <date.varun123@gmail.com>
2020-10-06 17:22:23 +1100

    Settings: Remove ambient wake gesture switch
    does nothing
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -27,7 +27,6 @@ import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
-import com.android.settings.display.AmbientWakeGesturesPreferenceController;
 import com.android.settings.display.PulseOnNewTracksPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
@@ -97,7 +96,6 @@ public class LockscreenDashboardFragment extends DashboardFragment {
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
-        use(AmbientWakeGesturesPreferenceController.class).setConfig(getConfig(context));
         use(PulseOnNewTracksPreferenceController.class).setConfig(getConfig(context));
     }
 
